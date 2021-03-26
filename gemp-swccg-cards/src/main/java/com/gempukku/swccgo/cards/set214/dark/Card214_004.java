bdc916ba7cce4d0ff55679303ca7bc60cd12241d
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2021-03-25 23:31:36 -0600

    Update Card214_004.java

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set214/dark/Card214_004.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set214/dark/Card214_004.java
@@ -56,7 +56,7 @@ public class Card214_004 extends AbstractSite {
         List<Modifier> modifiers = new LinkedList<>();
         modifiers.add(new MayNotForceDrainAtLocationModifier(self));
         modifiers.add(new CancelForceIconModifier(self, self, Integer.MAX_VALUE, Icon.LIGHT_FORCE, false));
-        modifiers.add(new DeployCostToLocationModifier(self, 2, Filters.and(Filters.your(playerOnLightSideOfLocation), Filters.character)));
+        modifiers.add(new DeployCostToLocationModifier(self, Filters.and(Filters.your(playerOnLightSideOfLocation), Filters.character), 2, self));
         return modifiers;
     }
 }
