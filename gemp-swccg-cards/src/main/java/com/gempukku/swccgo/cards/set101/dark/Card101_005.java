407341abd8d2dd100c73628aef7812c3656daaed
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2021-03-30 23:53:43 -0600

    April Fools

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set101/dark/Card101_005.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set101/dark/Card101_005.java
@@ -1,29 +1,32 @@
 package com.gempukku.swccgo.cards.set101.dark;
-
 import com.gempukku.swccgo.cards.AbstractImperial;
 import com.gempukku.swccgo.cards.GameConditions;
-import com.gempukku.swccgo.cards.conditions.AloneCondition;
 import com.gempukku.swccgo.cards.conditions.OnTableCondition;
+import com.gempukku.swccgo.cards.conditions.PilotingCondition;
+import com.gempukku.swccgo.cards.effects.usage.OncePerBattleEffect;
 import com.gempukku.swccgo.common.*;
 import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.game.state.GameState;
+import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.conditions.AndCondition;
 import com.gempukku.swccgo.logic.conditions.Condition;
 import com.gempukku.swccgo.logic.conditions.NotCondition;
 import com.gempukku.swccgo.logic.effects.DrawDestinyEffect;
+import com.gempukku.swccgo.logic.effects.LoseCardFromTableEffect;
+import com.gempukku.swccgo.logic.effects.RespondableEffect;
+import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
 import com.gempukku.swccgo.logic.effects.choose.ChooseCardToLoseFromTableEffect;
 import com.gempukku.swccgo.logic.modifiers.*;
+import com.gempukku.swccgo.logic.timing.Action;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 import com.gempukku.swccgo.logic.timing.GuiUtils;
-
 import java.util.*;
-
-
 /**
  * Set: Premium (Premiere Introductory Two Player Game)
  * Type: Character
@@ -38,14 +41,12 @@ public class Card101_005 extends AbstractImperial {
         addPersona(Persona.VADER);
         addIcons(Icon.WARRIOR);
     }
-
     @Override
     protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
         if (game.isCasual())
             return Filters.any;
         return Filters.Deploys_on_Death_Star;
     }
-
     @Override
     protected List<Modifier> getGameTextAlwaysOnModifiers(final SwccgGame game, PhysicalCard self) {
         Condition isCasualCondition = new Condition() {
@@ -58,9 +59,10 @@ public class Card101_005 extends AbstractImperial {
         modifiers.add(new MayNotDeployModifier(self, new AndCondition(new NotCondition(isCasualCondition), new OnTableCondition(self, 2, Filters.and(Filters.opponents(self), Filters.unique, Filters.character)))));
         modifiers.add(new PowerModifier(self, isCasualCondition, 3));
         modifiers.add(new DestinyModifier(self, self, isCasualCondition, 8));
+        modifiers.add(new DeployCostModifier(self, self, isCasualCondition, -7));
+        modifiers.add(new IconModifier(self, isCasualCondition, Icon.PILOT));
         return modifiers;
     }
-
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(final SwccgGame game, final PhysicalCard self) {
         Condition isCasualCondition = new Condition() {
@@ -71,18 +73,86 @@ public class Card101_005 extends AbstractImperial {
         };
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(new ImmuneToAttritionModifier(self, isCasualCondition));
+        modifiers.add(new PowerModifier(self, Filters.hasPiloting(self), isCasualCondition, 7));
+        modifiers.add(new AddsBattleDestinyModifier(self, new AndCondition(new PilotingCondition(self), isCasualCondition), 1));
+        modifiers.add(new MayNotHaveGameTextCanceledModifier(self, isCasualCondition));
         return modifiers;
     }
 
     @Override
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, final SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        // Check condition(s)
+        if (GameConditions.isOncePerBattle(game, self, playerId, gameTextSourceCardId)
+                && game.isCasual()) {
+            TargetingReason targetingReason = TargetingReason.TO_BE_CHOKED;
+            Filter filter = Filters.and(Filters.opponents(self), Filters.character, Filters.present(self));
+            if (GameConditions.canTarget(game, self, targetingReason, filter)) {
+                final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId);
+                action.setText("'Choke' a character");
+                // Update usage limit(s)
+                action.appendUsage(
+                        new OncePerBattleEffect(action));
+                // Choose target(s)
+                action.appendTargeting(
+                        new TargetCardOnTableEffect(action, playerId, "Target character", targetingReason, filter) {
+                            @Override
+                            protected void cardTargeted(final int targetGroupId, PhysicalCard targetedCard) {
+                                action.addAnimationGroup(targetedCard);
+                                // Allow response(s)
+                                action.allowResponses(playerId + " targets to 'choke' " + GameUtils.getCardLink(targetedCard),
+                                        new RespondableEffect(action) {
+                                            @Override
+                                            protected void performActionResults(Action targetingAction) {
+                                                // Get the targeted card(s) from the action using the targetGroupId.
+                                                // This needs to be done in case the target(s) were changed during the responses.
+                                                final PhysicalCard cardToChoke = targetingAction.getPrimaryTargetCard(targetGroupId);
+                                                // Perform result(s)
+                                                action.appendEffect(
+                                                        new DrawDestinyEffect(action, playerId, 1, DestinyType.CHOKE_DESTINY) {
+                                                            @Override
+                                                            protected Collection<PhysicalCard> getGameTextAbilityManeuverOrDefenseValueTargeted() {
+                                                                return Collections.singletonList(cardToChoke);
+                                                            }
+                                                            @Override
+                                                            protected void destinyDraws(SwccgGame game, List<PhysicalCard> destinyCardDraws, List<Float> destinyDrawValues, Float totalDestiny) {
+                                                                GameState gameState = game.getGameState();
+                                                                if (totalDestiny == null) {
+                                                                    gameState.sendMessage("Result: Failed due to failed destiny draw");
+                                                                    return;
+                                                                }
+                                                                float ability = game.getModifiersQuerying().getAbility(game.getGameState(), cardToChoke);
+                                                                gameState.sendMessage("Destiny: " + GuiUtils.formatAsString(totalDestiny));
+                                                                gameState.sendMessage("Ability: " + GuiUtils.formatAsString(ability));
+                                                                if (totalDestiny > ability) {
+                                                                    gameState.sendMessage("Result: Succeeded");
+                                                                    action.appendEffect(
+                                                                            new LoseCardFromTableEffect(action, cardToChoke));
+                                                                    if (Filters.Amidala.accepts(game, cardToChoke)) {
+                                                                        gameState.sendMessage("She was alive! I felt it!");
+                                                                    }
+                                                                } else {
+                                                                    gameState.sendMessage("Result: Failed");
+                                                                }
+                                                            }
+                                                        });
+                                            }
+                                        }
+                                );
+                            }
+                        }
+                );
+                return Collections.singletonList(action);
+            }
+        }
+        return null;
+    }
+    @Override
     protected List<RequiredGameTextTriggerAction> getGameTextRequiredAfterTriggers(SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
         final String playerId = self.getOwner();
-
         // Check condition(s)
         if (TriggerConditions.lostBattle(game, effectResult, playerId)
                 && GameConditions.isInBattle(game, self)
                 && !game.isCasual()) {
-
             final RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
             action.setText("Draw destiny");
             // Perform result(s)
@@ -94,7 +164,6 @@ public class Card101_005 extends AbstractImperial {
                             if (totalDestiny != null) {
                                 gameState.sendMessage("Destiny: " + GuiUtils.formatAsString(totalDestiny));
                             }
-
                             if (totalDestiny != null && totalDestiny <= 4) {
                                 gameState.sendMessage("Result: Failed");
                             }
@@ -103,7 +172,6 @@ public class Card101_005 extends AbstractImperial {
                                     gameState.sendMessage("Result: Successful due to failed destiny draw");
                                 else
                                     gameState.sendMessage("Result: Successful");
-
                                 Filter targetFilter = Filters.and(Filters.Imperial, Filters.present(self));
                                 Set<TargetingReason> targetingReasons = new HashSet<>(Arrays.asList(TargetingReason.TO_BE_CHOKED, TargetingReason.TO_BE_LOST));
                                 if (GameConditions.canTarget(game, self, targetingReasons, targetFilter)) {
--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set101/light/Card101_002.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set101/light/Card101_002.java
@@ -1,20 +1,24 @@
 package com.gempukku.swccgo.cards.set101.light;
-
 import com.gempukku.swccgo.cards.AbstractRebel;
+import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.conditions.OnTableCondition;
+import com.gempukku.swccgo.cards.conditions.PilotingCondition;
 import com.gempukku.swccgo.common.*;
 import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.modifiers.ForfeitModifier;
-import com.gempukku.swccgo.logic.modifiers.MayNotDeployModifier;
-import com.gempukku.swccgo.logic.modifiers.Modifier;
-
+import com.gempukku.swccgo.game.state.GameState;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.conditions.AndCondition;
+import com.gempukku.swccgo.logic.conditions.Condition;
+import com.gempukku.swccgo.logic.conditions.NotCondition;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardToLocationFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.modifiers.*;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-
-
 /**
  * Set: Premium (Premiere Introductory Two Player Game)
  * Type: Character
@@ -29,23 +33,62 @@ public class Card101_002 extends AbstractRebel {
         addPersona(Persona.LUKE);
         addIcons(Icon.WARRIOR);
     }
-
     @Override
     protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        if (game.isCasual())
+            return Filters.any;
         return Filters.Deploys_on_Tatooine;
     }
-
     @Override
-    protected List<Modifier> getGameTextAlwaysOnModifiers(SwccgGame game, PhysicalCard self) {
+    protected List<Modifier> getGameTextAlwaysOnModifiers(final SwccgGame game, PhysicalCard self) {
+        Condition isCasualCondition = new Condition() {
+            @Override
+            public boolean isFulfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                return game.isCasual();
+            }
+        };
         List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new MayNotDeployModifier(self, new OnTableCondition(self, 2, Filters.and(Filters.opponents(self), Filters.unique, Filters.character))));
+        modifiers.add(new MayNotDeployModifier(self, new AndCondition(new NotCondition(isCasualCondition), new OnTableCondition(self, 2, Filters.and(Filters.opponents(self), Filters.unique, Filters.character)))));
+        modifiers.add(new PowerModifier(self, isCasualCondition, 3));
+        modifiers.add(new DestinyModifier(self, self, isCasualCondition, 8));
+        modifiers.add(new DeployCostModifier(self, self, isCasualCondition, -4));
+        modifiers.add(new IconModifier(self, isCasualCondition, Icon.PILOT));
         return modifiers;
     }
-
     @Override
-    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(final SwccgGame game, final PhysicalCard self) {
+        Condition isCasualCondition = new Condition() {
+            @Override
+            public boolean isFulfilled(GameState gameState, ModifiersQuerying modifiersQuerying) {
+                return game.isCasual();
+            }
+        };
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(new ForfeitModifier(self, Filters.and(Filters.your(self), Filters.warrior, Filters.atSameOrAdjacentSite(self)), 1));
+        modifiers.add(new ImmuneToAttritionModifier(self, isCasualCondition));
+        modifiers.add(new PowerModifier(self, Filters.hasPiloting(self), isCasualCondition, 5));
+        modifiers.add(new AddsBattleDestinyModifier(self, new AndCondition(isCasualCondition, new PilotingCondition(self)), 1));
         return modifiers;
+    }
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, final SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.LEGACY__HEAR_ME_BABY_HOLD_TOGETHER__UPLOAD_GRABBER;
+        // Check condition(s)
+        if (GameConditions.isDuringYourPhase(game, self, Phase.DEPLOY)
+                && (GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, Persona.LEIA)
+                || GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, Persona.HAN)
+                || GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, Persona.CHEWIE)
+                || GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, Persona.OBIWAN)
+                || GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, Persona.R2D2)
+                || GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, Persona.C3PO)
+            )){
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerId, gameTextSourceCardId, gameTextActionId);
+            action.setText("Deploy Luke's friends from Reserve Deck");
+            // Perform result(s)
+            action.appendEffect(
+                    new DeployCardToLocationFromReserveDeckEffect(action, Filters.or(Filters.Leia, Filters.Han, Filters.Chewie, Filters.ObiWan, Filters.R2D2, Filters.C3PO), Filters.here(self), true, true));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
