be552b2eceb6b176501835e0a11b82f744ca73d0
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2021-04-16 08:44:25 -0400

    Stolen Stormtrooper Blaster Rifle

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_026.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_026.java
@@ -1,9 +1,21 @@
 package com.gempukku.swccgo.cards.set501.light;
 
 import com.gempukku.swccgo.cards.AbstractCharacterWeapon;
-import com.gempukku.swccgo.common.Icon;
-import com.gempukku.swccgo.common.Keyword;
-import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.cards.conditions.HereCondition;
+import com.gempukku.swccgo.common.*;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.actions.FireWeaponAction;
+import com.gempukku.swccgo.logic.actions.FireWeaponActionBuilder;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.PowerModifier;
+import com.gempukku.swccgo.logic.modifiers.TotalWeaponDestinyModifier;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Set: Set 15
@@ -20,5 +32,37 @@ public class Card501_026 extends AbstractCharacterWeapon {
         addKeywords(Keyword.BLASTER_RIFLE);
         setTestingText("Stolen Stormtrooper Blaster Rifle");
         hideFromDeckBuilder();
+    }
+
+    @Override
+    protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        return Filters.and(Filters.your(self), Filters.warrior);
+    }
+
+    @Override
+    protected Filter getGameTextValidToUseWeaponFilter(final SwccgGame game, final PhysicalCard self) {
+        return Filters.warrior;
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new PowerModifier(self, Filters.hasAttached(self), new HereCondition(self, Filters.stormtrooper), 1));
+        modifiers.add(new TotalWeaponDestinyModifier(self, 1, Filters.character));
+        modifiers.add(new TotalWeaponDestinyModifier(self, 2, Filters.vehicle));
+        return modifiers;
+    }
+
+    @Override
+    protected List<FireWeaponAction> getGameTextFireWeaponActions(String playerId, SwccgGame game, PhysicalCard self, boolean forFree, int extraForceRequired, PhysicalCard sourceCard, boolean repeatedFiring, Filter targetedAsCharacter, Float defenseValueAsCharacter, Filter fireAtTargetFilter, boolean ignorePerAttackOrBattleLimit) {
+        FireWeaponActionBuilder actionBuilder = FireWeaponActionBuilder.startBuildPrep(playerId, game, sourceCard, self, forFree, extraForceRequired, repeatedFiring, targetedAsCharacter, defenseValueAsCharacter, fireAtTargetFilter, ignorePerAttackOrBattleLimit)
+                .targetForFree(Filters.or(Filters.character, targetedAsCharacter, Filters.creature, Filters.vehicle), TargetingReason.TO_BE_HIT).finishBuildPrep();
+        if (actionBuilder != null) {
+
+            // Build action using common utility
+            FireWeaponAction action = actionBuilder.buildBlasterRifleVAction(0);
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
