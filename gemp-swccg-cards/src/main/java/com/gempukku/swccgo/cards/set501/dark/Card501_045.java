6435c43198d885217f12daeca01187e1c108a9bd
jdipaolo1 <42298136+jdipaolo1@users.noreply.github.com>
2021-07-01 13:56:26 -0400

    Rule of Two

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_045.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_045.java
@@ -63,9 +63,9 @@ public class Card501_045 extends AbstractUsedOrStartingInterrupt {
         if (startingLocation != null && Filters.battleground.accepts(game, startingLocation)) {
 
             final PlayInterruptAction action = new PlayInterruptAction(game, self, CardSubtype.STARTING);
-            action.setText("Deploy a Kashyyyk location and Effects from Reserve Deck");
+            action.setText("Deploy a Rule of Two and Effects from Reserve Deck");
             // Allow response(s)
-            action.allowResponses("Deploy a Kashyyyk location and up to three Effects from Reserve Deck",
+            action.allowResponses("Deploy Rule of Two and up to two Effects from Reserve Deck",
                     new RespondablePlayCardEffect(action) {
                         @Override
                         protected void performActionResults(Action targetingAction) {
