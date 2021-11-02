cc034abcfee1b702652c6c9bdfbe55de76dc3c88
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2021-11-01 23:10:31 -0600

    CollectionStats

--- gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/ExpansionSet.java
+++ gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/ExpansionSet.java
@@ -51,7 +51,7 @@ public enum ExpansionSet {
     SET_15("Set 15", 215),
     SET_16("Set 16", 216),
 
-    DEMO_DECK("Demonstration Deck", 301),
+    DEMO_DECK("Virtual Premium Set", 301),
 
     DREAM_CARDS("Dream Cards", 401),
 
