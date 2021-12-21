e3880edde63506fb64d321ef1b3e13b6d1d4f63b
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2021-12-20 22:33:27 -0700

    Update Card9_053.java

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set9/light/Card9_053.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set9/light/Card9_053.java
@@ -1,6 +1,5 @@
 package com.gempukku.swccgo.cards.set9.light;
 
-import com.gempukku.swccgo.cards.AbstractUsedInterrupt;
 import com.gempukku.swccgo.cards.AbstractUsedOrLostInterrupt;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.common.*;
@@ -11,94 +10,83 @@ import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
-import com.gempukku.swccgo.logic.effects.AttachCardFromTableEffect;
 import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
-import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
 import com.gempukku.swccgo.logic.effects.choose.ModifyEachBattleDestinyDrawUntilEndOfBattleEffect;
-import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.effects.choose.TakeRandomStackedCardIntoHandEffect;
-import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.modifiers.ModifyGameTextType;
 import com.gempukku.swccgo.logic.timing.Action;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
-import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
+
 /**
- * Set: Set 14
+ * Set: Death Star II
  * Type: Interrupt
- * Subtype: Used
- * Title: Our Only Hope (V)
+ * Subtype: Used Or Lost
+ * Title: Our Only Hope
  */
-public class Card9_053 extends AbstractUsedInterrupt {
+public class Card9_053 extends AbstractUsedOrLostInterrupt {
     public Card9_053() {
-        super(Side.LIGHT, 4, Title.Our_Only_Hope, Uniqueness.UNIQUE);
+        super(Side.LIGHT, 4, "Our Only Hope", Uniqueness.UNIQUE);
         setLore("'The Emperor knew, as I did, if Anakin were to have any offspring, they would be a threat to him.'");
-        setGameText("Relocate Prophecy Of The Force to a site. OR If Like My Father Before Me on table, [upload] Yoda's Hut or a Death Star II site.");
-        addIcons(Icon.DEATH_STAR_II, Icon.VIRTUAL_SET_14);
-        setVirtualSuffix(true);
-        setTestingText("Our Only Hope (V) (ERRATA)");
+        setGameText("USED: If your Rebel of ability > 2 just won a battle or duel, randomly take into hand one card stacked on Insignificant Rebellion. (Immune to Sense.) LOST: If Luke is in battle (except with Vader), subtract 2 from each of opponent's battle destiny draws.");
+        addIcons(Icon.DEATH_STAR_II);
     }
 
     @Override
-    protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, final SwccgGame game, final PhysicalCard self) {
-        List<PlayInterruptAction> actions = new LinkedList<>();
+    protected List<PlayInterruptAction> getGameTextOptionalAfterActions(final String playerId, SwccgGame game, final EffectResult effectResult, final PhysicalCard self) {
+        Filter filter = Filters.and(Filters.your(self), Filters.Rebel, Filters.abilityMoreThan(2));
 
-        GameTextActionId gameTextActionId = GameTextActionId.OUR_ONLY_HOPE_V__UPLOAD_SITE;
+        // Check condition(s)
+        if (TriggerConditions.wonBattle(game, effectResult, filter)
+                || TriggerConditions.wonDuel(game, effectResult, filter)) {
+            final PhysicalCard insignificantRebellion = Filters.findFirstActive(game, self, Filters.and(Filters.Insignificant_Rebellion, Filters.hasStacked(Filters.any)));
+            if (insignificantRebellion != null) {
 
-        if (GameConditions.canSpot(game, self, Filters.title(Title.Like_My_Father_Before_Me))
-                && GameConditions.canTakeCardsIntoHandFromReserveDeck(game, playerId, self, gameTextActionId)) {
-            final PlayInterruptAction action = new PlayInterruptAction(game, self, gameTextActionId);
-            action.setText("Take site into hand from Reserve Deck");
+                final PlayInterruptAction action = new PlayInterruptAction(game, self, CardSubtype.USED);
+                action.setImmuneTo(Title.Sense);
+                action.setText("Take stacked card into hand");
+                // Allow response(s)
+                action.allowResponses("Randomly take a card stacked on " + GameUtils.getCardLink(insignificantRebellion) + " into hand",
+                        new RespondablePlayCardEffect(action) {
+                            @Override
+                            protected void performActionResults(Action targetingAction) {
+                                // Perform result(s)
+                                action.appendEffect(
+                                        new TakeRandomStackedCardIntoHandEffect(action, playerId, insignificantRebellion));
+                            }
+                        }
+                );
+                return Collections.singletonList(action);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self) {
+        final String opponent = game.getOpponent(playerId);
+
+        // Check condition(s)
+        if (GameConditions.isDuringBattleWithParticipant(game, Filters.Luke)
+                && !GameConditions.isDuringBattleWithParticipant(game, Filters.Vader)) {
+
+            final PlayInterruptAction action = new PlayInterruptAction(game, self, CardSubtype.LOST);
+            action.setText("Subtract 2 from battle destiny draws");
             // Allow response(s)
-            action.allowResponses("Take Yoda's Hut or a Death Star II site into hand from Reserve Deck",
+            action.allowResponses("Subtract 2 from each of opponent's battle destiny draws",
                     new RespondablePlayCardEffect(action) {
                         @Override
                         protected void performActionResults(Action targetingAction) {
                             // Perform result(s)
                             action.appendEffect(
-                                    new TakeCardIntoHandFromReserveDeckEffect(action, playerId, Filters.or(Filters.Yodas_Hut, Filters.Death_Star_II_site), true));
+                                    new ModifyEachBattleDestinyDrawUntilEndOfBattleEffect(action, opponent, -2));
                         }
                     }
             );
-            actions.add(action);
+            return Collections.singletonList(action);
         }
-
-        if (GameConditions.canSpot(game, self, Filters.Prophecy_Of_The_Force)) {
-            final PhysicalCard prophecyOfTheForce = Filters.findFirstActive(game, self, Filters.Prophecy_Of_The_Force);
-            boolean canRelocate = GameConditions.canSpot(game, self, Filters.canRelocateEffectTo(playerId, prophecyOfTheForce));
-            Collection<Modifier> modifiers = game.getModifiersQuerying().getModifiersAffecting(game.getGameState(), prophecyOfTheForce);
-            for (Modifier m : modifiers) {
-                if (m.getModifyGameTextType(game.getGameState(), game.getModifiersQuerying(), prophecyOfTheForce) == ModifyGameTextType.PROPHECY_OF_THE_FORCE__MAY_NOT_BE_RELOCATED)
-                    canRelocate = false;
-            }
-            if (canRelocate) {
-                final PlayInterruptAction action = new PlayInterruptAction(game, self);
-                action.setText("Relocate " + GameUtils.getCardLink(prophecyOfTheForce) + " to a site");
-                action.appendTargeting(new TargetCardOnTableEffect(action, playerId, "Choose site", Filters.canRelocateEffectTo(playerId, prophecyOfTheForce)) {
-                    @Override
-                    protected void cardTargeted(int targetGroupId, PhysicalCard site) {
-
-                        final PhysicalCard finalSite = action.getPrimaryTargetCard(targetGroupId);
-                        action.addAnimationGroup(prophecyOfTheForce);
-                        action.addAnimationGroup(finalSite);
-                        action.allowResponses(new RespondablePlayCardEffect(action) {
-                            @Override
-                            protected void performActionResults(Action targetingAction) {
-                                action.appendEffect(
-                                        new AttachCardFromTableEffect(action, prophecyOfTheForce, finalSite)
-                                );
-                            }
-                        });
-                    }
-                });
-
-                actions.add(action);
-            }
-        }
-        return actions;
+        return null;
     }
 }
