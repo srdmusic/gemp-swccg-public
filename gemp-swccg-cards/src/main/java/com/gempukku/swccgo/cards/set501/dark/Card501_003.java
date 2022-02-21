c54c6254f56ad4baa6b0a67979b1eca2891532ac
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2022-02-21 00:16:10 -0700

    Playtesting

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_003.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_003.java
@@ -21,7 +21,6 @@ import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromReserveDeckE
 import com.gempukku.swccgo.logic.effects.choose.TakeDestinyCardIntoHandEffect;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -32,44 +31,49 @@ import java.util.List;
  */
 public class Card501_003 extends AbstractNormalEffect {
     public Card501_003() {
-        super(Side.DARK, 5, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, "Shadows Of The Empire & Information Exchange", Uniqueness.UNIQUE);
+        super(Side.DARK, 0, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, "Shadows Of The Empire & Information Exchange", Uniqueness.UNIQUE);
         addComboCardTitles("Shadows Of The Empire", Title.Information_Exchange);
-        setGameText("Deploy on table. If [Reflections II] Emperor drawn for destiny, may take him into hand to cancel that destiny and cause a re-draw. If [Reflections II] Emperor on Coruscant, and you just deployed an information broker, may peek at one card (random selection) from opponent's hand. If your [Reflections II] objective on table: Once during your control phase, may use 1 Force to take Imperial Square or [Reflections II] Emperor into hand from Reserve Deck; reshuffle; and once per turn, if [Reflections II] Emperor on Coruscant, may draw top card of Force Pile (if during your turn and you occupy three battlegrounds, opponent also loses 1 Force). [Immune to Alter.]");
+        setGameText("If your [Reflections II] objective on table, deploy on table. If you just deployed an information broker, may peek at a card (random selection) from opponent's hand. Once per turn, if [Reflections II] Emperor on Coruscant, may draw top card of Force Pile (if during your turn and you occupy three battlegrounds, opponent also loses 1 Force). Once per turn, may use 1 Force to deploy Imperial Square or [Reflections II] Emperor from Reserve Deck; reshuffle. If [Reflections II] Emperor drawn for destiny, may take him into hand to cancel that destiny and cause a re-draw. [Immune to Alter.]");
         addIcons(Icon.REFLECTIONS_II, Icon.VIRTUAL_SET_18);
         addImmuneToCardTitle(Title.Alter);
         setTestingText("Shadows Of The Empire & Information Exchange");
     }
 
     @Override
+    protected boolean checkGameTextDeployRequirements(String playerId, SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        return Filters.canSpot(game, self, Filters.and(Filters.your(self), Icon.REFLECTIONS_II, Filters.Objective));
+    }
+
+    @Override
     protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
         List<TopLevelGameTextAction> actions = new LinkedList<TopLevelGameTextAction>();
 
-        GameTextActionId gameTextActionId = GameTextActionId.SHADOWS_OF_THE_EMPIRE_INFORMATION_EXCHANGE__UPLOAD_IMPERIAL_SQUARE_OR_EMPEROR;
+        GameTextActionId gameTextActionId = GameTextActionId.SHADOWS_OF_THE_EMPIRE_INFORMATION_EXCHANGE__DEPLOY_IMPERIAL_SQUARE_OR_EMPEROR;
         if (GameConditions.canSpot(game, self, Filters.and(Filters.your(self), Icon.REFLECTIONS_II, Filters.Objective))
-                && GameConditions.isOnceDuringYourPhase(game, self, playerId, gameTextSourceCardId, gameTextActionId, Phase.CONTROL)
+                && GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
                 && GameConditions.canUseForce(game, playerId, 1)
-                && GameConditions.canTakeCardsIntoHandFromReserveDeck(game, playerId, self, gameTextActionId)) {
+                && (GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, Title.Coruscant_Imperial_Square)
+                    || GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, Persona.SIDIOUS))) {
 
             final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
-            action.setText("Take Imperial Square or Emperor into hand from Reserve Deck");
-            action.setActionMsg("Take Imperial Square or [Reflections II] Emperor into hand from Reserve Deck");
+            action.setText("Deploy Imperial Square or Emperor");
+            action.setActionMsg("Deploy Imperial Square or [Reflections II] Emperor from Reserve Deck");
             // Update usage limit(s)
             action.appendUsage(
-                    new OncePerPhaseEffect(action));
+                    new OncePerTurnEffect(action));
             // Pay cost(s)
             action.appendCost(
                     new UseForceEffect(action, playerId, 1));
             // Perform result(s)
             action.appendEffect(
-                    new TakeCardIntoHandFromReserveDeckEffect(action, playerId, Filters.or(Filters.Coruscant_Imperial_Square, Filters.and(Icon.REFLECTIONS_II, Filters.Emperor)), true));
+                    new DeployCardFromReserveDeckEffect(action, Filters.or(Filters.Coruscant_Imperial_Square, Filters.and(Icon.REFLECTIONS_II, Filters.Emperor)), true));
             actions.add(action);
         }
 
 
         // Check conditions for drawing top card of force pile
         gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
-        if (GameConditions.canSpot(game, self, Filters.and(Filters.your(self), Icon.REFLECTIONS_II, Filters.Objective))
-                && GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
+        if (GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
                 && GameConditions.hasForcePile(game, playerId)
                 && GameConditions.canSpot(game, self, Filters.and(Icon.REFLECTIONS_II, Filters.Emperor, Filters.on(Title.Coruscant)))) {
 
@@ -123,8 +127,7 @@ public class Card501_003 extends AbstractNormalEffect {
 
         gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_3;
 
-        if (GameConditions.canSpot(game, self, Filters.and(Icon.REFLECTIONS_II, Filters.Emperor, Filters.on(Title.Coruscant)))
-                && TriggerConditions.justDeployed(game, effectResult, playerId, Filters.information_broker)
+        if (TriggerConditions.justDeployed(game, effectResult, playerId, Filters.information_broker)
                 && GameConditions.hasHand(game, opponent)) {
 
             final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
