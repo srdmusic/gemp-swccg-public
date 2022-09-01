b418b0c34002d32ab5042b90c4aae45d68432270
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2022-09-01 11:51:05 -0400

    Utinni (V) fix

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set219/light/Card219_046.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set219/light/Card219_046.java
@@ -71,7 +71,7 @@ public class Card219_046 extends AbstractUsedInterrupt {
                         protected void performActionResults(Action targetingAction) {
                             // Perform result(s)
                             action.appendEffect(
-                                    new DeployCardToTargetFromHandEffect(action, playerId, filter, Filters.battleLocation, false, true, -1));
+                                    new DeployCardToTargetFromHandEffect(action, playerId, filter, Filters.or(Filters.forceDrainLocation, Filters.battleLocation), false, true, -1));
                     }
                     }
             );
@@ -145,7 +145,7 @@ public class Card219_046 extends AbstractUsedInterrupt {
             );
             actions.add(action);
 
-            Filter yourMagneticSuctionTube = Filters.and(Filters.your(playerId), Filters.Magnetic_Suction_Tube);
+            Filter yourMagneticSuctionTube = Filters.and(Filters.your(playerId), Filters.Magnetic_Suction_Tube, Filters.attachedTo(Filters.driven));
 
             if(GameConditions.isDuringBattleWithParticipant(game, yourMagneticSuctionTube)){
                 final PlayInterruptAction action2 = new PlayInterruptAction(game, self);
