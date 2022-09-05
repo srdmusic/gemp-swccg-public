d2162138662a4da8555087ec6db77f273a9ef99c
jdipaolo1 <42298136+jdipaolo1@users.noreply.github.com>
2022-09-05 08:57:06 -0400

    Update jCards.js

--- gemp-swccg-async/src/main/web/js/gemp-016/jCards.js
+++ gemp-swccg-async/src/main/web/js/gemp-016/jCards.js
@@ -3542,6 +3542,7 @@ var fixedImages = {
 "501_60":"https://res.starwarsccg.org/cards/Hoth-Light/large/echobasetrooper.gif",
 "501_61":"https://res.starwarsccg.org/cards/SpecialEdition-Light/large/scurrier.gif",
 "501_62":"https://res.starwarsccg.org/cards/SpecialEdition-Light/large/antillesmaneuver.gif",
+"501_63":"https://res.starwarsccg.org/cards/Tatooine-Light/large/ajedisfocus.gif",
 "501_65": "https://res.starwarsccg.org/cards/legacy/VirtualBlock9-Light/large/republicatwar.gif",
 "501_65_BACK": "https://res.starwarsccg.org/cards/legacy/VirtualBlock9-Light/large/aggressivenegotiations.gif",
 "501_66": "https://res.starwarsccg.org/cards/ReflectionsIII-Light/large/coloclawfish.gif",
