4d40827fd8ea9322b668dc0a40a58181c1728bd5
Jared Greenwald <greenwaldjared@gmail.com>
2022-10-05 22:27:27 -0400

    update vehicle classes

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/AbstractCombatVehicle.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/AbstractCombatVehicle.java
@@ -1,6 +1,8 @@
 package com.gempukku.swccgo.cards;
 
 import com.gempukku.swccgo.common.CardSubtype;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filter;
@@ -59,8 +61,7 @@ public abstract class AbstractCombatVehicle extends AbstractVehicle {
      * @param uniqueness the uniqueness
      */
     protected AbstractCombatVehicle(Side side, float destiny, float deployCost, float power, Float armor, float maneuver, float landspeed, float forfeit, String title, Uniqueness uniqueness) {
-        super(side, destiny, deployCost, power, armor, maneuver, landspeed, forfeit, title, uniqueness);
-        setCardSubtype(CardSubtype.COMBAT);
+        this(side, destiny, deployCost, power, armor, maneuver, landspeed, forfeit, title, uniqueness, null, null);
     }
 
     /**
@@ -77,7 +78,26 @@ public abstract class AbstractCombatVehicle extends AbstractVehicle {
      * @param uniqueness the uniqueness
      */
     protected AbstractCombatVehicle(Side side, float destiny, float deployCost, float power, float armor, Float maneuver, float landspeed, float forfeit, String title, Uniqueness uniqueness) {
-        super(side, destiny, deployCost, power, armor, maneuver, landspeed, forfeit, title, uniqueness);
+        this(side, destiny, deployCost, power, armor, maneuver, landspeed, forfeit, title, uniqueness, null, null);
+    }
+
+    /**
+     * Creates a blueprint for a creature vehicle.
+     * @param side the side of the Force
+     * @param destiny the destiny value
+     * @param deployCost the deploy cost
+     * @param power the power value
+     * @param armor the armor value
+     * @param maneuver the maneuver value
+     * @param landspeed the landspeed value
+     * @param forfeit the forfeit value
+     * @param title the card title
+     * @param uniqueness the uniqueness
+     * @param expansionSet the expansionSet
+￼    * @param rarity the rarity
+     */
+    protected AbstractCombatVehicle(Side side, float destiny, float deployCost, float power, Float armor, Float maneuver, float landspeed, float forfeit, String title, Uniqueness uniqueness, ExpansionSet expansionSet, Rarity rarity) {
+        super(side, destiny, deployCost, power, armor, maneuver, landspeed, forfeit, title, uniqueness, expansionSet, rarity);
         setCardSubtype(CardSubtype.COMBAT);
     }
 
--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/AbstractShuttleVehicle.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/AbstractShuttleVehicle.java
@@ -1,6 +1,8 @@
 package com.gempukku.swccgo.cards;
 
 import com.gempukku.swccgo.common.CardSubtype;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filter;
@@ -41,7 +43,25 @@ public abstract class AbstractShuttleVehicle extends AbstractVehicle {
      * @param uniqueness the uniqueness
      */
     protected AbstractShuttleVehicle(Side side, float destiny, float deployCost, float power, float maneuver, Float landspeed, float forfeit, String title, Uniqueness uniqueness) {
-        super(side, destiny, deployCost, power, null, maneuver, landspeed, forfeit, title, uniqueness);
+        this(side, destiny, deployCost, power, maneuver, landspeed, forfeit, title, uniqueness, null, null);
+    }
+
+    /**
+     * Creates a blueprint for a shuttle vehicle.
+     * @param side the side of the Force
+     * @param destiny the destiny value
+     * @param deployCost the deploy cost
+     * @param power the power value
+     * @param maneuver the maneuver value
+     * @param landspeed the landspeed value
+     * @param forfeit the forfeit value
+     * @param title the card title
+     * @param uniqueness the uniqueness
+     * @param expansionSet the expansionSet
+￼    * @param rarity the rarity
+     */
+    protected AbstractShuttleVehicle(Side side, float destiny, float deployCost, float power, float maneuver, Float landspeed, float forfeit, String title, Uniqueness uniqueness, ExpansionSet expansionSet, Rarity rarity) {
+        super(side, destiny, deployCost, power, null, maneuver, landspeed, forfeit, title, uniqueness, expansionSet, rarity);
         setCardSubtype(CardSubtype.SHUTTLE);
     }
 
