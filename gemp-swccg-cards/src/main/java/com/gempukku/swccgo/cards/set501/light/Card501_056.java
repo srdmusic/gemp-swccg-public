e0754e3d884983564c5d5ab6a686ab16e4a6825c
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2022-10-27 23:10:47 -0600

    Tweak set

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_056.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_056.java
@@ -1,18 +1,26 @@
 package com.gempukku.swccgo.cards.set501.light;
 
 import com.gempukku.swccgo.cards.AbstractRebel;
-import com.gempukku.swccgo.cards.conditions.HereCondition;
 import com.gempukku.swccgo.cards.conditions.ImprisonedOnlyCondition;
-import com.gempukku.swccgo.common.*;
+import com.gempukku.swccgo.cards.conditions.OnCondition;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.Persona;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Species;
+import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.conditions.AndCondition;
-import com.gempukku.swccgo.logic.conditions.UnlessCondition;
+import com.gempukku.swccgo.logic.conditions.Condition;
+import com.gempukku.swccgo.logic.modifiers.DrawsBattleDestinyIfUnableToOtherwiseModifier;
 import com.gempukku.swccgo.logic.modifiers.ForceDrainModifier;
-import com.gempukku.swccgo.logic.modifiers.GenerateNoForceModifier;
-import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionLessThanModifier;
+import com.gempukku.swccgo.logic.modifiers.MayNotBeTransferredModifier;
+import com.gempukku.swccgo.logic.modifiers.MayNotHaveGameTextCanceledModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.MovesForFreeUsingLandspeedModifier;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -25,10 +33,10 @@ import java.util.List;
  */
 public class Card501_056 extends AbstractRebel {
     public Card501_056() {
-        super(Side.LIGHT, 1, 0, 4, 3, 6, Title.Prisoner_2187, Uniqueness.UNIQUE);
+        super(Side.LIGHT, 1, 6, 4, 3, 6, Title.Prisoner_2187, Uniqueness.UNIQUE);
         setVirtualSuffix(true);
         setLore("Princess Leia Organa. Alderaanian senator. Targeted by Vader for capture and interrogation. The Dark Lord of the Sith wanted her alive.");
-        setGameText("Deploys only if A Power Loss on table. Opponent generates no Force from same Death Star site (even if imprisoned) unless an Imperial here. Your Force drains are +1 where you have a stormtrooper. Immune to attrition < 4.");
+        setGameText("While on Death Star (even if imprisoned): Leia moves using landspeed for free, her game text may not be canceled, may not be transferred, and Leia draws one battle destiny if unable to otherwise. Your Force drains are +1 where you have a Rebel stormtrooper.");
         addPersona(Persona.LEIA);
         addIcons(Icon.PREMIUM, Icon.WARRIOR, Icon.VIRTUAL_SET_20);
         addKeywords(Keyword.SENATOR, Keyword.FEMALE);
@@ -37,30 +45,28 @@ public class Card501_056 extends AbstractRebel {
     }
 
     @Override
-    protected boolean checkGameTextDeployRequirements(String playerId, SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
-        return Filters.canSpot(game, self, Filters.A_Power_Loss);
-    }
-
-    @Override
     protected List<Modifier> getGameTextWhileInactiveInPlayModifiers(SwccgGame game, PhysicalCard self) {
-        String playerId = self.getOwner();
-        String opponent = game.getOpponent(playerId);
+        Condition onDeathStarAndImprisonedCondition = new AndCondition(new ImprisonedOnlyCondition(self), new OnCondition(self, Title.Death_Star));
 
-        List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new GenerateNoForceModifier(self, Filters.and(Filters.sameSite(self), Filters.Death_Star_site),
-                new AndCondition(new ImprisonedOnlyCondition(self), new UnlessCondition(new HereCondition(self, Filters.Imperial))), opponent));
+        List<Modifier> modifiers = new LinkedList<>();
+        modifiers.add(new MovesForFreeUsingLandspeedModifier(self, onDeathStarAndImprisonedCondition));
+        modifiers.add(new MayNotHaveGameTextCanceledModifier(self, onDeathStarAndImprisonedCondition));
+        modifiers.add(new MayNotBeTransferredModifier(self, onDeathStarAndImprisonedCondition));
+        modifiers.add(new DrawsBattleDestinyIfUnableToOtherwiseModifier(self, onDeathStarAndImprisonedCondition, 1));
         return modifiers;
     }
 
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
         String playerId = self.getOwner();
-        String opponent = game.getOpponent(playerId);
+        Condition onDeathStarCondition = new OnCondition(self, Title.Death_Star);
 
-        List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new GenerateNoForceModifier(self, Filters.and(Filters.sameSite(self), Filters.Death_Star_site), new UnlessCondition(new HereCondition(self, Filters.Imperial)), opponent));
-        modifiers.add(new ForceDrainModifier(self, Filters.sameLocationAs(self, Filters.and(Filters.your(self), Filters.stormtrooper)), 1, playerId));
-        modifiers.add(new ImmuneToAttritionLessThanModifier(self, 4));
+        List<Modifier> modifiers = new LinkedList<>();
+        modifiers.add(new MovesForFreeUsingLandspeedModifier(self, onDeathStarCondition));
+        modifiers.add(new MayNotHaveGameTextCanceledModifier(self, onDeathStarCondition));
+        modifiers.add(new MayNotBeTransferredModifier(self, onDeathStarCondition));
+        modifiers.add(new DrawsBattleDestinyIfUnableToOtherwiseModifier(self, onDeathStarCondition, 1));
+        modifiers.add(new ForceDrainModifier(self, Filters.sameLocationAs(self, Filters.and(Filters.Rebel, Filters.stormtrooper)), 1, playerId));
         return modifiers;
     }
 }
