044ed88f3b283704c571583c7cfcadd0257d34a2
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2022-12-20 23:32:24 -0700

    Corporal Janse

--- gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/Persona.java
+++ gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/Persona.java
@@ -65,6 +65,7 @@ public enum Persona implements Filterable {
     JAINA("Jaina"),
     JANGO_FETT("Jango"),
     JANNAH("Jannah"),
+    JANSE("Janse"),
     JAR_JAR("Jar Jar"),
     JENDON("Jendon"),
     JERUS("Jerus"),
