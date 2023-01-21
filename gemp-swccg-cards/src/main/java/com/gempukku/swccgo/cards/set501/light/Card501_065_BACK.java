28432fd1752ba6bfccd596249b99f8dfaed603ea
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-01-20 20:19:29 -0700

    Clones

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_065_BACK.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_065_BACK.java
@@ -18,10 +18,7 @@ import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
-import com.gempukku.swccgo.logic.actions.CancelCardActionBuilder;
-import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
 import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
-import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.decisions.MultipleChoiceAwaitingDecision;
 import com.gempukku.swccgo.logic.effects.FlipCardEffect;
 import com.gempukku.swccgo.logic.effects.LoseForceEffect;
@@ -38,11 +35,9 @@ import com.gempukku.swccgo.logic.modifiers.IconModifier;
 import com.gempukku.swccgo.logic.modifiers.ImmuneToTitleModifier;
 import com.gempukku.swccgo.logic.modifiers.MayNotDeployModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.timing.Effect;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -54,7 +49,7 @@ import java.util.List;
 public class Card501_065_BACK extends AbstractObjective {
     public Card501_065_BACK() {
         super(Side.LIGHT, 7, "Grievous Will Run And Hide", ExpansionSet.PLAYTESTING, Rarity.V);
-        setGameText("While this side up, your Force drains are +1 where you have a clone/Jedi pair. X = number of battlegrounds your [Clone Army] cards occupy. If you just initiated a battle: peek at the top card of your Reserve Deck or Used Pile (may take it into hand or place it on bottom of Reserve Deck), then if X > 1, retrieve a [Clone Army] card (or Cloning Cylinders) into hand, then if X > 2, your clone may make a regular move (for free) to the battle location. \n" +
+        setGameText("While this side up, your Force drains are +1 where you have a Jedi/clone pair. X = number of battlegrounds your [Clone Army] cards occupy. If you just initiated a battle: peek at the top card of your Reserve Deck or Used Pile (may take it into hand or place it on bottom of Reserve Deck), then if X > 1, retrieve a [Clone Army] card into hand, then if X > 2, your clone may make a regular move (for free) to the battle location. \n" +
                 "Flip this card if He Is A Coward at a battleground or Grievous alone at a battleground.");
         addIcons(Icon.CLONE_ARMY, Icon.EPISODE_I, Icon.VIRTUAL_SET_21);
         setTestingText("Grievous Will Run And Hide");
@@ -69,42 +64,6 @@ public class Card501_065_BACK extends AbstractObjective {
         modifiers.add(new ImmuneToTitleModifier(self, Filters.and(Filters.your(self), Icon.EPISODE_I, Filters.site), Title.No_Escape));
         modifiers.add(new ForceDrainModifier(self, Filters.and(Filters.occupiesWith(self.getOwner(), self, Filters.Jedi), Filters.occupiesWith(self.getOwner(), self, Filters.clone)), 1, self.getOwner()));
         return modifiers;
-    }
-
-    @Override
-    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
-        List<TopLevelGameTextAction> actions = new LinkedList<>();
-
-        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
-
-        // Check condition(s)
-        if (GameConditions.canTargetToCancel(game, self, Filters.You_Are_Beaten)) {
-
-            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
-            // Build action using common utility
-            CancelCardActionBuilder.buildCancelCardAction(action, Filters.You_Are_Beaten, Title.You_Are_Beaten);
-            action.appendCost(new LoseForceEffect(action, playerId, 1, true));
-            actions.add(action);
-        }
-
-        return actions;
-    }
-
-    @Override
-    protected List<OptionalGameTextTriggerAction> getGameTextOptionalBeforeTriggers(final String playerId, SwccgGame game, final Effect effect, final PhysicalCard self, int gameTextSourceCardId) {
-        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
-
-        // Check condition(s)
-        if (TriggerConditions.isPlayingCard(game, effect, Filters.You_Are_Beaten)
-                && GameConditions.canCancelCardBeingPlayed(game, self, effect)) {
-
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-            // Build action using common utility
-            CancelCardActionBuilder.buildCancelCardBeingPlayedAction(action, effect);
-            action.appendCost(new LoseForceEffect(action, playerId, 1, true));
-            return Collections.singletonList(action);
-        }
-        return null;
     }
 
     @Override
@@ -171,7 +130,7 @@ public class Card501_065_BACK extends AbstractObjective {
 
 
             if (x > 1) {
-                action.appendEffect(new RetrieveCardIntoHandEffect(action, playerId, Filters.or(Icon.CLONE_ARMY, Filters.title(Title.Cloning_Cylinders))));
+                action.appendEffect(new RetrieveCardIntoHandEffect(action, playerId, Filters.icon(Icon.CLONE_ARMY)));
 
                 final PhysicalCard location = Filters.findFirstFromTopLocationsOnTable(game, Filters.battleLocation);
                 final Filter cloneToMove = Filters.and(Filters.your(self), Filters.clone, Filters.movableAsRegularMove(playerId, true, 0, false, Filters.locationAndCardsAtLocation(Filters.battleLocation)));
--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_076.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_076.java
@@ -3,7 +3,6 @@ package com.gempukku.swccgo.cards.set501.light;
 import com.gempukku.swccgo.cards.AbstractNormalEffect;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.effects.usage.OncePerPhaseEffect;
-import com.gempukku.swccgo.cards.evaluators.CardMatchesEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
@@ -21,8 +20,9 @@ import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.effects.ShowCardOnScreenEffect;
 import com.gempukku.swccgo.logic.effects.choose.ChooseCardFromHandEffect;
 import com.gempukku.swccgo.logic.effects.choose.DeployCardFromReserveDeckSimultaneouslyWithCardEffect;
+import com.gempukku.swccgo.logic.modifiers.DeployCostModifier;
+import com.gempukku.swccgo.logic.modifiers.ImmunityToAttritionChangeModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.modifiers.PowerModifier;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -35,7 +35,7 @@ import java.util.List;
 public class Card501_076 extends AbstractNormalEffect {
     public Card501_076() {
         super(Side.LIGHT, 5, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, "Galactic Republic Navy", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setGameText("Deploy on table. Your [Clone Army] starships with a Jedi or clone aboard are power +1 (+2 if both). Once per turn, may reveal a [Clone Army] starship from hand to take a [Episode I] pilot character from Reserve Deck (or vice versa) and deploy both simultaneously; reshuffle. [Immune to Alter.]");
+        setGameText("Deploy on table. Unique (•) [Clone Army] capital starships are deploy -1 and immunity to attrition +1. Once per turn, may reveal a [Clone Army] starship from hand to take an [Episode I] pilot character from Reserve Deck (or vice versa) and deploy both simultaneously; reshuffle. [Immune to Alter.]");
         addIcons(Icon.EPISODE_I, Icon.VIRTUAL_SET_21);
         addImmuneToCardTitle(Title.Alter);
         setTestingText("Galactic Republic Navy");
@@ -44,7 +44,8 @@ public class Card501_076 extends AbstractNormalEffect {
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<>();
-        modifiers.add(new PowerModifier(self, Filters.and(Filters.your(self), Icon.CLONE_ARMY, Filters.starship, Filters.hasAboard(self, Filters.or(Filters.clone, Filters.Jedi))), new CardMatchesEvaluator(1, 2, Filters.and(Filters.hasAboard(self, Filters.clone), Filters.hasAboard(self, Filters.Jedi)))));
+        modifiers.add(new DeployCostModifier(self, Filters.and(Filters.unique, Icon.CLONE_ARMY, Filters.capital_starship), -1));
+        modifiers.add(new ImmunityToAttritionChangeModifier(self, Filters.and(Filters.unique, Icon.CLONE_ARMY, Filters.capital_starship), 1));
         return modifiers;
     }
 
