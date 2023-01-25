d1fe82dd51a8d440fd4a9962749872d321a9d17b
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-01-24 22:42:16 -0700

    Playtesting

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_008.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_008.java
@@ -14,12 +14,15 @@ import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.common.Variable;
 import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.decisions.IntegerAwaitingDecision;
+import com.gempukku.swccgo.logic.effects.PlayoutDecisionEffect;
 import com.gempukku.swccgo.logic.effects.choose.DeployCardFromReserveDeckEffect;
 
 import java.util.LinkedList;
@@ -35,7 +38,7 @@ public class Card501_008 extends AbstractNormalEffect {
         super(Side.DARK, 4, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, Title.Juri_Juice, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setVirtualSuffix(true);
         setLore("Popular beverage served in many cantinas and tapcafes. Has intoxicating effect on many species. Favorite drink of Kabe, Chadra-Fan thief of Mos Eisley.");
-        setGameText("Deploy on table. Once per turn, may deploy Baniss or Cantina from Reserve Deck; reshuffle. During your draw phase, may peek at the top X cards of Used Pile; take one into hand, where X = number of your aliens at Cantina. Immune to Blue Milk. [Immune to Alter.]");
+        setGameText("Deploy on table. Once per turn, may deploy Baniss or Cantina from Reserve Deck; reshuffle. During your draw phase, may peek at up to X cards from the top of your Used Pile; take one into hand, where X = number of your aliens at Cantina. Immune to Blue Milk. [Immune to Alter.]");
         addIcons(Icon.VIRTUAL_SET_21);
         addImmuneToCardTitle(Title.Alter);
         addImmuneToCardTitle(Title.Blue_Milk);
@@ -73,18 +76,32 @@ public class Card501_008 extends AbstractNormalEffect {
                 && GameConditions.canTarget(game, self, Filters.Cantina)) {
 
             int alienCount = Filters.countActive(game, self, Filters.and(Filters.your(self), Filters.alien, Filters.at(Filters.Cantina)));
-            if (alienCount > 0) {
+
+            final int maxValueOfX = (int) Math.min(game.getModifiersQuerying().getVariableValue(game.getGameState(), self, Variable.X,
+                            alienCount), game.getGameState().getUsedPile(playerId).size());
+
+            if (maxValueOfX > 0) {
 
                 final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
-                action.setText("Peek at top "+alienCount+" card"+ GameUtils.s(alienCount)+" of Used Pile");
-                action.setActionMsg("Peek at top "+alienCount+" card"+ GameUtils.s(alienCount)+" of Used Pile and take one into hand");
+                action.setText("Peek at up to "+alienCount+" card"+ GameUtils.s(alienCount)+" from Used Pile");
+                action.setActionMsg("Peek at up to "+alienCount+" card"+ GameUtils.s(alienCount)+" from the top of Used Pile and take one into hand");
 
                 // Update usage limit(s)
                 action.appendUsage(
                         new OncePerPhaseEffect(action));
                 // Perform result(s)
                 action.appendEffect(
-                        new PeekAtTopCardsOfUsedPileAndChooseCardsToTakeIntoHandEffect(action, playerId, alienCount, 1, 1));
+                        new PlayoutDecisionEffect(action, playerId,
+                                new IntegerAwaitingDecision("Choose number of cards to peek at", 1, maxValueOfX, maxValueOfX) {
+                                    @Override
+                                    public void decisionMade(final int num) {
+                                        action.appendEffect(
+                                                new PeekAtTopCardsOfUsedPileAndChooseCardsToTakeIntoHandEffect(action, playerId, num, 1, 1));
+                                    }
+                                }
+                        ));
+
+                // Perform result(s)
                 actions.add(action);
             }
         }
--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_010.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_010.java
@@ -1,9 +1,6 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractSite;
-import com.gempukku.swccgo.cards.evaluators.AddEvaluator;
-import com.gempukku.swccgo.cards.evaluators.DivideEvaluator;
-import com.gempukku.swccgo.cards.evaluators.HereEvaluator;
 import com.gempukku.swccgo.cards.evaluators.NegativeEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
@@ -11,12 +8,15 @@ import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.game.state.GameState;
 import com.gempukku.swccgo.logic.evaluators.Evaluator;
 import com.gempukku.swccgo.logic.modifiers.ForceGenerationModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.ModifiersQuerying;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -40,9 +40,35 @@ public class Card501_010 extends AbstractSite {
     }
 
     @Override
-    protected List<Modifier> getGameTextDarkSideWhileActiveModifiers(String playerOnDarkSideOfLocation, SwccgGame game, PhysicalCard self) {
-        Evaluator pairEvaluator = new AddEvaluator(new DivideEvaluator(new HereEvaluator(self, Filters.and(Filters.opponents(playerOnDarkSideOfLocation), Filters.alien)), 2, false),
-                new DivideEvaluator(new HereEvaluator(self, Filters.and(Filters.opponents(playerOnDarkSideOfLocation), Filters.smuggler)), 2, false));
+    protected List<Modifier> getGameTextDarkSideWhileActiveModifiers(String playerOnDarkSideOfLocation, SwccgGame game, final PhysicalCard self) {
+        final Filter alienFilter = Filters.and(Filters.opponents(playerOnDarkSideOfLocation), Filters.alien, Filters.here(self));
+        final Filter alternateFilter = Filters.and(Filters.opponents(playerOnDarkSideOfLocation), Filters.smuggler, Filters.here(self));
+
+        Evaluator pairEvaluator = new Evaluator() {
+            @Override
+            public float evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected) {
+                int opponentNonAlienAlternate = Filters.countActive(gameState.getGame(), self, Filters.and(Filters.not(alienFilter), alternateFilter));
+                int opponentNonAlternateAlien = Filters.countActive(gameState.getGame(), self, Filters.and(Filters.not(alternateFilter), alienFilter));
+                int opponentAlienAndAlternate = Filters.countActive(gameState.getGame(), self, Filters.and(alienFilter, alternateFilter));;
+
+                if (opponentNonAlienAlternate%2==1 && opponentAlienAndAlternate>0) {
+                    opponentNonAlienAlternate++;
+                    opponentAlienAndAlternate--;
+                }
+                if (opponentNonAlternateAlien%2==1 && opponentAlienAndAlternate>0) {
+                    opponentNonAlternateAlien++;
+                    opponentAlienAndAlternate--;
+                }
+
+                return opponentNonAlienAlternate/2 + opponentNonAlternateAlien/2 + opponentAlienAndAlternate/2;
+            }
+
+            @Override
+            public float evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected, PhysicalCard otherCard) {
+                return 0;
+            }
+        };
+
 
         List<Modifier> modifiers = new LinkedList<>();
         modifiers.add(new ForceGenerationModifier(self, new NegativeEvaluator(pairEvaluator), playerOnDarkSideOfLocation));
@@ -50,10 +76,34 @@ public class Card501_010 extends AbstractSite {
     }
 
     @Override
-    protected List<Modifier> getGameTextLightSideWhileActiveModifiers(String playerOnLightSideOfLocation, SwccgGame game, PhysicalCard self) {
-        Evaluator pairEvaluator = new AddEvaluator(new DivideEvaluator(new HereEvaluator(self, Filters.and(Filters.opponents(playerOnLightSideOfLocation), Filters.alien)), 2, false),
-                new DivideEvaluator(new HereEvaluator(self, Filters.and(Filters.opponents(playerOnLightSideOfLocation), Filters.sandtrooper)), 2, false));
+    protected List<Modifier> getGameTextLightSideWhileActiveModifiers(String playerOnLightSideOfLocation, SwccgGame game, final PhysicalCard self) {
+        final Filter alienFilter = Filters.and(Filters.opponents(playerOnLightSideOfLocation), Filters.alien, Filters.here(self));
+        final Filter alternateFilter = Filters.and(Filters.opponents(playerOnLightSideOfLocation), Filters.sandtrooper, Filters.here(self));
 
+        Evaluator pairEvaluator = new Evaluator() {
+            @Override
+            public float evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected) {
+                int opponentNonAlienAlternate = Filters.countActive(gameState.getGame(), self, Filters.and(Filters.not(alienFilter), alternateFilter));
+                int opponentNonAlternateAlien = Filters.countActive(gameState.getGame(), self, Filters.and(Filters.not(alternateFilter), alienFilter));
+                int opponentAlienAndAlternate = Filters.countActive(gameState.getGame(), self, Filters.and(alienFilter, alternateFilter));;
+
+                if (opponentNonAlienAlternate%2==1 && opponentAlienAndAlternate>0) {
+                    opponentNonAlienAlternate++;
+                    opponentAlienAndAlternate--;
+                }
+                if (opponentNonAlternateAlien%2==1 && opponentAlienAndAlternate>0) {
+                    opponentNonAlternateAlien++;
+                    opponentAlienAndAlternate--;
+                }
+
+                return opponentNonAlienAlternate/2 + opponentNonAlternateAlien/2 + opponentAlienAndAlternate/2;
+            }
+
+            @Override
+            public float evaluateExpression(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard cardAffected, PhysicalCard otherCard) {
+                return 0;
+            }
+        };
         List<Modifier> modifiers = new LinkedList<>();
         modifiers.add(new ForceGenerationModifier(self, new NegativeEvaluator(pairEvaluator), playerOnLightSideOfLocation));
         return modifiers;
