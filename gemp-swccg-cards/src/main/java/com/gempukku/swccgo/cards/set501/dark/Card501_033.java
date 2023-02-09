73318781b14a3b77b41abefb1d2cd7ff506670bd
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-02-08 19:57:03 -0700

    Playtesting

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_033.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_033.java
@@ -4,30 +4,24 @@ import com.gempukku.swccgo.cards.AbstractPermanentAboard;
 import com.gempukku.swccgo.cards.AbstractPermanentPilot;
 import com.gempukku.swccgo.cards.AbstractStarfighter;
 import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.usage.NumTimesPerTurnEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.ModelType;
-import com.gempukku.swccgo.common.Persona;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.GameUtils;
-import com.gempukku.swccgo.logic.TriggerConditions;
-import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
-import com.gempukku.swccgo.logic.effects.choose.DeployCardToTargetFromReserveDeckEffect;
-import com.gempukku.swccgo.logic.modifiers.LandsAsUnlimitedMoveModifier;
-import com.gempukku.swccgo.logic.modifiers.LandsForFreeModifier;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.effects.StackActionEffect;
+import com.gempukku.swccgo.logic.modifiers.MayDeployAsLandedToLocationModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.modifiers.TakesOffAsUnlimitedMoveModifier;
-import com.gempukku.swccgo.logic.modifiers.TakesOffForFreeModifier;
-import com.gempukku.swccgo.logic.timing.EffectResult;
+import com.gempukku.swccgo.logic.timing.Action;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -40,7 +34,7 @@ import java.util.List;
 public class Card501_033 extends AbstractStarfighter {
     public Card501_033() {
         super(Side.DARK, 4, 2, 3, null, 5, 4, 4, "CIS Transport Shuttle", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setGameText("May add 1 pilot and 2 passengers. Permanent pilot provides ability of 2. When deployed, may deploy a battle droid aboard from Reserve Deck; reshuffle. Takes off and lands as an unlimited move (for free).");
+        setGameText("May add 1 pilot and 2 passengers. Permanent pilot provides ability of 2. May deploy to exterior sites. Twice per turn,  may land or take off as an unlimited move for free.");
         addIcons(Icon.NAV_COMPUTER, Icon.EPISODE_I, Icon.SCOMP_LINK, Icon.SEPARATIST, Icon.VIRTUAL_SET_21);
         addIcon(Icon.PILOT, 1);
         addModelType(ModelType.SHEATHIPEDE_CLASS_SHUTTLE);
@@ -57,37 +51,54 @@ public class Card501_033 extends AbstractStarfighter {
     }
 
     @Override
-    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggersEvenIfUnpiloted(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
-        GameTextActionId gameTextActionId = GameTextActionId.CIS_TRANSPORT_SHUTTLE__DEPLOY_BATTLE_DROID_ABOARD;
+    protected List<Modifier> getGameTextAlwaysOnModifiers(SwccgGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<>();
+        modifiers.add(new MayDeployAsLandedToLocationModifier(self, Filters.exterior_site));
+        return modifiers;
+    }
 
-        // Check condition(s)
-        if (TriggerConditions.justDeployed(game, effectResult, self)
-                && GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, Persona.KYLO)) {
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(String playerId, SwccgGame game, PhysicalCard self, int gameTextSourceCardId) {
+        List<TopLevelGameTextAction> actions = new LinkedList<>();
 
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-            action.setText("Deploy a battle droid from Reserve Deck");
-            action.setActionMsg("Deploy a battle droid aboard " + GameUtils.getCardLink(self) + " from Reserve Deck");
-            // Perform result(s)
-            action.appendEffect(
-                    new DeployCardToTargetFromReserveDeckEffect(action, Filters.battle_droid, Filters.sameCardId(self), true));
-            return Collections.singletonList(action);
+        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+        if (GameConditions.isNumTimesPerTurn(game, self, playerId, 2, gameTextSourceCardId, gameTextActionId)) {
+
+            Action landAction = getLandAction(playerId, game, self, true, false, false, false, true, Filters.any);
+            if (landAction != null) {
+                final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerId, gameTextSourceCardId, gameTextActionId);
+                action.setText("Land as an unlimited move");
+                action.appendUsage(
+                        new NumTimesPerTurnEffect(action, 2));
+                action.appendEffect(
+                        new StackActionEffect(action, landAction));
+
+                actions.add(action);
+            }
         }
-        return null;
-    }
 
-    @Override
-    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<>();
-        modifiers.add(new LandsForFreeModifier(self));
-        modifiers.add(new LandsAsUnlimitedMoveModifier(self));
-        return modifiers;
+        return actions;
     }
-
     @Override
-    protected List<Modifier> getGameTextWhileActiveInPlayModifiersEvenIfUnpiloted(SwccgGame game, final PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<>();
-        modifiers.add(new TakesOffForFreeModifier(self));
-        modifiers.add(new TakesOffAsUnlimitedMoveModifier(self));
-        return modifiers;
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActionsEvenIfUnpiloted(String playerId, SwccgGame game, PhysicalCard self, int gameTextSourceCardId) {
+        List<TopLevelGameTextAction> actions = new LinkedList<>();
+
+        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+        if (GameConditions.isNumTimesPerTurn(game, self, playerId, 2, gameTextSourceCardId, gameTextActionId)
+                && Filters.pilotedForTakeOff.accepts(game, self)) {
+
+            Action takeOffAction = getTakeOffAction(playerId, game, self, true, false, false, false, true, Filters.any);
+            if (takeOffAction != null) {
+                final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerId, gameTextSourceCardId, gameTextActionId);
+                action.setText("Take off as an unlimited move");
+                action.appendUsage(
+                        new NumTimesPerTurnEffect(action, 2));
+                action.appendEffect(
+                        new StackActionEffect(action, takeOffAction));
+
+                actions.add(action);
+            }
+        }
+        return actions;
     }
 }
