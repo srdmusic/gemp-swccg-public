0b76c014119817f73486b17346ec5ff1092ceef1
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-02-17 12:43:30 -0700

    Update Card8_006.java

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set8/light/Card8_006.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set8/light/Card8_006.java
@@ -3,9 +3,11 @@ package com.gempukku.swccgo.cards.set8.light;
 import com.gempukku.swccgo.cards.AbstractRebel;
 import com.gempukku.swccgo.cards.effects.ChangePlayedInterruptSubtypeEffect;
 import com.gempukku.swccgo.common.CardSubtype;
+import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
 import com.gempukku.swccgo.common.Persona;
+import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filters;
@@ -33,9 +35,9 @@ import java.util.List;
  */
 public class Card8_006 extends AbstractRebel {
     public Card8_006() {
-        super(Side.LIGHT, 3, 2, 2, 2, 4, "Corporal Janse", Uniqueness.UNIQUE);
+        super(Side.LIGHT, 3, 2, 2, 2, 4, "Corporal Janse", Uniqueness.UNIQUE, ExpansionSet.ENDOR, Rarity.U);
         setLore("Sharpshooter from Ukio. Recently joined the Alliance. Former BlasTech employee who evaluated new weapon designs. Served as a scout on hunting expeditions.");
-        setGameText("When firing a character weapon, adds 1 to each weapon destiny draw. When Janse fires an A280 Sharpshooter Rifle at a character at an adjacent site and is using Sorry About The Mess, it is a Used Interrupt.");
+        setGameText("When firing a character weapon, adds 1 to each weapon destiny draw. If Janse just targeted with an A280 Sharpshooter Rifle at a character at an adjacent site and is using Sorry About The Mess, it is a Used Interrupt.");
         addIcons(Icon.ENDOR, Icon.WARRIOR);
         addKeywords(Keyword.SCOUT);
         addPersona(Persona.JANSE);
