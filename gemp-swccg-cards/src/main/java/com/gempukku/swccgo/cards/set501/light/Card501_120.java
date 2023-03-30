546b3bb52364cec4ce644a976fd483bb28ff8049
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-03-30 00:33:38 -0600

    Playtesting

---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_120.java
@@ -0,0 +1,81 @@
+package com.gempukku.swccgo.cards.set501.light;
+
+import com.gempukku.swccgo.cards.AbstractNormalEffect;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.conditions.PlayersTurnCondition;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.PlayCardOptionId;
+import com.gempukku.swccgo.common.PlayCardZoneOption;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
+import com.gempukku.swccgo.logic.effects.DrawOneCardFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainsMayNotBeCanceledModifier;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainsMayNotBeModifiedModifier;
+import com.gempukku.swccgo.logic.modifiers.InitiateBattlesForFreeModifier;
+import com.gempukku.swccgo.logic.modifiers.MayNotHaveForfeitValueIncreasedModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Set 21
+ * Type: Effect
+ * Title: Let's Keep A Little Optimism Here (V)
+ */
+public class Card501_120 extends AbstractNormalEffect {
+    public Card501_120() {
+        super(Side.LIGHT, 5, PlayCardZoneOption.ATTACHED, Title.Lets_Keep_A_Little_Optimism_Here, Uniqueness.UNIQUE, ExpansionSet.DEATH_STAR_II, Rarity.C);
+        setVirtualSuffix(true);
+        setLore("The heroes of the Rebellion know that where there is life, there is hope.");
+        setGameText("Deploy on a battleground. During opponent's turn, forfeit values may not be increased here. You initiate battles here for free. If you just initiated battle here, may draw top card of Reserve Deck. Opponent may not cancel or modify Force drains here.");
+        addIcons(Icon.DEATH_STAR_II, Icon.VIRTUAL_SET_21);
+        addKeywords(Keyword.DEPLOYS_ON_LOCATION);
+        setTestingText("Let's Keep A Little Optimism Here (V) (Effect)");
+    }
+
+    @Override
+    protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        return Filters.battleground;
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        String playerId = self.getOwner();
+        String opponent = game.getOpponent(playerId);
+
+        List<Modifier> modifiers = new LinkedList<>();
+        modifiers.add(new MayNotHaveForfeitValueIncreasedModifier(self, Filters.here(self), new PlayersTurnCondition(opponent)));
+        modifiers.add(new InitiateBattlesForFreeModifier(self, Filters.here(self), playerId));
+        modifiers.add(new ForceDrainsMayNotBeModifiedModifier(self, Filters.here(self), opponent, null));
+        modifiers.add(new ForceDrainsMayNotBeCanceledModifier(self, Filters.here(self), opponent, null));
+        return modifiers;
+    }
+
+    @Override
+    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(String playerId, SwccgGame game, EffectResult effectResult, PhysicalCard self, int gameTextSourceCardId) {
+        if(TriggerConditions.battleInitiatedAt(game, effectResult, playerId, Filters.here(self))
+                && GameConditions.hasReserveDeck(game, playerId))   {
+
+            OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId);
+            action.setText("Draw top card of Reserve Deck");
+
+            action.appendEffect(
+                    new DrawOneCardFromReserveDeckEffect(action, playerId));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+}
