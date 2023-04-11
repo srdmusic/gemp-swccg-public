024fa80f130703e4ca255f29d7783bb592f0438b
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-04-10 20:14:04 -0600

    Guards

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_044.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_044.java
@@ -38,7 +38,7 @@ public class Card501_044 extends AbstractImperial {
         setLore("Stormtrooper assigned to Commander Igar's honor guard. Accompanied Vader on Drazin's homeworld of Bespin. Shot C-3PO there.");
         setGameText("Deploys -1 to Endor. Your Lando and your [Endor] leaders may not be targeted by weapons here. While at a Cloud City site, modifiers to Force drains here may not be canceled. Once per game, may take Blasted Droid into hand from Reserve Deck; reshuffle.");
         addIcons(Icon.ENDOR, Icon.WARRIOR, Icon.VIRTUAL_SET_21);
-        addKeywords(Keyword.STORMTROOPER);
+        addKeywords(Keyword.STORMTROOPER, Keyword.GUARD);
         setTestingText("Corporal Drazin (V)");
     }
 
