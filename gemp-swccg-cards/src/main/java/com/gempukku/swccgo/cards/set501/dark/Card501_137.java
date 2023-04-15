b3973a4a32816d0e8cb6024b4366eefd98193c80
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-04-15 17:33:41 -0600

    Playtesting

---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_137.java
@@ -0,0 +1,71 @@
+package com.gempukku.swccgo.cards.set501.dark;
+
+import com.gempukku.swccgo.cards.AbstractSite;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.actions.MoveUsingLocationTextAction;
+import com.gempukku.swccgo.cards.conditions.ControlsCondition;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Phase;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.modifiers.DeployCostModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Set: Set 21
+ * Type: Location
+ * Subtype: Site
+ * Title: Coruscant: ISB Central Headquarters
+ */
+public class Card501_137 extends AbstractSite {
+    public Card501_137() {
+        super(Side.DARK, "Coruscant: ISB Central Headquarters", Title.Coruscant, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
+        setLocationDarkSideGameText("During your move phase, ISB Agents may move between here and any battleground site.");
+        setLocationLightSideGameText("If you control, ISB Agents are deploy +1.");
+        addIcon(Icon.DARK_FORCE, 2);
+        addIcon(Icon.LIGHT_FORCE, 1);
+        addIcons(Icon.EXTERIOR_SITE, Icon.PLANET, Icon.SCOMP_LINK, Icon.VIRTUAL_SET_21);
+        setTestingText("Coruscant: ISB Central Headquarters");
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextDarkSideTopLevelActions(String playerOnDarkSideOfLocation, SwccgGame game, PhysicalCard self, int gameTextSourceCardId) {
+        List<TopLevelGameTextAction> actions = new LinkedList<>();
+        Filter otherBattlegroundSites = Filters.and(Filters.other(self), Filters.battleground_site);
+
+        // Check condition(s)
+        if (GameConditions.isDuringYourPhase(game, playerOnDarkSideOfLocation, Phase.MOVE)
+                && GameConditions.canSpotLocation(game, otherBattlegroundSites)) {
+            if (GameConditions.canPerformMovementUsingLocationText(playerOnDarkSideOfLocation, game, Filters.ISB_agent, self, otherBattlegroundSites, false)) {
+                MoveUsingLocationTextAction action = new MoveUsingLocationTextAction(playerOnDarkSideOfLocation, game, self, gameTextSourceCardId, Filters.ISB_agent, self, otherBattlegroundSites, false);
+                action.setText("Move from here to other battleground site");
+                actions.add(action);
+            }
+            if (GameConditions.canPerformMovementUsingLocationText(playerOnDarkSideOfLocation, game, Filters.ISB_agent, otherBattlegroundSites, self, false)) {
+                MoveUsingLocationTextAction action = new MoveUsingLocationTextAction(playerOnDarkSideOfLocation, game, self, gameTextSourceCardId, Filters.ISB_agent, otherBattlegroundSites, self, false);
+                action.setText("Move from other battleground site to here");
+                actions.add(action);
+            }
+        }
+
+        return actions;
+    }
+
+    @Override
+    protected List<Modifier> getGameTextLightSideWhileActiveModifiers(String playerOnLightSideOfLocation, SwccgGame game, final PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<>();
+        modifiers.add(new DeployCostModifier(self, Filters.ISB_agent, new ControlsCondition(playerOnLightSideOfLocation, self), 1));
+        return modifiers;
+    }
+}
