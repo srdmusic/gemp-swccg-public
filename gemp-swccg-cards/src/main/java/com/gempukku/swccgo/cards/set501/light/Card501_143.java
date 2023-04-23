fc474e96acd1e90a1404df9bca00e68250185e6b
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-04-23 17:45:37 -0600

    Playtesting

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_143.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_143.java
@@ -3,8 +3,10 @@ package com.gempukku.swccgo.cards.set501.light;
 import com.gempukku.swccgo.cards.AbstractNormalEffect;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.conditions.AtCondition;
+import com.gempukku.swccgo.cards.effects.PreventEffectOnCardEffect;
 import com.gempukku.swccgo.cards.effects.usage.OncePerPhaseEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
 import com.gempukku.swccgo.common.Phase;
@@ -18,14 +20,18 @@ import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.GameUtils;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.conditions.Condition;
+import com.gempukku.swccgo.logic.effects.PlaceCardInUsedPileFromTableEffect;
 import com.gempukku.swccgo.logic.effects.RetrieveForceEffect;
 import com.gempukku.swccgo.logic.modifiers.DefenseValueModifier;
-import com.gempukku.swccgo.logic.modifiers.DestinyModifier;
 import com.gempukku.swccgo.logic.modifiers.ForceDrainModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.modifiers.PowerModifier;
+import com.gempukku.swccgo.logic.timing.EffectResult;
+import com.gempukku.swccgo.logic.timing.results.AboutToLeaveTableResult;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -41,7 +47,7 @@ public class Card501_143 extends AbstractNormalEffect {
         super(Side.LIGHT, 3, PlayCardZoneOption.ATTACHED, Title.Reflection, Uniqueness.UNIQUE, ExpansionSet.SET_21, Rarity.R);
         setLore("It was hard to imagine the enormous losses the Alliance suffered during the Battle of Hoth. Leia contemplated what she could do to help the Rebellion recover.");
         setVirtualSuffix(true);
-        setGameText("Deploy on [Cloud City] or [Endor] Leia. She is defense value +2. While at a battleground, Rebels of ability < 4 are destiny and power +1, Force drain +1 here and during opponent’s draw phase, if a battle was not initiated this turn, may retrieve 1 Force. [Immune to Alter.]");
+        setGameText("Deploy on [Cloud City] or [Endor] Leia. She is defense value +2. While at a battleground, Force drain +1 here and during opponent’s draw phase, you may retrieve 1 Force if a battle was not initiated this turn. If Leia about to be lost, may place this Effect in Used Pile. [Immune to Alter.]");
         addKeywords(Keyword.DEPLOYS_ON_CHARACTERS);
         addIcons(Icon.DAGOBAH, Icon.VIRTUAL_SET_21);
         addImmuneToCardTitle(Title.Alter);
@@ -60,12 +66,9 @@ public class Card501_143 extends AbstractNormalEffect {
 
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new DefenseValueModifier(self, Filters.hasAttached(self), 2));
-        Filter rebelsOfAbilityLessThanFour = Filters.and(Filters.Rebel, Filters.abilityLessThan(4));
+        List<Modifier> modifiers = new LinkedList<>();
         Condition atBattlegroundCondition = new AtCondition(self, Filters.battleground);
-        modifiers.add(new DestinyModifier(self, rebelsOfAbilityLessThanFour, atBattlegroundCondition, 1));
-        modifiers.add(new PowerModifier(self, rebelsOfAbilityLessThanFour, atBattlegroundCondition, 1));
+        modifiers.add(new DefenseValueModifier(self, Filters.hasAttached(self), 2));
         modifiers.add(new ForceDrainModifier(self, Filters.here(self), atBattlegroundCondition, 1, self.getOwner()));
         return modifiers;
     }
@@ -75,7 +78,8 @@ public class Card501_143 extends AbstractNormalEffect {
         // Check condition(s)
         if (GameConditions.isOnceDuringOpponentsPhase(game, self, playerId, gameTextSourceCardId, Phase.DRAW)
                 && GameConditions.isAtLocation(game, self, Filters.battleground)
-                && !GameConditions.hasInitiatedBattleThisTurn(game, game.getOpponent(playerId))) {
+                && !GameConditions.hasInitiatedBattleThisTurn(game, game.getOpponent(playerId))
+                && !GameConditions.hasInitiatedBattleThisTurn(game, playerId)) {
 
             final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId);
             action.setText("Retrieve a Force");
@@ -85,6 +89,27 @@ public class Card501_143 extends AbstractNormalEffect {
             // Perform result(s)
             action.appendEffect(
                     new RetrieveForceEffect(action, playerId, 1));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+
+        // Check condition(s)
+        if (TriggerConditions.isAboutToBeLostIncludingAllCardsSituation(game, effectResult, Filters.Leia)
+                || TriggerConditions.isAboutToBeForfeitedToLostPile(game, effectResult, Filters.Leia)) {
+
+            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
+            action.setText("Place in Used Pile");
+            action.setActionMsg("Place " + GameUtils.getCardLink(self) + " in Used Pile");
+            // Perform result(s)
+            action.appendEffect(
+                    new PreventEffectOnCardEffect(action, ((AboutToLeaveTableResult) effectResult).getPreventableCardEffect(), self, null));
+            action.appendEffect(
+                    new PlaceCardInUsedPileFromTableEffect(action, self));
             return Collections.singletonList(action);
         }
         return null;
