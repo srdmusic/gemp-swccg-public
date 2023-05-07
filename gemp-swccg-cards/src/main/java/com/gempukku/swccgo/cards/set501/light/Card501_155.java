b19dc68145f324e50d63b533e1eb58b4929116a6
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-05-07 17:29:58 -0600

    Playtesting

---
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_155.java
@@ -0,0 +1,97 @@
+package com.gempukku.swccgo.cards.set501.light;
+
+import com.gempukku.swccgo.cards.AbstractLostOrStartingInterrupt;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.common.CardSubtype;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
+import com.gempukku.swccgo.logic.effects.AddUntilEndOfTurnModifierEffect;
+import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
+import com.gempukku.swccgo.logic.effects.TakeCardFromVoidIntoHandEffect;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardsFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.modifiers.GenerateNoForceModifier;
+import com.gempukku.swccgo.logic.timing.Action;
+import com.gempukku.swccgo.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Set: Set 21
+ * Type: Interrupt
+ * Subtype: Lost or Starting
+ * Title: Anakin's Destiny
+ */
+public class Card501_155 extends AbstractLostOrStartingInterrupt {
+    public Card501_155() {
+        super(Side.LIGHT, 1, "Anakin's Destiny", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
+        setLore("'A vergence, you say?'");
+        setGameText("LOST: At start of opponent's turn, opponent does not activate Force from your [Death Star II] sites for remainder of turn. " +
+                "STARTING: If He Is The Chosen One on table, deploy His Destiny and two Effects that deploy for free and are always [Immune to Alter]. Place Interrupt in hand.");
+        addIcons(Icon.VIRTUAL_SET_21);
+        setTestingText("Anakin's Destiny");
+    }
+
+    @Override
+    protected List<PlayInterruptAction> getGameTextOptionalAfterActions(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self) {
+        if (TriggerConditions.isStartOfOpponentsTurn(game, effectResult, playerId)
+                && GameConditions.canTarget(game, self, Filters.and(Filters.your(self), Icon.DEATH_STAR_II, Filters.site))) {
+
+            final String opponent = game.getOpponent(playerId);
+
+            final PlayInterruptAction action = new PlayInterruptAction(game, self, CardSubtype.LOST);
+            action.setText("Cancel opponent's activation at your sites");
+
+            // Allow response(s)
+            action.allowResponses(
+                    new RespondablePlayCardEffect(action) {
+                        @Override
+                        protected void performActionResults(Action targetingAction) {
+                            // Perform result(s)
+                            action.appendEffect(
+                                    new AddUntilEndOfTurnModifierEffect(action, new GenerateNoForceModifier(self, Filters.and(Filters.your(self), Icon.DEATH_STAR_II, Filters.site), opponent), "Causes opponent to generate no Force from your [Death Star II] sites"));
+                        }
+                    }
+            );
+            return Collections.singletonList(action);
+        }
+
+        return null;
+    }
+
+    @Override
+    protected PlayInterruptAction getGameTextStartingAction(final String playerId, SwccgGame game, final PhysicalCard self) {
+        // Check condition(s)
+        if (GameConditions.canTarget(game, self, Filters.He_Is_The_Chosen_One)) {
+
+            final PlayInterruptAction action = new PlayInterruptAction(game, self);
+            action.setText("Deploy His Destiny and two Effects that deploy for free and are always immune to Alter.");
+            // Allow response(s)
+            action.allowResponses(
+                    new RespondablePlayCardEffect(action) {
+                        @Override
+                        protected void performActionResults(Action targetingAction) {
+                            // Perform result(s)
+                            action.appendEffect(
+                                    new DeployCardFromReserveDeckEffect(action, Filters.titleContains("His Destiny"),  true, false));
+                            action.appendEffect(
+                                    new DeployCardsFromReserveDeckEffect(action, Filters.and(Filters.Effect, Filters.deploysForFree, Filters.immune_to_Alter), 2, 2, true, false));
+                            action.appendEffect(
+                                    new TakeCardFromVoidIntoHandEffect(action, playerId, self));
+                        }
+                    }
+            );
+            return action;
+        }
+        return null;
+    }
+}
