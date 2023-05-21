e02d67cfcc2a45202a0dc9db0644f9232a216ec5
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2023-05-21 01:07:40 -0600

    Playtesting

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_135.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_135.java
@@ -3,7 +3,8 @@ package com.gempukku.swccgo.cards.set501.dark;
 import com.gempukku.swccgo.cards.AbstractLostInterrupt;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.effects.AddBattleDestinyEffect;
-import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
+import com.gempukku.swccgo.cards.effects.usage.OncePerBattleEffect;
+import com.gempukku.swccgo.common.DestinyType;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
@@ -11,68 +12,43 @@ import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
-import com.gempukku.swccgo.common.Zone;
 import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.AbstractActionProxy;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
-import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.actions.TriggerAction;
+import com.gempukku.swccgo.logic.decisions.MultipleChoiceAwaitingDecision;
+import com.gempukku.swccgo.logic.effects.AddUntilEndOfBattleActionProxyEffect;
 import com.gempukku.swccgo.logic.effects.AddUntilEndOfBattleModifierEffect;
 import com.gempukku.swccgo.logic.effects.AddUntilEndOfTurnModifierEffect;
+import com.gempukku.swccgo.logic.effects.ModifyTotalPowerUntilEndOfBattleEffect;
+import com.gempukku.swccgo.logic.effects.PlayoutDecisionEffect;
 import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
-import com.gempukku.swccgo.logic.effects.RetrieveCardIntoHandEffect;
 import com.gempukku.swccgo.logic.modifiers.AttritionModifier;
-import com.gempukku.swccgo.logic.modifiers.MayNotHaveTotalAbilityReducedModifier;
-import com.gempukku.swccgo.logic.modifiers.MayNotModifyTotalBattleDestinyModifier;
 import com.gempukku.swccgo.logic.modifiers.MayNotResetTotalBattleDestinyModifier;
-import com.gempukku.swccgo.logic.modifiers.ModifierFlag;
 import com.gempukku.swccgo.logic.timing.Action;
+import com.gempukku.swccgo.logic.timing.EffectResult;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Set: Set 21
  * Type: Interrupt
  * Subtype: Lost
- * Title: Wounded Wookiee (V)
+ * Title: Hutt Smooch (V)
  */
 public class Card501_135 extends AbstractLostInterrupt {
     public Card501_135() {
-        super(Side.DARK, 2, Title.Wounded_Wookiee, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
+        super(Side.DARK, 2, Title.Hutt_Smooch, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setVirtualSuffix(true);
-        setLore("When blaster fire from the barge's gun hit Chewie's skiff, his leg was injured by shrapnel. This setback distracted the Rebels, causing them to momentarily lose their advantage.");
-        setGameText("Once per game, if on top of your Lost Pile, may retrieve into hand. Add one battle destiny (if Chewie or a guard in that battle, attrition against you is -2); for remainder of turn, opponent may not modify or reset either player's total battle destiny (or reduce your total ability).");
+        setLore("'We have powerful friends. You're gonna regret this.' 'Rota go ma namatota.'");
+        setGameText("During battle, add one battle destiny. For remainder of turn, opponent may not reset your total battle destiny. If your gangster or guard in battle, choose: your total power is +2 OR If opponent draws a destiny to power or attrition this battle, attrition against you is -2.");
         addIcons(Icon.JABBAS_PALACE, Icon.VIRTUAL_SET_21);
-        setTestingText("Wounded Wookiee (V)");
-    }
-
-    @Override
-    public List<Action> getCardPilePhaseActions(final String playerId, final SwccgGame game, final PhysicalCard self) {
-        List<Action> actions = new LinkedList<>();
-        actions.addAll(super.getCardPilePhaseActions(playerId, game, self));
-
-
-        GameTextActionId gameTextActionId = GameTextActionId.WOUNDED_WOOKIEE_V__RETRIEVE;
-
-        if (self.getZone()== Zone.TOP_OF_LOST_PILE
-                && !game.getModifiersQuerying().hasFlagActive(game.getGameState(), ModifierFlag.LOST_PILE_FACE_DOWN, playerId)
-                && GameConditions.isOncePerGame(game, self, gameTextActionId)) {
-
-            TopLevelGameTextAction action = new TopLevelGameTextAction(self, self.getCardId(), gameTextActionId);
-            action.setText("Retrieve into hand");
-
-            action.appendUsage(
-                    new OncePerGameEffect(action));
-
-            action.appendEffect(
-                    new RetrieveCardIntoHandEffect(action, playerId, self));
-
-            actions.add(action);
-        }
-
-        return actions;
+        setTestingText("Hutt Smooch (V)");
     }
 
     @Override
@@ -93,20 +69,54 @@ public class Card501_135 extends AbstractLostInterrupt {
                             // Perform result(s)
                             action.appendEffect(
                                     new AddBattleDestinyEffect(action, 1));
-                            if (GameConditions.isDuringBattleWithParticipant(game, Filters.or(Filters.Chewie, Filters.guard))) {
+
+                            //may not reset total battle destiny
+                            action.appendEffect(new AddUntilEndOfTurnModifierEffect(action,
+                                    new MayNotResetTotalBattleDestinyModifier(self, playerId, opponent),"Prevents "+opponent+" from resetting your total battle destiny"));
+
+                            if (GameConditions.isDuringBattleWithParticipant(game, Filters.and(Filters.your(self), Filters.or(Filters.gangster, Filters.guard)))) {
+                                action.appendEffect(new PlayoutDecisionEffect(action, playerId, new MultipleChoiceAwaitingDecision("Choose", new String[]{"Add 2 to total power", "Subtract 2 from attrition if opponent draws a destiny to power or attrition this battle"}) {
+                                    @Override
+                                    protected void validDecisionMade(int index, String result) {
+                                        if (index==0) {
+                                            action.appendEffect(new ModifyTotalPowerUntilEndOfBattleEffect(action, 2, playerId, "Adds 2 to total power"));
+                                        } else {
+                                            final int permCardId = self.getPermanentCardId();
+                                            action.appendEffect(new AddUntilEndOfBattleActionProxyEffect(action, new AbstractActionProxy() {
+                                                @Override
+                                                public List<TriggerAction> getRequiredAfterTriggers(SwccgGame game, EffectResult effectResult) {
+                                                    final PhysicalCard self = game.findCardByPermanentId(permCardId);
+
+                                                    GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+
+                                                    if (TriggerConditions.isDestinyJustDrawnBy(game, effectResult, opponent)
+                                                            && (TriggerConditions.isDestinyDrawType(game, effectResult, DestinyType.DESTINY_TO_TOTAL_POWER)
+                                                            || TriggerConditions.isDestinyDrawType(game, effectResult, DestinyType.DESTINY_TO_ATTRITION))
+                                                            && GameConditions.isOncePerBattle(game, self, self.getCardId(), gameTextActionId)) {
+
+                                                        RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, self.getCardId(), gameTextActionId);
+                                                        action.setText("Subtract 2 from attrition");
+
+                                                        action.addAnimationGroup(self);
+
+                                                        action.appendUsage(
+                                                                new OncePerBattleEffect(action));
+                                                        action.appendEffect(new AddUntilEndOfBattleModifierEffect(action, new AttritionModifier(self, -2, playerId),
+                                                                "Subtracts 2 from attrition against " + playerId));
+
+                                                        return Collections.singletonList((TriggerAction) action);
+                                                    }
+
+                                                    return null;
+                                                }
+                                            }));
+                                        }
+                                    }
+                                }));
                                 action.appendEffect(new AddUntilEndOfBattleModifierEffect(action,
                                         new AttritionModifier(self, -2, playerId), "Subtracts 2 from attrition against you"));
                             }
 
-                            //modify total battle destiny
-                            action.appendEffect(new AddUntilEndOfTurnModifierEffect(action,
-                                    new MayNotModifyTotalBattleDestinyModifier(self, null, opponent),"Prevents "+opponent+" from modifying either player's total battle destiny"));
-                            //reset total battle destiny
-                            action.appendEffect(new AddUntilEndOfTurnModifierEffect(action,
-                                    new MayNotResetTotalBattleDestinyModifier(self, null, opponent),"Prevents "+opponent+" from resetting either player's total battle destiny"));
-                            //reduce total ability
-                            action.appendEffect(new AddUntilEndOfTurnModifierEffect(action,
-                                    new MayNotHaveTotalAbilityReducedModifier(self, Filters.any, playerId),"Prevents your total ability from being reduced"));
                         }
                     }
             );
--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_146.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_146.java
@@ -3,8 +3,7 @@ package com.gempukku.swccgo.cards.set501.light;
 import com.gempukku.swccgo.cards.AbstractLostInterrupt;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.effects.AddBattleDestinyEffect;
-import com.gempukku.swccgo.cards.effects.AddDuelDestinyEffect;
-import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
+import com.gempukku.swccgo.cards.effects.usage.OncePerBattleEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
@@ -12,27 +11,25 @@ import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
-import com.gempukku.swccgo.common.Zone;
 import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.AbstractActionProxy;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
-import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.actions.TriggerAction;
+import com.gempukku.swccgo.logic.effects.AddUntilEndOfBattleActionProxyEffect;
 import com.gempukku.swccgo.logic.effects.AddUntilEndOfBattleModifierEffect;
 import com.gempukku.swccgo.logic.effects.AddUntilEndOfTurnModifierEffect;
-import com.gempukku.swccgo.logic.effects.ModifyDuelTotalEffect;
 import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
-import com.gempukku.swccgo.logic.effects.RetrieveCardIntoHandEffect;
+import com.gempukku.swccgo.logic.modifiers.AttritionModifier;
 import com.gempukku.swccgo.logic.modifiers.MayNotModifyTotalBattleDestinyModifier;
-import com.gempukku.swccgo.logic.modifiers.MayNotResetTotalBattleDestinyModifier;
-import com.gempukku.swccgo.logic.modifiers.ModifierFlag;
 import com.gempukku.swccgo.logic.modifiers.TotalPowerModifier;
 import com.gempukku.swccgo.logic.timing.Action;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -46,36 +43,9 @@ public class Card501_146 extends AbstractLostInterrupt {
         super(Side.LIGHT, 2, Title.Courage_Of_A_Skywalker, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setVirtualSuffix(true);
         setLore("Despite being alone, trapped and desperately outmatched, Luke continued his battle with the Dark Lord of the Sith.");
-        setGameText("Once per game, if on top of Lost Pile, may retrieve into hand. Add one battle destiny or duel destiny (if a Skywalker in that battle or duel, your total power or duel total is +2); for remainder of turn, opponent may not modify or reset either player’s total battle destiny.");
+        setGameText("During battle, add one battle destiny. For remainder of turn, opponent may not modify their total battle destiny. If your Skywalker in that battle, your total power is +2 and, if opponent draws more than one battle destiny, attrition against you is -2.");
         addIcons(Icon.CLOUD_CITY, Icon.VIRTUAL_SET_21);
         setTestingText("Courage Of A Skywalker (V)");
-    }
-
-    @Override
-    public List<Action> getCardPilePhaseActions(final String playerId, final SwccgGame game, final PhysicalCard self) {
-        List<Action> actions = new LinkedList<>();
-        actions.addAll(super.getCardPilePhaseActions(playerId, game, self));
-
-
-        GameTextActionId gameTextActionId = GameTextActionId.COURAGE_OF_A_SKYWALKER_V__RETRIEVE;
-
-        if (self.getZone()== Zone.TOP_OF_LOST_PILE
-                && !game.getModifiersQuerying().hasFlagActive(game.getGameState(), ModifierFlag.LOST_PILE_FACE_DOWN, playerId)
-                && GameConditions.isOncePerGame(game, self, gameTextActionId)) {
-
-            TopLevelGameTextAction action = new TopLevelGameTextAction(self, self.getCardId(), gameTextActionId);
-            action.setText("Retrieve into hand");
-
-            action.appendUsage(
-                    new OncePerGameEffect(action));
-
-            action.appendEffect(
-                    new RetrieveCardIntoHandEffect(action, playerId, self));
-
-            actions.add(action);
-        }
-
-        return actions;
     }
 
     @Override
@@ -96,60 +66,50 @@ public class Card501_146 extends AbstractLostInterrupt {
                             // Perform result(s)
                             action.appendEffect(
                                     new AddBattleDestinyEffect(action, 1));
+                            //may not modify total battle destiny
+                            action.appendEffect(new AddUntilEndOfTurnModifierEffect(action,
+                                    new MayNotModifyTotalBattleDestinyModifier(self, opponent, opponent),"Prevents "+opponent+" from modifying their total battle destiny"));
+
                             if (GameConditions.isDuringBattleWithParticipant(game, Filters.Skywalker)) {
                                 action.appendEffect(new AddUntilEndOfBattleModifierEffect(action,
                                         new TotalPowerModifier(self, Filters.battleLocation, 2, playerId), "Adds 2 to your total power"));
+
+                                final int permCardId = self.getPermanentCardId();
+                                action.appendEffect(new AddUntilEndOfBattleActionProxyEffect(action, new AbstractActionProxy() {
+                                    @Override
+                                    public List<TriggerAction> getRequiredAfterTriggers(SwccgGame game, EffectResult effectResult) {
+                                        final PhysicalCard self = game.findCardByPermanentId(permCardId);
+
+                                        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+
+                                        if (TriggerConditions.isBattleDestinyDrawingJustCompletedForPlayer(game, effectResult, opponent)
+                                                && GameConditions.didDrawMoreThanBattleDestinies(game, opponent, 1)
+                                                && GameConditions.isOncePerBattle(game, self, self.getCardId(), gameTextActionId)) {
+
+                                            RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, self.getCardId(), gameTextActionId);
+                                            action.setText("Subtract 2 from attrition");
+
+                                            action.addAnimationGroup(self);
+
+                                            action.appendUsage(
+                                                    new OncePerBattleEffect(action));
+                                            action.appendEffect(new AddUntilEndOfBattleModifierEffect(action, new AttritionModifier(self, -2, playerId),
+                                                    "Subtracts 2 from attrition against " + playerId));
+
+                                            return Collections.singletonList((TriggerAction) action);
+                                        }
+
+                                        return null;
+                                    }
+                                }));
                             }
 
-                            //modify total battle destiny
-                            action.appendEffect(new AddUntilEndOfTurnModifierEffect(action,
-                                    new MayNotModifyTotalBattleDestinyModifier(self, null, opponent),"Prevents "+opponent+" from modifying either player's total battle destiny"));
-                            //reset total battle destiny
-                            action.appendEffect(new AddUntilEndOfTurnModifierEffect(action,
-                                    new MayNotResetTotalBattleDestinyModifier(self, null, opponent),"Prevents "+opponent+" from resetting either player's total battle destiny"));
                         }
                     }
             );
             return Collections.singletonList(action);
         }
 
-        return null;
-    }
-
-    @Override
-    protected List<PlayInterruptAction> getGameTextOptionalAfterActions(final String playerId, final SwccgGame game, EffectResult effectResult, final PhysicalCard self) {
-        // Check condition(s)
-        if (TriggerConditions.isDuelAddOrModifyDuelDestiniesStep(game, effectResult)) {
-
-            final String opponent = game.getOpponent(playerId);
-
-            final PlayInterruptAction action = new PlayInterruptAction(game, self);
-            action.setText("Add one duel destiny");
-            // Allow response(s)
-            action.allowResponses(
-                    new RespondablePlayCardEffect(action) {
-                        @Override
-                        protected void performActionResults(Action targetingAction) {
-                            // Perform result(s)
-                            action.appendEffect(
-                                    new AddDuelDestinyEffect(action, 1));
-
-                            if (GameConditions.isDuringDuelWithParticipant(game, Filters.Skywalker)) {
-                                action.appendEffect(
-                                        new ModifyDuelTotalEffect(action, 2, playerId, "Adds 2 to duel total"));
-                            }
-
-                            //modify total battle destiny
-                            action.appendEffect(new AddUntilEndOfTurnModifierEffect(action,
-                                    new MayNotModifyTotalBattleDestinyModifier(self, null, opponent),"Prevents "+opponent+" from modifying either player's total battle destiny"));
-                            //reset total battle destiny
-                            action.appendEffect(new AddUntilEndOfTurnModifierEffect(action,
-                                    new MayNotResetTotalBattleDestinyModifier(self, null, opponent),"Prevents "+opponent+" from resetting either player's total battle destiny"));
-                        }
-                    }
-            );
-            return Collections.singletonList(action);
-        }
         return null;
     }
 }
