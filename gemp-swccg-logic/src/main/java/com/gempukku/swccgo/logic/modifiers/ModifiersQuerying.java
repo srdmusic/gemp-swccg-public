f534d44189754792ef55cecfaa327abb26cf1666
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2023-09-21 10:15:28 -0400

    Set 22 Playtesting Updates

--- gemp-swccg-logic/src/main/java/com/gempukku/swccgo/logic/modifiers/ModifiersQuerying.java
+++ gemp-swccg-logic/src/main/java/com/gempukku/swccgo/logic/modifiers/ModifiersQuerying.java
@@ -1,8 +1,32 @@
 package com.gempukku.swccgo.logic.modifiers;
 
-import com.gempukku.swccgo.common.*;
+import com.gempukku.swccgo.common.Agenda;
+import com.gempukku.swccgo.common.CardState;
+import com.gempukku.swccgo.common.CardSubtype;
+import com.gempukku.swccgo.common.CardType;
+import com.gempukku.swccgo.common.Filterable;
+import com.gempukku.swccgo.common.GameTextActionId;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.InactiveReason;
+import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.Persona;
+import com.gempukku.swccgo.common.Phase;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Species;
+import com.gempukku.swccgo.common.Statistic;
+import com.gempukku.swccgo.common.TargetingReason;
+import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.common.Variable;
+import com.gempukku.swccgo.common.Zone;
 import com.gempukku.swccgo.filters.Filter;
-import com.gempukku.swccgo.game.*;
+import com.gempukku.swccgo.game.DeployAsCaptiveOption;
+import com.gempukku.swccgo.game.DeploymentOption;
+import com.gempukku.swccgo.game.DeploymentRestrictionsOption;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.PlayCardOption;
+import com.gempukku.swccgo.game.ReactActionOption;
+import com.gempukku.swccgo.game.SwccgBuiltInCardBlueprint;
+import com.gempukku.swccgo.game.SwccgCardBlueprint;
 import com.gempukku.swccgo.game.state.GameState;
 import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
 
@@ -4680,7 +4704,8 @@ public interface ModifiersQuerying {
     Collection<PhysicalCard> getCardsConsideredOutOfPlay(GameState gameState);
     Collection<PhysicalCard> getActiveCardsAffectedByModifier(GameState gameState, ModifierType modifierType);
     Collection<PhysicalCard> getCardsForPersonaChecking(String playerId);
-    boolean mayNotCancelBattle(GameState gameState, String playerId);
+
+    boolean mayNotCancelBattle(GameState gameState, String playerId, PhysicalCard battleLocation);
     boolean blownAwayForceLossMayNotBeReduced(GameState gameState);
     boolean onlyDeploysAdjacentToSpecificLocations(GameState gameState, PhysicalCard card);
     Filter getFilterForOnlyDeploysAdjacentToSpecificLocations(GameState gameState, PhysicalCard card);
