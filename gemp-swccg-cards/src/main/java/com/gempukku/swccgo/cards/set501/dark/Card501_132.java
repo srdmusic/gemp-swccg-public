37a6da60131c05bb5006dae768798aa8b13e9cea
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2023-12-08 18:18:26 -0500

    Set 23 - Zuckuss With Snare Rifle

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_132.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_132.java
@@ -1,10 +1,28 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractAlien;
+import com.gempukku.swccgo.cards.AbstractPermanentWeapon;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.Persona;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Species;
+import com.gempukku.swccgo.common.TargetingReason;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.actions.FireWeaponAction;
+import com.gempukku.swccgo.logic.actions.FireWeaponActionBuilder;
+import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Set: Playtesting
@@ -12,10 +30,46 @@ import com.gempukku.swccgo.common.Uniqueness;
  * Subtype: Alien
  * Title: Zuckuss With Snare Rifle
  */
-
 public class Card501_132 extends AbstractAlien {
     public Card501_132() {
         super(Side.DARK, 1, 4, 2, 4, 4, "Zuckuss With Snare Rifle", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        hideFromDeckBuilder();
+        setLore("");
+        setGameText("Adds 2 to power of anything he pilots. " +
+                "Once per game, may peek at top card of any Reserve Deck. " +
+                "Permanent weapon is Zuckuss' Snare Rifle (may target a character for 1 Force; draw destiny; " +
+                "target excluded from battle if destiny +1 > target's power).");
+        addPersona(Persona.ZUCKUSS);
+        addIcons(Icon.PILOT, Icon.WARRIOR, Icon.VIRTUAL_SET_23);
+        addKeywords(Keyword.BOUNTY_HUNTER, Keyword.SCOUT);
+        setSpecies(Species.GAND);
+        setTestingText("Zuckuss With Snare Rifle");
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new AddsPowerToPilotedBySelfModifier(self, 2));
+        return modifiers;
+    }
+
+    // Define "Zuckuss Snare Rifle" permanent weapon
+    @Override
+    protected AbstractPermanentWeapon getGameTextPermanentWeapon() {
+        AbstractPermanentWeapon permanentWeapon = new AbstractPermanentWeapon(Persona.ZUCKUSS_SNARE_RIFLE) {
+            @Override
+            public List<FireWeaponAction> getGameTextFireWeaponActions(String playerId, SwccgGame game, PhysicalCard self, boolean forFree, int extraForceRequired, PhysicalCard sourceCard, boolean repeatedFiring, Filter targetedAsCharacter, Float defenseValueAsCharacter, Filter fireAtTargetFilter, boolean ignorePerAttackOrBattleLimit) {
+                FireWeaponActionBuilder actionBuilder = FireWeaponActionBuilder.startBuildPrep(playerId, game, sourceCard, self, this, forFree, extraForceRequired, repeatedFiring, targetedAsCharacter, defenseValueAsCharacter, fireAtTargetFilter, ignorePerAttackOrBattleLimit)
+                        .targetUsingForce(Filters.and(Filters.or(Filters.character, targetedAsCharacter), Filters.hasPowerDefined), 1, TargetingReason.TO_BE_EXCLUDED_FROM_BATTLE).finishBuildPrep();
+                if (actionBuilder != null) {
+
+                    // Build action using common utility
+                    FireWeaponAction action = actionBuilder.buildFirePermanentWeaponZuckussSnareRifleAction();
+                    return Collections.singletonList(action);
+                }
+                return null;
+            }
+        };
+        permanentWeapon.addKeyword(Keyword.RIFLE);
+        return permanentWeapon;
     }
 }
