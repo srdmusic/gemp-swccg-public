ab6e45b35a68161210de02ed111ea4e0d1dee460
Jared Greenwald <greenwaldjared@gmail.com>
2024-02-13 01:19:31 -0500

    initial pass

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_141.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_141.java
@@ -1,10 +1,30 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractImperial;
+import com.gempukku.swccgo.cards.effects.usage.OncePerTurnEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.Persona;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
+import com.gempukku.swccgo.logic.effects.choose.DrawCardIntoHandFromUsedPileEffect;
+import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
+import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionLessThanModifier;
+import com.gempukku.swccgo.logic.modifiers.MayInitiateBattlesForFreeModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.TotalBattleDestinyModifier;
+import com.gempukku.swccgo.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Set: Playtesting
@@ -16,6 +36,39 @@ import com.gempukku.swccgo.common.Uniqueness;
 public class Card501_141 extends AbstractImperial {
     public Card501_141() {
         super(Side.DARK, 1, 4, 4, 4, 6, "Moff Gideon, Suited For Battle", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        hideFromDeckBuilder();
+        setLore("Leader");
+        setGameText("Adds 3 power to anything he pilots. If you just deployed Darksaber here, may take a card from Used Pile into hand; reshuffle. May initiate battle here for free. Your total battle destiny here is +1. Immune to attrition < 3.");
+        addIcons(Icon.PILOT, Icon.WARRIOR);
+        addPersona(Persona.GIDEON);
+        addKeywords(Keyword.LEADER, Keyword.MOFF);
+        setTestingText("Moff Gideon, Suited for Battle");
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        String playerId = self.getOwner();
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new AddsPowerToPilotedBySelfModifier(self, 3));
+        modifiers.add(new MayInitiateBattlesForFreeModifier(self, Filters.here(self), playerId));
+        modifiers.add(new TotalBattleDestinyModifier(self, Filters.here(self), 1, playerId));
+        modifiers.add(new ImmuneToAttritionLessThanModifier(self, 3));
+        return modifiers;
+    }
+
+    @Override
+    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
+        // Check condition(s)
+        if (TriggerConditions.justDeployedToLocation(game, effectResult, playerId, Filters.Darksaber, Filters.sameSite(self))) {
+            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId);
+            action.setText("Take a card from Used Pile into Hand");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerTurnEffect(action));
+            // Perform result(s)
+            action.appendEffect(
+                    new DrawCardIntoHandFromUsedPileEffect(action, playerId));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
