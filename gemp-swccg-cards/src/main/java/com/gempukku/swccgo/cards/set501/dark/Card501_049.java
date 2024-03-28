c94eb55487e4cd001a65197c2f8a6881a1de2833
Jared Greenwald <greenwaldjared@gmail.com>
2024-03-28 13:21:39 -0400

    updates from comments

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_049.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_049.java
@@ -36,7 +36,7 @@ public class Card501_049 extends AbstractImperial {
     public Card501_049() {
         super(Side.DARK, 3, 2, 2, 2, 3, "JS-1975", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Biker Scout Trooper. Imperial Remnant");
-        setGameText("Adds 2 to the power of anything he pilots (3 if a Speeder Bike).  Once per game, may take Moff Gideon into hand from Reserve Deck; reshuffle. While piloting a speeder bike, opponent's weapon destinies targeting your speeder bikes are -1 here.");
+        setGameText("Adds 2 to power of anything he pilots. Once per game, may take Gideon into hand from Reserve Deck; reshuffle. While piloting a speeder bike, opponent's weapon destinies targeting your speeder bikes are -1 (and Grogu is forfeit -2) here.");
         addKeywords(Keyword.BIKER_SCOUT, Keyword.IMPERIAL_REMNANT);
         addIcons(Icon.PILOT, Icon.WARRIOR, Icon.VIRTUAL_SET_23);
         setTestingText("JS-1975");
@@ -50,7 +50,7 @@ public class Card501_049 extends AbstractImperial {
         
         modifiers.add(new AddsPowerToPilotedBySelfModifier(self, 2));
         modifiers.add(new EachWeaponDestinyModifier(self, Filters.and(Filters.opponents(self), Filters.weapon), pilotingSpeederBike, Filters.any, -1, Filters.and(Filters.your(self), Filters.speeder_bike)));
-        modifiers.add(new ForfeitModifier(self, Filters.Grogu, pilotingSpeederBike, -2));
+        modifiers.add(new ForfeitModifier(self, Filters.and(Filters.Grogu, Filters.here(self)), pilotingSpeederBike, -2));
         return modifiers;
     }
 
