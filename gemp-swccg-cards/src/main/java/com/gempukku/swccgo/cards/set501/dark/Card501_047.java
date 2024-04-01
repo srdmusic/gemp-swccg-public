a13410d7befe67d25744daf971484cd5299b2f13
Jeremy DiPaolo <jeremydipaolo1@gmail.com>
2024-04-01 16:23:06 -0400

    Captain Sarkli (V)

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_047.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_047.java
@@ -1,12 +1,12 @@
-package com.gempukku.swccgo.cards.set9.dark;
+package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractImperial;
 import com.gempukku.swccgo.cards.GameConditions;
-import com.gempukku.swccgo.cards.conditions.ControlsCondition;
-import com.gempukku.swccgo.cards.conditions.OccupiesCondition;
+import com.gempukku.swccgo.cards.conditions.AtCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.Phase;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
@@ -17,11 +17,14 @@ import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.conditions.AndCondition;
+import com.gempukku.swccgo.logic.conditions.PhaseCondition;
 import com.gempukku.swccgo.logic.effects.FlipCardEffect;
 import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
-import com.gempukku.swccgo.logic.modifiers.GenerateNoForceModifier;
-import com.gempukku.swccgo.logic.modifiers.MayNotBeFlippedModifier;
+import com.gempukku.swccgo.logic.modifiers.BattleDamageLimitModifier;
+import com.gempukku.swccgo.logic.modifiers.CancelOpponentsForceDrainBonusesModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.SuspendsCardModifier;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
 import java.util.Collections;
@@ -29,29 +32,43 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Set: Death Star II
+ * Set: Playtesting
  * Type: Character
  * Subtype: Imperial
- * Title: Captain Sarkli
+ * Title: Captain Sarkli (V)
  */
-public class Card9_102 extends AbstractImperial {
-    public Card9_102() {
-        super(Side.DARK, 2, 2, 2, 2, 3, Title.Captain_Sarkli, Uniqueness.UNIQUE, ExpansionSet.DEATH_STAR_II, Rarity.R);
+public class Card501_047 extends AbstractImperial {
+    public Card501_047() {
+        super(Side.DARK, 2, 2, 2, 2, 3, Title.Captain_Sarkli, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.R);
+        setVirtualSuffix(true);
         setLore("Piett's nephew. Once granted audience with Emperor. On fast-track to promotion. Absolutely fearless spy.");
-        setGameText("Adds 2 to power of anything he pilots. While he controls opponent's non-battleground location, opponent generates no Force here. While he occupies opponent's Subjugated system, Liberation is flipped and Local Uprising may not be flipped.");
-        addIcons(Icon.DEATH_STAR_II, Icon.PILOT, Icon.WARRIOR);
+        setGameText("[P] 2. While at opponent’s War Room, " +
+                "opponent’s Force drain bonuses are canceled and battle damage here is limited to 2. " +
+                "While at a ‘probed’ system, opponent’s [SE] objective is suspended during your draw phase. " +
+                "Immune to Dodonna.");
+        addIcons(Icon.DEATH_STAR_II, Icon.PILOT, Icon.WARRIOR, Icon.VIRTUAL_SET_23);
         addKeywords(Keyword.SPY, Keyword.CAPTAIN);
+        addImmuneToCardTitle(Title.General_Dodonna);
+        setTestingText("Captain Sarkli (V)");
     }
 
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
-        String opponent = game.getOpponent(self.getOwner());
+        String playerId = self.getOwner();
+        String opponent = game.getOpponent(playerId);
 
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(new AddsPowerToPilotedBySelfModifier(self, 2));
-        modifiers.add(new GenerateNoForceModifier(self, Filters.here(self), new ControlsCondition(self,
-                Filters.and(Filters.opponents(self), Filters.non_battleground_location)), opponent));
-        modifiers.add(new MayNotBeFlippedModifier(self, new OccupiesCondition(self, Filters.Subjugated_system), Filters.Local_Uprising));
+
+        AtCondition atOpponentsWarRoomCondition = new AtCondition(self, Filters.and(Filters.opponents(playerId), Filters.war_room));
+        modifiers.add(new CancelOpponentsForceDrainBonusesModifier(self, atOpponentsWarRoomCondition));
+        modifiers.add(new BattleDamageLimitModifier(self, Filters.here(self), atOpponentsWarRoomCondition, 2, playerId));
+        modifiers.add(new BattleDamageLimitModifier(self, Filters.here(self), atOpponentsWarRoomCondition, 2, opponent));
+
+        AtCondition atProbedSystemCondition = new AtCondition(self, Filters.and(Filters.system, Filters.hasStacked(Filters.probeCard)));
+        PhaseCondition duringDrawPhaseCondition = new PhaseCondition(Phase.DRAW, playerId);
+        AndCondition condition = new AndCondition(atProbedSystemCondition, duringDrawPhaseCondition);
+        modifiers.add(new SuspendsCardModifier(self, Filters.and(Filters.opponents(playerId), Filters.icon(Icon.SPECIAL_EDITION), Filters.Objective), condition));
         return modifiers;
     }
 
