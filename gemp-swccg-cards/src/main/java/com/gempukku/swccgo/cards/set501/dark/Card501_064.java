6df06fe3b533e29ac3239402007a7e63be1ab9c5
Jared Greenwald <greenwaldjared@gmail.com>
2024-04-02 21:26:37 -0400

    update from comments

--- gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_064.java
+++ gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_064.java
@@ -10,25 +10,18 @@ import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
-import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
-import com.gempukku.swccgo.logic.effects.CancelGameTextUntilEndOfTurnEffect;
-import com.gempukku.swccgo.logic.effects.LookAtLostPileEffect;
-import com.gempukku.swccgo.logic.effects.PlaceCardsOutOfPlayFromOffTableEffect;
-import com.gempukku.swccgo.logic.effects.PutStackedCardInLostPileEffect;
 import com.gempukku.swccgo.logic.effects.RecirculateEffect;
 import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
-import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
-import com.gempukku.swccgo.logic.effects.choose.ChooseStackedCardEffect;
+import com.gempukku.swccgo.logic.effects.choose.ChooseCardFromLostPileEffect;
 import com.gempukku.swccgo.logic.effects.choose.DeployCardFromReserveDeckEffect;
-import com.gempukku.swccgo.logic.effects.choose.StackCardFromLostPileEffect;
+import com.gempukku.swccgo.logic.effects.choose.StackOneCardFromLostPileEffect;
 import com.gempukku.swccgo.logic.timing.Action;
 
-import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -79,36 +72,31 @@ public class Card501_064 extends AbstractUsedOrLostInterrupt {
 
         // Check Condition(s)
         if (GameConditions.isOncePerGame(game, self, gameTextActionId2)) {
-            if (GameConditions.canSpot(game, self, Filters.and(Filters.A_Useless_Gesture, Filters.icon(Icon.VIRTUAL_DEFENSIVE_SHIELD)))) {
+            if (GameConditions.canTarget(game, self, Filters.and(Filters.A_Useless_Gesture, Filters.icon(Icon.VIRTUAL_DEFENSIVE_SHIELD)))) {
                 final PhysicalCard auselessgesturev = Filters.findFirstActive(game, self, Filters.and(Filters.A_Useless_Gesture, Filters.icon(Icon.VIRTUAL_DEFENSIVE_SHIELD)));
-                if (! GameConditions.hasStackedCards(game, auselessgesturev)) {
-                    final PlayInterruptAction action = new PlayInterruptAction(game, self, gameTextActionId2, CardSubtype.LOST);
-                    action.setText("Stack a card on A Useless Gesture from opponent's Lost Pile.");
-                    // Update usage limit(s)
-                    action.appendUsage(
-                            new OncePerGameEffect(action));
-                    // Allow response(s)
-                    action.allowResponses(
-                            new RespondablePlayCardEffect(action) {
-                                @Override
-                                protected void performActionResults(Action targetingAction) {
-                                    // Perform result(s)
-                                    action.appendEffect(
-                                            new LookAtLostPileEffect(action, playerId, opponent) {
-                                                @Override
-                                                protected void cardsInCardPile(List<PhysicalCard> cardsInCardPile) {
-                                                    Collection<PhysicalCard> dockingBays = Filters.filter(cardsInCardPile, game, Filters.docking_bay);
-                                                    if (!dockingBays.isEmpty()) {
-                                                        action.appendEffect(
-                                                                new StackCardFromLostPileEffect(action, dockingBays));
-                                                    }
-                                                }
-                                            });
-                                }
+
+                final PlayInterruptAction action = new PlayInterruptAction(game, self, gameTextActionId2, CardSubtype.LOST);
+                action.setText("Stack a card on " + GameUtils.getFullName(auselessgesturev) + " from opponent's Lost Pile.");
+                // Update usage limit(s)
+                action.appendUsage(
+                        new OncePerGameEffect(action));
+                // Allow response(s)
+                action.allowResponses(
+                        new RespondablePlayCardEffect(action) {
+                            @Override
+                            protected void performActionResults(Action targetingAction) {
+                                // Perform result(s)
+                                action.appendEffect(new ChooseCardFromLostPileEffect(action, playerId, opponent, Filters.Interrupt) {
+                                        @Override
+                                        protected void cardSelected(SwccgGame game, PhysicalCard selectedCard) {
+                                            action.appendEffect(
+                                                new StackOneCardFromLostPileEffect(action, selectedCard, auselessgesturev, false, false, false));
+                                        }
+                                    });
                             }
-                    );
-                    actions.add(action);
-                }
+                        }
+                );
+                actions.add(action);
             }
         
             if (GameConditions.isDuringBattle(game)
