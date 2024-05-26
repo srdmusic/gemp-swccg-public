64e8abe0ad18599c4375744d38a221fcb839397e
hatou003 <hatou003@umn.edu>
2024-05-26 18:19:25 -0500

    Jabba's Last Chance

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_193.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_193.java
@@ -1,13 +1,36 @@
 package com.gempukku.swccgo.cards.set501.light;
 
 import com.gempukku.swccgo.cards.AbstractNormalEffect;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
+import com.gempukku.swccgo.cards.effects.usage.OncePerTurnEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.PlayCardOptionId;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.SpotOverride;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.effects.RespondableEffect;
+import com.gempukku.swccgo.logic.effects.TakeFirstBattleWeaponsSegmentActionEffect;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardToTargetFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionLessThanModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.timing.Action;
+import com.gempukku.swccgo.logic.timing.EffectResult;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /*
  * Set: Set 23
@@ -19,10 +42,80 @@ public class Card501_193 extends AbstractNormalEffect {
     public Card501_193() {
         super(Side.LIGHT, 4, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, "Jabba's Last Chance", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Jabba! This is your last chance. Free us or die.");
-        setGameText("If Han is frozen, deploy on table. Chewie, Lando, and Leia are immune to attrition < 4. Once per turn, may deploy a character weapon from Reserve Deck; reshuffle. Once per game, if opponent just initiated a battle, may take the first weapons segment action. [Immune to Alter.]");
+        setGameText("If Han is frozen, deploy on table. " +
+                "Chewie, Lando, and Leia are immune to attrition < 4. " +
+                "Once per turn, may deploy a character weapon from Reserve Deck; reshuffle. " +
+                "Once per game, if opponent just initiated a battle, may take the first weapons segment action. [Immune to Alter.]");
         addIcon(Icon.VIRTUAL_SET_23);
         addImmuneToCardTitle(Title.Alter);
         setTestingText("Jabba's Last Chance");
-        hideFromDeckBuilder();
+    }
+
+    @Override
+    protected boolean checkGameTextDeployRequirements(String playerId, SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        return Filters.canSpot(game, self, SpotOverride.INCLUDE_CAPTIVE, Filters.and(Filters.Han, Filters.frozenCaptive));
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+
+        modifiers.add(new ImmuneToAttritionLessThanModifier(self, Filters.or(Filters.Chewie, Filters.Lando, Filters.Leia), 4));
+        return modifiers;
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(String playerId, SwccgGame game, PhysicalCard self, int gameTextSourceCardId) {
+        List<TopLevelGameTextAction> actions = new LinkedList<>();
+        GameTextActionId gameTextActionId = GameTextActionId.JABBAS_LAST_CHANCE__DOWNLOAD_CHARACTER_WEAPON;
+
+        if (GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
+            && GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId)) {
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerId, gameTextSourceCardId, gameTextActionId);
+            action.setText("Deploy a character weapon from Reserve Deck");
+            action.setActionMsg("Deploy a character weapon from Reserve Deck");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerTurnEffect(action)
+            );
+            // Perform result(s)
+            action.appendEffect(
+                    new DeployCardToTargetFromReserveDeckEffect(action, Filters.character_weapon, Filters.and(Filters.your(self), Filters.character), true)
+            );
+            actions.add(action);
+        }
+
+        return actions;
+    }
+
+    @Override
+    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(String playerId, SwccgGame game, EffectResult effectResult, PhysicalCard self, int gameTextSourceCardId) {
+        final String opponent = game.getOpponent(playerId);
+
+        GameTextActionId gameTextActionId = GameTextActionId.JABBAS_LAST_CHANCE__TAKE_FIRST_WEAPONS_SEGMENT_ACTION;
+
+        // Check condition(s)
+        if (TriggerConditions.battleInitiated(game, effectResult, opponent)
+                && GameConditions.isOncePerGame(game, self, gameTextActionId)) {
+
+            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, playerId, gameTextSourceCardId, gameTextActionId);
+            action.setText("Take first weapons segment action");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerGameEffect(action));
+            // Allow response(s)
+            action.allowResponses("Take the first weapons segment action",
+                new RespondableEffect(action) {
+                        @Override
+                        protected void performActionResults(Action targetingAction) {
+                            // Perform result(s)
+                            action.appendEffect(
+                                    new TakeFirstBattleWeaponsSegmentActionEffect(action, playerId));
+                        }
+                    }
+            );
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
