84fa6f15b84683735b513e71210c1f1a1b9b58cb
Jared Greenwald <jared.greenwald@oracle.com>
2024-05-26 23:56:36 -0400

    initial pass

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_162.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_162.java
@@ -1,12 +1,33 @@
 package com.gempukku.swccgo.cards.set501.light;
 
 import com.gempukku.swccgo.cards.AbstractUsedInterrupt;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.PayRelocateBetweenLocationsCostEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Phase;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.GameUtils;
+import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
+import com.gempukku.swccgo.logic.effects.PutStackedCardInLostPileEffect;
+import com.gempukku.swccgo.logic.effects.RelocateBetweenLocationsEffect;
+import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
+import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
+import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
+import com.gempukku.swccgo.logic.effects.choose.ChooseStackedCardEffect;
+import com.gempukku.swccgo.logic.timing.Action;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 /*
  * Set: Set 23
@@ -22,6 +43,76 @@ public class Card501_162 extends AbstractUsedInterrupt {
         addIcons(Icon.A_NEW_HOPE, Icon.VIRTUAL_SET_23);
         setVirtualSuffix(true);
         setTestingText("Out Of Commission (V)");
-        hideFromDeckBuilder();
+    }
+
+    @Override
+    protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self) {
+        List<PlayInterruptAction> actions = new LinkedList<>();
+
+        if (GameConditions.canSpot(game, self, Filters.and(Filters.Thrawns_Art_Collection, Filters.hasStacked(Filters.any)))) {
+            final PlayInterruptAction action = new PlayInterruptAction(game, self);
+            action.setText("Choose an artwork card to be lost.");
+            action.setActionMsg("Choose a card stacked on Thrawn's Art Collection in owner's Lost Pile");
+            action.appendEffect(new ChooseStackedCardEffect(action, playerId, Filters.Thrawns_Art_Collection) {
+                @Override
+                protected void cardSelected(final PhysicalCard selectedCard) {
+                    action.appendEffect(
+                            new PutStackedCardInLostPileEffect(action, playerId, selectedCard, false));
+                }
+            });
+            actions.add(action);
+        }
+
+        // forfeit/hit logic here
+
+        Filter setOneObi = Filters.and(Filters.icon(Icon.VIRTUAL_SET_1), Filters.ObiWan);
+        GameTextActionId gameTextActionId = GameTextActionId.OUT_OF_COMMISSION__MOVE_OBI;
+
+        if (GameConditions.isOncePerGame(game, self, gameTextActionId)
+                && GameConditions.isDuringYourPhase(game, playerId, Phase.CONTROL)
+                && GameConditions.canSpot(game, self, setOneObi)
+                && GameConditions.hasForcePile(game, playerId)) {
+            final PlayInterruptAction action = new PlayInterruptAction(game, self);
+            action.setText("Relocate [Set 1] Obi-Wan to adjacent site");
+            // Choose target(s)
+            action.appendTargeting(
+                    new TargetCardOnTableEffect(action, playerId, "Choose Obi-Wan to relocate", setOneObi) {
+                        @Override
+                        protected void cardTargeted(final int targetGroupId, final PhysicalCard obiWanToRelocate) {
+                            Collection<PhysicalCard> otherSites = Filters.filterTopLocationsOnTable(game,
+                                    Filters.and(Filters.adjacentSite(obiWanToRelocate), Filters.locationCanBeRelocatedTo(obiWanToRelocate, 0)));
+                            action.appendTargeting(
+                                    new ChooseCardOnTableEffect(action, playerId, "Choose site to relocate " + GameUtils.getCardLink(obiWanToRelocate) + " to", Filters.in(otherSites)) {
+                                        @Override
+                                        protected void cardSelected(final PhysicalCard siteSelected) {
+                                            action.addAnimationGroup(obiWanToRelocate);
+                                            action.addAnimationGroup(siteSelected);
+                                            // Pay cost(s)
+                                            action.appendCost(
+                                                    new PayRelocateBetweenLocationsCostEffect(action, playerId, obiWanToRelocate, siteSelected, 1));
+                                            // Allow response(s)
+                                            action.allowResponses("Relocate " + GameUtils.getCardLink(obiWanToRelocate) + " to " + GameUtils.getCardLink(siteSelected),
+                                                    new RespondablePlayCardEffect(action) {
+                                                        @Override
+                                                        protected void performActionResults(Action targetingAction) {
+                                                            // Get the targeted card(s) from the action using the targetGroupId.
+                                                            // This needs to be done in case the target(s) were changed during the responses.
+                                                            PhysicalCard finalCharacter = action.getPrimaryTargetCard(targetGroupId);
+
+                                                            // Perform result(s)
+                                                            action.appendEffect(
+                                                                    new RelocateBetweenLocationsEffect(action, finalCharacter, siteSelected));
+                                                        }
+                                                    }
+                                            );
+                                        }
+                                    }
+                            );
+                        }
+                    }
+            );
+            actions.add(action);
+        }
+        return actions;
     }
 }
