de8fb77f8e415759e96db8e3c511ebe556c47da9
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2024-06-06 22:23:15 -0600

    Update Card501_038.java

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_038.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_038.java
@@ -42,7 +42,7 @@ import java.util.List;
 public class Card501_038 extends AbstractUsedOrLostInterrupt {
     public Card501_038() {
         super(Side.DARK, 4, "The Emperor's Hand", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setGameText("USED: Take Mara Jade, The Emperor's Hand into hand from Reserve Deck; reshuffle. OR If a battle just ended that your Mara won, opponent loses 1 Force and, if Emperor and your Mara on table, may relocate your Mara to a battleground site. LOST: Retrieve Mara Jade's Lightsaber.");
+        setGameText("USED: Take Mara Jade, The Emperor's Hand into hand from Reserve Deck; reshuffle. OR If a battle just ended that your [Premium] Mara won, opponent loses 1 Force and, if Emperor on table, may relocate your Mara to a battleground site. LOST: Retrieve Mara Jade's Lightsaber.");
         addIcons(Icon.VIRTUAL_SET_23);
         setTestingText("The Emperor's Hand");
     }
@@ -109,7 +109,7 @@ public class Card501_038 extends AbstractUsedOrLostInterrupt {
             if (battleState.isWinner(playerId)) {
                 Collection<PhysicalCard> yourParticipants = battleState.getCardsParticipatingWhenResultDetermined(playerId);
 
-                if (!Filters.filter(yourParticipants, game, Filters.Mara_Jade).isEmpty()) {
+                if (!Filters.filter(yourParticipants, game, Filters.and(Filters.Mara_Jade, Icon.PREMIUM)).isEmpty()) {
                     final String opponent = game.getOpponent(playerId);
 
                     // Action 1: force loss but no relocation
