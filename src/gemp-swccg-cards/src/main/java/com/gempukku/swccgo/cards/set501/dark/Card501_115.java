1d2cc0001fc61f00b71b7823f355018ba8fc6d0c
Jared Greenwald <jared.greenwald@oracle.com>
2024-07-05 08:32:32 -0400

    update

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_115.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_115.java
@@ -32,7 +32,7 @@ import java.util.List;
 public class Card501_115 extends AbstractNormalEffect {
     public Card501_115() {
         super(Side.DARK, 5, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, "Your Eyes Can Deceive You", Uniqueness.UNRESTRICTED, ExpansionSet.PLAYTESTING, Rarity.V);
-        setGameText("Deploy on table. If C-3PO at a non-battleground, once during your turn, may peek at the top two cards of Reserve Deck and take one into hand. While you occupy three battlegrounds, opponent may not cancel your Force drains at battlegrounds. [Immune to Alter]");
+        setGameText("Deploy on table. If C-3PO at a non-battleground, once during your turn, may peek at the top two cards of Reserve Deck and take one into hand. While you occupy two battlegrounds, opponent may not cancel your Force drains at battlegrounds. [Immune to Alter]");
         addImmuneToCardTitle(Title.Alter);
         addIcons(Icon.VIRTUAL_SET_23);
         setVirtualSuffix(true);
@@ -68,7 +68,7 @@ public class Card501_115 extends AbstractNormalEffect {
         String playerId = self.getOwner();
         String opponent = game.getOpponent(playerId);
         List<Modifier> modifiers = new LinkedList<>();
-        modifiers.add(new ForceDrainsMayNotBeCanceledModifier(self, Filters.battleground, new OccupiesCondition(playerId, 3, Filters.battleground), opponent, playerId));
+        modifiers.add(new ForceDrainsMayNotBeCanceledModifier(self, Filters.battleground, new OccupiesCondition(playerId, 2, Filters.battleground), opponent, playerId));
         return modifiers;
     }
 }
