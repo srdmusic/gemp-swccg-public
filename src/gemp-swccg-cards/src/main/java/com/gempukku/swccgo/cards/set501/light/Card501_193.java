da5e3995ff059ae9676639fb76b15880955b5868
Jared Greenwald <jared.greenwald@oracle.com>
2024-07-05 21:23:49 -0400

    updates

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_193.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_193.java
@@ -17,18 +17,12 @@ import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.TriggerConditions;
-import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
-import com.gempukku.swccgo.logic.effects.RespondableEffect;
-import com.gempukku.swccgo.logic.effects.TakeFirstBattleWeaponsSegmentActionEffect;
 import com.gempukku.swccgo.logic.effects.choose.DeployCardFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.effects.choose.ExchangeCardInHandWithCardInReserveDeckEffect;
 import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionLessThanModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.timing.Action;
-import com.gempukku.swccgo.logic.timing.EffectResult;
 
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -44,8 +38,8 @@ public class Card501_193 extends AbstractNormalEffect {
         setLore("Jabba! This is your last chance. Free us or die.");
         setGameText("If Han is frozen, deploy on table. " +
                 "Chewie, Lando, and Leia are immune to attrition < 4. " +
-                "Once per turn, may deploy a character weapon from Reserve Deck; reshuffle. " +
-                "Once per game, if opponent just initiated a battle, may take the first weapons segment action. [Immune to Alter.]");
+                "Once per turn, may deploy a unique character weapon from Reserve Deck; reshuffle. " +
+                "Once per game, if Artoo in battle, may exchange a card from hand with any card from Reserve Deck; reshuffle. [Immune to Alter.]");
         addIcon(Icon.VIRTUAL_SET_23);
         addImmuneToCardTitle(Title.Alter);
         setTestingText("Jabba's Last Chance");
@@ -72,50 +66,39 @@ public class Card501_193 extends AbstractNormalEffect {
         if (GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
             && GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId)) {
             final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerId, gameTextSourceCardId, gameTextActionId);
-            action.setText("Deploy a character weapon from Reserve Deck");
-            action.setActionMsg("Deploy a character weapon from Reserve Deck");
+            action.setText("Deploy a unique character weapon from Reserve Deck");
+            action.setActionMsg("Deploy a unique character weapon from Reserve Deck");
             // Update usage limit(s)
             action.appendUsage(
                     new OncePerTurnEffect(action)
             );
             // Perform result(s)
             action.appendEffect(
-                    new DeployCardFromReserveDeckEffect(action, Filters.character_weapon, true)
+                    new DeployCardFromReserveDeckEffect(action, Filters.and(Filters.unique, Filters.character_weapon), true)
             );
             actions.add(action);
         }
 
-        return actions;
-    }
-
-    @Override
-    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(String playerId, SwccgGame game, EffectResult effectResult, PhysicalCard self, int gameTextSourceCardId) {
-        final String opponent = game.getOpponent(playerId);
-
-        GameTextActionId gameTextActionId = GameTextActionId.JABBAS_LAST_CHANCE__TAKE_FIRST_WEAPONS_SEGMENT_ACTION;
+        GameTextActionId gameTextActionId2 = GameTextActionId.JABBAS_LAST_CHANCE__EXCHANGE_CARD_WITH_CARD_IN_RESERVE_DECK;
 
         // Check condition(s)
-        if (TriggerConditions.battleInitiated(game, effectResult, opponent)
-                && GameConditions.isOncePerGame(game, self, gameTextActionId)) {
+        if (GameConditions.isOncePerGame(game, self, gameTextActionId2)
+                && GameConditions.isDuringBattleWithParticipant(game, Filters.R2D2)
+                && GameConditions.hasHand(game, playerId)
+                && GameConditions.hasReserveDeck(game, playerId)) {
 
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, playerId, gameTextSourceCardId, gameTextActionId);
-            action.setText("Take first weapons segment action");
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerId, gameTextSourceCardId, gameTextActionId2);
+            action.setText("Exchange a card");
             // Update usage limit(s)
             action.appendUsage(
-                    new OncePerGameEffect(action));
-            // Allow response(s)
-            action.allowResponses("Take the first weapons segment action",
-                new RespondableEffect(action) {
-                        @Override
-                        protected void performActionResults(Action targetingAction) {
-                            // Perform result(s)
-                            action.appendEffect(
-                                    new TakeFirstBattleWeaponsSegmentActionEffect(action, playerId));
-                        }
-                    }
+                    new OncePerGameEffect(action)
             );
-            return Collections.singletonList(action);
+            // Perform result(s)
+            action.appendEffect(
+                    new ExchangeCardInHandWithCardInReserveDeckEffect(action, playerId, Filters.any, Filters.any, true)
+            );
+            actions.add(action);
         }
-        return null;
+        return actions;
     }
 }
