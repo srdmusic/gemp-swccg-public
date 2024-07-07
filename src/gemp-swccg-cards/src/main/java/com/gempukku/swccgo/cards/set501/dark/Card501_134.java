9a193e752ff8fc76b981a527e9260c7fe82cbef4
Jared Greenwald <jared.greenwald@oracle.com>
2024-07-06 22:07:46 -0400

    updates

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_134.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_134.java
@@ -2,7 +2,6 @@ package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractDevice;
 import com.gempukku.swccgo.cards.GameConditions;
-import com.gempukku.swccgo.cards.conditions.ControlsCondition;
 import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
@@ -11,7 +10,6 @@ import com.gempukku.swccgo.common.PlayCardOptionId;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
-import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
@@ -19,11 +17,9 @@ import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
-import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromForcePileEffect;
 import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromUsedPileEffect;
+import com.gempukku.swccgo.logic.modifiers.EachWeaponDestinyForWeaponFiredByModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.modifiers.ModifyGameTextModifier;
-import com.gempukku.swccgo.logic.modifiers.ModifyGameTextType;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
 import java.util.LinkedList;
@@ -39,26 +35,25 @@ public class Card501_134 extends AbstractDevice {
         super(Side.DARK, 6, PlayCardZoneOption.ATTACHED, "Electro-Rangefinder", Uniqueness.UNRESTRICTED, ExpansionSet.PLAYTESTING, Rarity.V);
         setVirtualSuffix(true);
         setLore("Long-range stereoscopic sighting device connected to the cannons of an Imperial walker. Calibrated to allow the AT-AT commander to accurately fire at distant targets.");
-        setGameText("Deploy on a [Hoth] or [Premium] AT-AT on Hoth. Once per game, when deployed, may search Force Pile or Used Pile and take any one card into hand; reshuffle. When this AT-AT fires an AT-AT Cannon with your [Hoth] Epic Event from a site you control, add one destiny to your total.");
+        setGameText("Deploy on an AT-AT; may immediately search your Used Pile and take an AT-AT Cannon or [Hoth] Epic Event into hand; reshuffle. Adds 2 to this AT-AT's weapon destiny draws (unless targeting a character).");
         addIcons(Icon.HOTH, Icon.VIRTUAL_SET_23);
         setTestingText("Electro-Rangefinder (V)");
     }
 
     @Override
     protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
-        return Filters.and(Filters.or(Icon.HOTH, Icon.PREMIUM), Filters.AT_AT, Filters.on(Title.Hoth));
+        return Filters.AT_AT;
     }
 
     @Override
     protected Filter getGameTextValidToUseDeviceFilter(final SwccgGame game, final PhysicalCard self) {
-        return Filters.and(Filters.or(Icon.HOTH, Icon.PREMIUM), Filters.AT_AT, Filters.on(Title.Hoth));
+        return Filters.AT_AT;
     }
 
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new ModifyGameTextModifier(self, Filters.Target_The_Main_Generator,
-                new ControlsCondition(self.getOwner(), Filters.here(self)), ModifyGameTextType.TARGET_THE_MAIN_GENERATOR__ADDS_ONE_DESTINY));
+        modifiers.add(new EachWeaponDestinyForWeaponFiredByModifier(self, 2, Filters.hasAttached(self), Filters.not(Filters.character)));
         return modifiers;
     }
 
@@ -69,31 +64,16 @@ public class Card501_134 extends AbstractDevice {
 
         // Check condition(s)
         if (TriggerConditions.justDeployed(game, effectResult, self)
-                && GameConditions.isOncePerGame(game, self, gameTextActionId)) {
-            
-            if (GameConditions.canTakeCardsIntoHandFromForcePile(game, playerId, self, gameTextActionId)) {
-                final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-                action.setText("Take card into hand from Force Pile");
-                action.setActionMsg("Take a card into hand from Force Pile");
-                action.appendUsage(
-                    new OncePerGameEffect(action));
-                // Perform result(s)
-                action.appendEffect(
-                        new TakeCardIntoHandFromForcePileEffect(action, playerId, true));
-                actions.add(action);
-            }
-
-            if (GameConditions.canTakeCardsIntoHandFromUsedPile(game, playerId, self, gameTextActionId)) {
-                final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-                action.setText("Take card into hand from Used Pile");
-                action.setActionMsg("Take a card into hand from Used Pile");
-                action.appendUsage(
-                    new OncePerGameEffect(action));
-                // Perform result(s)
-                action.appendEffect(
-                        new TakeCardIntoHandFromUsedPileEffect(action, playerId, true));
-                actions.add(action);
-            }
+                && GameConditions.canTakeCardsIntoHandFromUsedPile(game, playerId, self, gameTextActionId)) {
+            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
+            action.setText("Take card into hand from Used Pile");
+            action.setActionMsg("Take an AT-AT Cannon or [Hoth] Epic Event into hand from Used Pile");
+            action.appendUsage(
+                new OncePerGameEffect(action));
+            // Perform result(s)
+            action.appendEffect(
+                    new TakeCardIntoHandFromUsedPileEffect(action, playerId, Filters.or(Filters.AT_AT_Cannon, Filters.and(Filters.icon(Icon.HOTH), Filters.Epic_Event)), true));
+            actions.add(action);
         }
         return actions;
     }
