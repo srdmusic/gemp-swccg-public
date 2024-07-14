ab7ee5a911776b06c6dcfdade2e6d01e3afcc04b
hatou003 <hatou003@umn.edu>
2024-07-14 11:30:54 -0500

    Failure at the Cave (V) (ERRATA)

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_069.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_069.java
@@ -2,6 +2,8 @@ package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractDefensiveShield;
 import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.conditions.HasPlayedThisGameCondition;
+import com.gempukku.swccgo.cards.conditions.OnTableCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
@@ -14,6 +16,8 @@ import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.CancelCardActionBuilder;
 import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.conditions.NotCondition;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainsMayNotBeReducedModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 import com.gempukku.swccgo.logic.modifiers.ModifyGameTextModifier;
 import com.gempukku.swccgo.logic.modifiers.ModifyGameTextType;
@@ -34,7 +38,7 @@ public class Card501_069 extends AbstractDefensiveShield {
         super(Side.DARK, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, "Failure At The Cave", ExpansionSet.PLAYTESTING, Rarity.V);
         setVirtualSuffix(true);
         setLore("'That place is strong with the dark side of the Force. A domain of evil it is. In you must go.'");
-        setGameText("Plays on table. Stone Pile is canceled. While a Jedi Test on table, Projection Of A Skywalker is canceled. Domain Of Evil may not modify destiny draws more than once per battle (twice if Luke is apprentice).");
+        setGameText("Plays on table. Stone Pile is canceled. While a Jedi Test on table, opponent may not reduce your Force drains. Unless opponent has deployed a battleground, Domain Of Evil may not modify destiny draws more than once per battle.");
         addIcons(Icon.VIRTUAL_DEFENSIVE_SHIELD);
         setTestingText("Failure At The Cave (V) (ERRATA)");
     }
@@ -42,7 +46,10 @@ public class Card501_069 extends AbstractDefensiveShield {
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<>();
-        modifiers.add(new ModifyGameTextModifier(self, Filters.title(Title.Domain_Of_Evil), ModifyGameTextType.DOMAIN_OF_EVIL__LIMIT_USES_PER_BATTLE));
+        modifiers.add(new ModifyGameTextModifier(self, Filters.title(Title.Domain_Of_Evil),
+                new NotCondition(new HasPlayedThisGameCondition(self, Filters.battleground, game.getOpponent(self.getOwner()))),
+                ModifyGameTextType.DOMAIN_OF_EVIL__LIMIT_USES_PER_BATTLE));
+        modifiers.add(new ForceDrainsMayNotBeReducedModifier(self, new OnTableCondition(self, Filters.Jedi_Test), game.getOpponent(self.getOwner()), self.getOwner()));
         return modifiers;
     }
 
@@ -59,16 +66,6 @@ public class Card501_069 extends AbstractDefensiveShield {
             return Collections.singletonList(action);
         }
 
-        // Check condition(s)
-        if (TriggerConditions.isPlayingCard(game, effect, Filters.title(Title.Projection_Of_A_Skywalker))
-                && GameConditions.canSpot(game, self, Filters.Jedi_Test)
-                && GameConditions.canCancelCardBeingPlayed(game, self, effect)) {
-
-            RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
-            // Build action using common utility
-            CancelCardActionBuilder.buildCancelCardBeingPlayedAction(action, effect);
-            return Collections.singletonList(action);
-        }
         return null;
     }
 
@@ -82,17 +79,6 @@ public class Card501_069 extends AbstractDefensiveShield {
             final RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
             // Build action using common utility
             CancelCardActionBuilder.buildCancelCardAction(action, Filters.title(Title.Stone_Pile), Title.Stone_Pile);
-            actions.add(action);
-        }
-
-        // Check condition(s)
-        if (TriggerConditions.isTableChanged(game, effectResult)
-                && GameConditions.canTargetToCancel(game, self, Filters.title(Title.Projection_Of_A_Skywalker))
-                && GameConditions.canSpot(game, self, Filters.Jedi_Test)) {
-
-            final RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
-            // Build action using common utility
-            CancelCardActionBuilder.buildCancelCardAction(action, Filters.title(Title.Projection_Of_A_Skywalker), Title.Projection_Of_A_Skywalker);
             actions.add(action);
         }
 
