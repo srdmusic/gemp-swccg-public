753c3f7cc2a5e60d1f756bcc935d310c889615c4
Adam Fletcher <45436940+EchoBaseTrooper@users.noreply.github.com>
2024-07-22 20:36:21 -0600

    Update Card223_047.java

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set223/light/Card223_047.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set223/light/Card223_047.java
@@ -43,7 +43,7 @@ public class Card223_047 extends AbstractUsedInterrupt {
     public Card223_047() {
         super(Side.LIGHT, 3, Title.Scomp_Link_Access, Uniqueness.UNRESTRICTED, ExpansionSet.SET_23, Rarity.V);
         setVirtualSuffix(true);
-        setGameText("Once per game, 'BEEP-BOOP' (your [Grabber] card stacks a just played opponent's Interrupt; this does not count towards any [Grabber] card's once per game limit). OR Once per game, 'WHAAOW!' (lose your droid at a Scomp link to cancel a battle just initiated at same or related interior site).");
+        setGameText("Once per game, 'BEEP-BOOP' (your [Grabber] card stacks a just played opponent's Interrupt; this does not count towards any [Grabber] card's once per game limit). OR Once per game, 'WHAAAOW!' (lose your droid at a Scomp link to cancel a battle just initiated at same or related interior site).");
         addIcons(Icon.VIRTUAL_SET_23);
     }
 
@@ -97,7 +97,7 @@ public class Card223_047 extends AbstractUsedInterrupt {
                 && TriggerConditions.battleInitiatedAt(game, effectResult, Filters.and(Filters.interior_site, Filters.sameOrRelatedSiteAs(self, yourDroidAtAScompLink)))) {
 
             final PlayInterruptAction action = new PlayInterruptAction(game, self, gameTextActionId);
-            action.setText("WHAAOW! (Cancel battle)");
+            action.setText("WHAAAOW! (Cancel battle)");
 
             action.appendUsage(
                     new OncePerGameEffect(action));
@@ -107,7 +107,7 @@ public class Card223_047 extends AbstractUsedInterrupt {
                 protected void cardTargeted(int targetGroupId, PhysicalCard targetedCard) {
 
                     action.appendCost(
-                            new SendMessageEffect(action, GameUtils.getFullName(targetedCard) + ": WHAAOW!"));
+                            new SendMessageEffect(action, GameUtils.getFullName(targetedCard) + ": WHAAAOW!"));
                     action.appendCost(
                             new LoseCardFromTableEffect(action, targetedCard));
 
