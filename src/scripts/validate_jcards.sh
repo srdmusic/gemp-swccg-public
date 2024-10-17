ddcae497e7f3db1f32c026ad0af22d5c5d6eccec
Jared Greenwald <jared.greenwald@oracle.com>
2024-10-17 00:52:39 -0400

    add action for validating jcards on PR

---
+++ src/scripts/validate_jcards.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ls src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set*/*/Card*java | sort | \
+    sed -e 's/^.*.Card//' -e 's/\.java$//' | \
+    while read raw_id ; do
+        setNo=$(echo $raw_id | cut -d "_" -f 1)
+        id=$(echo $raw_id | cut -d "_" -f 2- | sed 's/^0*//')
+        grep -q "${setNo}_${id}" src/gemp-swccg-async/src/main/web/js/gemp-016/jCards.js || \
+            ls src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set${setNo}/*/Card${raw_id}.java
+    done
+
