e0475028f3b1e72109b6bb7c20f4fa12d024f921
Jared Greenwald <jared.greenwald@oracle.com>
2024-10-17 00:55:51 -0400

    fix CardImages file path

--- src/scripts/validate_jcards.sh
+++ src/scripts/validate_jcards.sh
@@ -5,7 +5,7 @@ ls src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set*/*/Card*java
     while read raw_id ; do
         setNo=$(echo $raw_id | cut -d "_" -f 1)
         id=$(echo $raw_id | cut -d "_" -f 2- | sed 's/^0*//')
-        grep -q "${setNo}_${id}" src/gemp-swccg-async/src/main/web/js/gemp-016/jCards.js || \
+        grep -q "${setNo}_${id}" src/gemp-swccg-async/src/main/web/js/gemp-016/cards/CardImages.js || \
             ls src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set${setNo}/*/Card${raw_id}.java
     done
 
