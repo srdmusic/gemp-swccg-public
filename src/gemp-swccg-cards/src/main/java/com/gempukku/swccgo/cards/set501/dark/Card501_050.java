fb6c4b137534612af2923024e05aeec9a43ae8b8
Jared Greenwald <jared.greenwald@oracle.com>
2024-10-24 16:53:06 -0400

    initial commit

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_050.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_050.java
@@ -1,7 +1,6 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractSystem;
-import com.gempukku.swccgo.cards.conditions.HereCondition;
 import com.gempukku.swccgo.cards.conditions.OccupiesCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
@@ -11,10 +10,11 @@ import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.conditions.UnlessCondition;
-import com.gempukku.swccgo.logic.modifiers.ForceDrainModifier;
+import com.gempukku.swccgo.logic.modifiers.ForfeitModifier;
 import com.gempukku.swccgo.logic.modifiers.IconModifier;
+import com.gempukku.swccgo.logic.modifiers.ImmuneToTitleModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.PowerModifier;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -29,12 +29,11 @@ public class Card501_050 extends AbstractSystem {
     public Card501_050() {
         super(Side.DARK, Title.Lothal, 6, ExpansionSet.PLAYTESTING, Rarity.V);
         setLocationDarkSideGameText("While you occupy, gains one [Dark Side] icon.");
-        setLocationLightSideGameText("While you occupy, gains one [Light Side] icon. Unless Ghost or Phantom piloted here, Force drain -1 here.");
+        setLocationLightSideGameText("Rebel starships here are power and forfeit +1 and immune to Gravity Shadow.");
         addIcon(Icon.DARK_FORCE, 2);
         addIcon(Icon.LIGHT_FORCE, 1);
         addIcons(Icon.PLANET, Icon.VIRTUAL_SET_19);
         setTestingText("Lothal (DS) (ERRATA)");
-        hideFromDeckBuilder();
     }
 
     @Override
@@ -47,8 +46,9 @@ public class Card501_050 extends AbstractSystem {
     @Override
     protected List<Modifier> getGameTextLightSideWhileActiveModifiers(String playerOnLightSideOfLocation, final SwccgGame game, final PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<>();
-        modifiers.add(new IconModifier(self, new OccupiesCondition(playerOnLightSideOfLocation, self), Icon.LIGHT_FORCE, 1));
-        modifiers.add(new ForceDrainModifier(self, new UnlessCondition(new HereCondition(self, Filters.and(Filters.piloted, Filters.or(Filters.Ghost, Filters.Phantom)))), -1, playerOnLightSideOfLocation));
+        modifiers.add(new PowerModifier(self, Filters.and(Filters.your(playerOnLightSideOfLocation), Filters.Rebel_starship, Filters.here(self)), 1));
+        modifiers.add(new ForfeitModifier(self, Filters.and(Filters.your(playerOnLightSideOfLocation), Filters.Rebel_starship, Filters.here(self)), 1));
+        modifiers.add(new ImmuneToTitleModifier(self, Filters.Rebel_starship, Title.Gravity_Shadow));
         return modifiers;
     }
 }
