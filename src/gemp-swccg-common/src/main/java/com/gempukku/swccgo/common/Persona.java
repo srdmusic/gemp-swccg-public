be2147f6fafbfc98e0acf38a0f473c2921bb52a4
Jared Greenwald <jared.greenwald@oracle.com>
2024-10-25 09:49:10 -0400

    remove captain from person def

--- src/gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/Persona.java
+++ src/gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/Persona.java
@@ -96,7 +96,7 @@ public enum Persona implements Filterable {
     LOTT("Lott"),
     LUKE("Luke"),
     MAAREK_STELE("Maarek Stele"),
-    MADAKOR("Captain Madakor"),
+    MADAKOR("Madakor"),
     MARA_JADE("Mara Jade"),
     MARA_SKYWALKER("Mara Skywalker"),
     MARGO("Margo"),
