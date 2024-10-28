913acf9a3f9c084c80e4c1a997f3858af5845e3a
Jared Greenwald <jared.greenwald@oracle.com>
2024-10-27 22:32:36 -0400

    new card

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_174.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_174.java
@@ -1,6 +1,9 @@
 package com.gempukku.swccgo.cards.set501.light;
 
 import com.gempukku.swccgo.cards.AbstractCapitalStarship;
+import com.gempukku.swccgo.cards.AbstractPermanentAboard;
+import com.gempukku.swccgo.cards.AbstractPermanentPilot;
+import com.gempukku.swccgo.cards.conditions.AtCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
@@ -8,6 +11,20 @@ import com.gempukku.swccgo.common.ModelType;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.conditions.Condition;
+import com.gempukku.swccgo.logic.modifiers.DeployCostAboardModifier;
+import com.gempukku.swccgo.logic.modifiers.DeployCostForSimultaneouslyDeployingPilotModifier;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainsMayNotBeCanceledModifier;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainsMayNotBeModifiedModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /*
  * Set: Playtesting
@@ -20,7 +37,7 @@ public class Card501_174 extends AbstractCapitalStarship {
     public Card501_174() {
         super(Side.LIGHT, 2, 4, 5, 4, null, 3, 7, "Liberator", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Phoenix Squadron.");
-        setGameText("May add 3 pilots and 4 passengers. Permanent pilot provides ability of 2. Phoenix Squadron pilots deploy -1 aboard. While at opponent’s battleground system, Force drains may not be modified or canceled here.");
+        setGameText("May add 3 pilots and 4 passengers. Permanent pilot provides ability of 2. Phoenix Squadron pilots deploy -1 aboard. While at opponent's battleground system, Force drains may not be modified or canceled here.");
         addIcons(Icon.NAV_COMPUTER, Icon.SCOMP_LINK, Icon.VIRTUAL_SET_24);
         addIcon(Icon.PILOT, 1);
         addKeywords(Keyword.PHOENIX_SQUADRON);
@@ -28,6 +45,39 @@ public class Card501_174 extends AbstractCapitalStarship {
         setPilotCapacity(3);
         setPassengerCapacity(4);
         setTestingText("Liberator");
-        hideFromDeckBuilder();
+    }
+
+    @Override
+    protected List<? extends AbstractPermanentAboard> getGameTextPermanentsAboard() {
+        List<AbstractPermanentAboard> permanentsAboard = new ArrayList<>();
+        permanentsAboard.add(new AbstractPermanentPilot(2) {});
+        return permanentsAboard;
+    }
+
+    public final Filter phoenixSquadPilots = Filters.and(Filters.Phoenix_Squadron_character, Filters.pilot);
+    @Override
+    protected List<Modifier> getGameTextAlwaysOnModifiers(SwccgGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new DeployCostForSimultaneouslyDeployingPilotModifier(self, phoenixSquadPilots, -1));
+        return modifiers;
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiersEvenIfUnpiloted(SwccgGame game, final PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new DeployCostAboardModifier(self, phoenixSquadPilots, -1, self));
+        return modifiers;
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        String playerId = self.getOwner();
+        String opponent = game.getOpponent(playerId);
+        Condition atOpponentsBattlegroundSystem = new AtCondition(self, Filters.and(Filters.opponents(playerId), Filters.battleground_system));
+
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new ForceDrainsMayNotBeModifiedModifier(self, Filters.here(self), atOpponentsBattlegroundSystem, opponent, playerId));
+        modifiers.add(new ForceDrainsMayNotBeCanceledModifier(self, Filters.here(self), atOpponentsBattlegroundSystem, opponent, playerId));
+        return modifiers;
     }
 }
