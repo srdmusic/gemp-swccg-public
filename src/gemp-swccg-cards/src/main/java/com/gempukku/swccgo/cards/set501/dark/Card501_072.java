acbc281fb09b96efe94ed532d3e29b1ac56edd7a
Jared Greenwald <jared.greenwald@oracle.com>
2024-11-11 02:14:02 -0500

    updated logic from comments

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_072.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_072.java
@@ -1,8 +1,8 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractNormalEffect;
-import com.gempukku.swccgo.cards.conditions.AtCondition;
 import com.gempukku.swccgo.cards.conditions.DuringBattleCondition;
+import com.gempukku.swccgo.cards.conditions.OnTableCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
@@ -11,7 +11,6 @@ import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.SpotOverride;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
-import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
@@ -43,12 +42,10 @@ public class Card501_072 extends AbstractNormalEffect {
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
         String playerId = self.getOwner();
-        Filter sameSiteAsFrozenCaptive = Filters.sameSiteAs(self, SpotOverride.INCLUDE_CAPTIVE, Filters.frozenCaptive);
-
         List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new ForceDrainModifier(self, Filters.and(Filters.site, sameSiteAsFrozenCaptive), 1, playerId));
+        modifiers.add(new ForceDrainModifier(self, Filters.and(Filters.battleground, Filters.sameLocationAs(self, SpotOverride.INCLUDE_CAPTIVE, Filters.frozenCaptive)), 1, playerId));
         modifiers.add(new MayNotBePlacedOutOfPlayModifier(self, Filters.My_Favorite_Decoration));
-        modifiers.add(new TotalPowerModifier(self, Filters.battleLocation, new AndCondition(new DuringBattleCondition(), new AtCondition(self, Filters.Scum_And_Villainy, sameSiteAsFrozenCaptive)), 3, playerId));
+        modifiers.add(new TotalPowerModifier(self, Filters.battleLocation, new AndCondition(new DuringBattleCondition(), new OnTableCondition(self, Filters.and(Filters.Scum_And_Villainy, Filters.with(self, SpotOverride.INCLUDE_CAPTIVE, Filters.frozenCaptive)))), 3, playerId));
         return modifiers;
     }
 }
