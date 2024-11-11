417eac23cc54be52c611c822ea4a3c9b99703c35
Jared Greenwald <jared.greenwald@oracle.com>
2024-11-11 18:16:51 -0500

    initial commit

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_199.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_199.java
@@ -3,28 +3,30 @@ package com.gempukku.swccgo.cards.set501.light;
 import com.gempukku.swccgo.cards.AbstractRepublic;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.effects.usage.OncePerBattleEffect;
+import com.gempukku.swccgo.cards.effects.usage.OncePerTurnEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
-import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
-import com.gempukku.swccgo.logic.effects.LoseCardFromTableEffect;
-import com.gempukku.swccgo.logic.effects.LoseForceEffect;
-import com.gempukku.swccgo.logic.modifiers.DeployCostToLocationModifier;
-import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.modifiers.TotalWeaponDestinyForWeaponFiredByModifier;
+import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.effects.CancelGameTextUntilEndOfTurnEffect;
+import com.gempukku.swccgo.logic.effects.ModifyDestinyEffect;
+import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
+import com.gempukku.swccgo.logic.effects.UnrespondableEffect;
+import com.gempukku.swccgo.logic.timing.Action;
 import com.gempukku.swccgo.logic.timing.EffectResult;
-import com.gempukku.swccgo.logic.timing.results.HitResult;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 
@@ -38,7 +40,7 @@ public class Card501_199 extends AbstractRepublic {
     public Card501_199() {
         super(Side.LIGHT, 3, 2, 3, 2, 5, "Corporal Rushing", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Royal Naboo security officer in charge of protecting Amidala's Throne Room. His wife and children were captured when the Trade Federation invaded the planet.");
-        setGameText("Deploys -1 to Naboo. While firing a blaster, adds one to total weapon destiny. Once per battle, if a battle droid was just hit by a blaster here, battle droid is lost and opponent loses 1 Force.");
+        setGameText("Once per turn, if armed with a blaster, may cancel the game text of a character of ability < 3 here for remainder of turn. Once per turn, if armed and with Panaka, may add 1 to a just drawn battle destiny or non-lightsaber weapon destiny.");
         addIcons(Icon.THEED_PALACE, Icon.EPISODE_I, Icon.WARRIOR, Icon.VIRTUAL_SET_24);
         addKeywords(Keyword.ROYAL_NABOO_SECURITY);
         setVirtualSuffix(true);
@@ -47,37 +49,62 @@ public class Card501_199 extends AbstractRepublic {
     }
 
     @Override
-    protected List<Modifier> getGameTextAlwaysOnModifiers(SwccgGame game, PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new DeployCostToLocationModifier(self, -1, Filters.Deploys_at_Naboo));
-        return modifiers;
-    }
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        final Filter targetFilter = Filters.and(Filters.opponents(self), Filters.character, Filters.atSameSite(self), Filters.abilityLessThan(3));
 
-    @Override
-    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new TotalWeaponDestinyForWeaponFiredByModifier(self, 1, Filters.blaster));
-        return modifiers;
-    }
-
-    @Override
-    protected List<RequiredGameTextTriggerAction> getGameTextRequiredAfterTriggers(SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
         // Check condition(s)
-        if (TriggerConditions.justHitBy(game, effectResult, Filters.and(Filters.battle_droid, Filters.here(self)), Filters.blaster)
-                && GameConditions.isOncePerBattle(game, self, gameTextSourceCardId)) {
-            PhysicalCard cardHit = ((HitResult) effectResult).getCardHit();
+        if (GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId)
+                && GameConditions.isArmedWith(game, self, Filters.blaster)
+                && GameConditions.canTarget(game, self, targetFilter)) {
 
-            final RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
-            action.setText("Make " + GameUtils.getFullName(cardHit) + " lost and opponent loses 1 Force");
-            action.setActionMsg("Make " + GameUtils.getCardLink(cardHit) + " lost and opponent loses 1 Force");
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId);
+            action.setText("Cancel a character's game text");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerTurnEffect(action));
+            // Choose target(s)
+            action.appendTargeting(
+                    new TargetCardOnTableEffect(action, playerId, "Choose character", targetFilter) {
+                        @Override
+                        protected void cardTargeted(int targetGroupId, final PhysicalCard targetedCard) {
+                            action.addAnimationGroup(targetedCard);
+                            // Allow response(s)
+                            action.allowResponses("Cancel " + GameUtils.getCardLink(targetedCard) + "'s game text",
+                                    new UnrespondableEffect(action) {
+                                        @Override
+                                        protected void performActionResults(Action targetingAction) {
+                                            // Perform result(s)
+                                            action.appendEffect(
+                                                    new CancelGameTextUntilEndOfTurnEffect(action, targetedCard));
+                                        }
+                                    }
+                            );
+                        }
+                    }
+            );
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+@Override
+    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+
+        // Check condition(s)
+        if ((TriggerConditions.isBattleDestinyJustDrawn(game, effectResult)
+                    || TriggerConditions.isWeaponDestinyJustDrawn(game, effectResult, Filters.and(Filters.not(Filters.lightsaber), Filters.weapon)))
+                && GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
+                && GameConditions.isArmedWith(game, self, Filters.weapon)
+                && GameConditions.isWith(game, self, Filters.Panaka)){
+            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
+            action.setText("Add 1 to destiny");
             // Update usage limit(s)
             action.appendUsage(
                     new OncePerBattleEffect(action));
             // Perform result(s)
             action.appendEffect(
-                    new LoseCardFromTableEffect(action, cardHit));
-            action.appendEffect(
-                    new LoseForceEffect(action, game.getOpponent(self.getOwner()), 1));
+                    new ModifyDestinyEffect(action, 1));
             return Collections.singletonList(action);
         }
         return null;
