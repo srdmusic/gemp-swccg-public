2ba012e330e6dbc4f6bd2165f242cea13e474515
Jared Greenwald <jared.greenwald@oracle.com>
2024-11-11 14:27:34 -0500

    dengar errata

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_069.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_069.java
@@ -1,6 +1,8 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractAlien;
+import com.gempukku.swccgo.cards.conditions.EscortingCaptiveCondition;
+import com.gempukku.swccgo.cards.conditions.PilotingCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
@@ -18,20 +20,17 @@ import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
-import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.conditions.OrCondition;
 import com.gempukku.swccgo.logic.effects.CaptureCharacterOnTableEffect;
-import com.gempukku.swccgo.logic.effects.HitCardEffect;
-import com.gempukku.swccgo.logic.effects.LoseForceEffect;
 import com.gempukku.swccgo.logic.effects.RespondableEffect;
 import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
+import com.gempukku.swccgo.logic.modifiers.AddsBattleDestinyModifier;
 import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 import com.gempukku.swccgo.logic.timing.Action;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 import com.gempukku.swccgo.logic.timing.results.AboutToBeHitResult;
-import com.gempukku.swccgo.logic.timing.results.CaptureCharacterResult;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -60,6 +59,7 @@ public class Card501_069 extends AbstractAlien {
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(new AddsPowerToPilotedBySelfModifier(self, 2));
+        modifiers.add(new AddsBattleDestinyModifier(self, new OrCondition(new EscortingCaptiveCondition(self), new PilotingCondition(self, Filters.Punishing_One)), 1));
         return modifiers;
     }
 
@@ -98,64 +98,6 @@ public class Card501_069 extends AbstractAlien {
                         }
                     }
             );
-            return Collections.singletonList(action);
-        }
-        return null;
-    }
-
-    @Override
-    protected List<RequiredGameTextTriggerAction> getGameTextRequiredAfterTriggers(SwccgGame game, final EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
-        final String playerId = self.getOwner();
-        final String opponent = game.getOpponent(playerId);
-
-        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_2;
-
-        // Check condition(s)
-        if (TriggerConditions.isAboutToBeHit(game, effectResult, self)) {
-            Collection<PhysicalCard> validCaptives = Filters.filter(game.getGameState().getCaptivesOfEscort(self), game, Filters.canBeTargetedBy(self, TargetingReason.TO_BE_HIT));
-            if (!validCaptives.isEmpty()) {
-
-                final RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-                action.setText("Make captive 'hit' instead");
-                // Choose target(s)
-                action.appendTargeting(
-                        new TargetCardOnTableEffect(action, playerId, "Choose captive", SpotOverride.INCLUDE_CAPTIVE, TargetingReason.TO_BE_HIT, Filters.in(validCaptives)) {
-                            @Override
-                            protected void cardTargeted(final int targetGroupId, final PhysicalCard cardTargeted) {
-                                action.addAnimationGroup(cardTargeted);
-                                // Allow response(s)
-                                action.allowResponses("Make " + GameUtils.getCardLink(cardTargeted) + " 'hit' instead",
-                                        new RespondableEffect(action) {
-                                            @Override
-                                            protected void performActionResults(Action targetingAction) {
-                                                // Get the targeted card(s) from the action using the targetGroupId.
-                                                // This needs to be done in case the target(s) were changed during the responses.
-                                                PhysicalCard captiveToHit = targetingAction.getPrimaryTargetCard(targetGroupId);
-
-                                                // Perform result(s)
-                                                ((AboutToBeHitResult) effectResult).getPreventableCardEffect().preventEffectOnCard(self);
-                                                action.appendEffect(
-                                                        new HitCardEffect(action, captiveToHit, self));
-                                            }
-                                        });
-                            }
-                        }
-                );
-                return Collections.singletonList(action);
-            }
-        }
-
-        gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_3;
-
-        // Check condition(s)
-        if (TriggerConditions.seizedBy(game, effectResult, Filters.or(Filters.Rebel, Filters.smuggler), self)) {
-            int numForce = Filters.Han.accepts(game, ((CaptureCharacterResult) effectResult).getCapturedCard()) ? 2 : 1;
-
-            final RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-            action.setText("Make opponent lose " + numForce + " Force");
-            // Perform result(s)
-            action.appendEffect(
-                    new LoseForceEffect(action, opponent, numForce));
             return Collections.singletonList(action);
         }
         return null;
