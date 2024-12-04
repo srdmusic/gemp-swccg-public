20466f8ef07c42f216fa6951ec07aa89cf0e1c7d
Greg Zinn <greg.zinn@gmail.com>
2024-12-04 00:17:29 -0500

    His Destiny

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_166.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_166.java
@@ -4,12 +4,8 @@ import com.gempukku.swccgo.cards.AbstractEpicEventDeployable;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.effects.PayRelocateBetweenLocationsCostEffect;
 import com.gempukku.swccgo.cards.effects.PeekAtTopCardsOfReserveDeckAndChooseCardsToTakeIntoHandEffect;
-import com.gempukku.swccgo.cards.effects.PreventEffectOnCardEffect;
 import com.gempukku.swccgo.cards.effects.usage.OncePerPhaseEffect;
 import com.gempukku.swccgo.cards.effects.usage.OncePerTurnEffect;
-import com.gempukku.swccgo.cards.evaluators.MaxLimitEvaluator;
-import com.gempukku.swccgo.cards.evaluators.NegativeEvaluator;
-import com.gempukku.swccgo.cards.evaluators.OccupiesEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
@@ -18,7 +14,6 @@ import com.gempukku.swccgo.common.PlayCardOptionId;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
-import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
@@ -26,27 +21,20 @@ import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
-import com.gempukku.swccgo.logic.actions.CancelCardActionBuilder;
 import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.effects.FlipSingleSidedStackedCard;
-import com.gempukku.swccgo.logic.effects.LoseForceEffect;
 import com.gempukku.swccgo.logic.effects.RelocateBetweenLocationsEffect;
 import com.gempukku.swccgo.logic.effects.RespondableEffect;
 import com.gempukku.swccgo.logic.effects.ShuffleReserveDeckEffect;
 import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
 import com.gempukku.swccgo.logic.effects.choose.ChooseStackedCardEffect;
-import com.gempukku.swccgo.logic.evaluators.Evaluator;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.modifiers.TotalForceGenerationModifier;
-import com.gempukku.swccgo.logic.modifiers.TotalPowerModifier;
+import com.gempukku.swccgo.logic.modifiers.NoForceLossFromCardModifier;
 import com.gempukku.swccgo.logic.timing.Action;
-import com.gempukku.swccgo.logic.timing.Effect;
 import com.gempukku.swccgo.logic.timing.EffectResult;
-import com.gempukku.swccgo.logic.timing.results.AboutToBeStolenResult;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -59,13 +47,11 @@ public class Card501_166 extends AbstractEpicEventDeployable {
     public Card501_166() {
         super(Side.LIGHT, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, "His Destiny", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setGameText("If He Is The Chosen One on table, deploy on table. " +
-                "Opponent's total Force generation is -1 (and your total power everywhere is +1) for each [Endor] or [Death Star II] battleground you occupy (limit 2). " +
-                "Once per turn, if a card was just stacked on I Feel The Conflict, may peek at top two cards of your Reserve Deck; take one into hand and shuffle your Reserve Deck." +
-                "May lose 2 Force to cancel You Are Beaten or an attempt to steal Luke's Lightsaber. " +
-                "During your move phase, may flip a card stacked on I Feel The Conflict face up to relocate Luke between a battleground site and your Death Star II site.");
+                "Indeed You Are Powerful: Once per turn, if you just won a battle or duel, may peek at the top two cards of your Reserve Deck; take one into hand and shuffle your Reserve Deck. " +
+                "I Have To Face Him: During your move phase, may turn a card stacked on I Feel The Conflict face up to relocate Luke between a battleground site and your [Endor] or [Death Star II] site. " +
+                "Father, Please!: Non-[Set 13] Anakin Skywalker does not cause Force loss and, once per game, you may play it from outside your deck.");
         addIcons(Icon.DEATH_STAR_II, Icon.VIRTUAL_SET_21);
         setTestingText("His Destiny (ERRATA)");
-        hideFromDeckBuilder();
     }
 
     @Override
@@ -74,29 +60,8 @@ public class Card501_166 extends AbstractEpicEventDeployable {
     }
 
     @Override
-    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, PhysicalCard self) {
-        String playerId = self.getOwner();
-        String opponent = game.getOpponent(playerId);
-
-        Evaluator battlegroundCount = new MaxLimitEvaluator(new OccupiesEvaluator(playerId, Filters.and(Filters.or(Icon.ENDOR, Icon.DEATH_STAR_II), Filters.battleground)), 2);
-        List<Modifier> modifiers = new ArrayList<>();
-        modifiers.add(new TotalForceGenerationModifier(self, new NegativeEvaluator(battlegroundCount), opponent));
-        modifiers.add(new TotalPowerModifier(self, Filters.location, battlegroundCount, playerId));
-        return modifiers;
-    }
-
-    @Override
     protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, final SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
         List<TopLevelGameTextAction> actions = new ArrayList<>();
-
-        if (GameConditions.canTargetToCancel(game, self, Filters.You_Are_Beaten)) {
-
-            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId);
-            // Build action using common utility
-            CancelCardActionBuilder.buildCancelCardAction(action, Filters.You_Are_Beaten, Title.You_Are_Beaten);
-            action.appendCost(new LoseForceEffect(action, playerId, 2, true));
-            actions.add(action);
-        }
 
         // Relocate Luke
         GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_2;
@@ -121,27 +86,12 @@ public class Card501_166 extends AbstractEpicEventDeployable {
     }
 
     @Override
-    protected List<OptionalGameTextTriggerAction> getGameTextOptionalBeforeTriggers(final String playerId, SwccgGame game, final Effect effect, final PhysicalCard self, int gameTextSourceCardId) {
-        // Check condition(s)
-        if (TriggerConditions.isPlayingCard(game, effect, Filters.You_Are_Beaten)
-                && GameConditions.canCancelCardBeingPlayed(game, self, effect)) {
-
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId);
-            // Build action using common utility
-            CancelCardActionBuilder.buildCancelCardBeingPlayedAction(action, effect);
-            action.appendCost(new LoseForceEffect(action, playerId, 2, true));
-            return Collections.singletonList(action);
-        }
-        return null;
-    }
-
-    @Override
     protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
         List<OptionalGameTextTriggerAction> actions = new LinkedList<>();
 
         GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
 
-        if(TriggerConditions.justStackedCardOn(game, effectResult, Filters.any, Filters.I_Feel_The_Conflict)
+        if((TriggerConditions.wonBattle(game, effectResult, playerId) || TriggerConditions.wonDuel(game, effectResult, playerId))
                 && GameConditions.hasReserveDeck(game, playerId)
                 && GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)) {
 
@@ -161,20 +111,6 @@ public class Card501_166 extends AbstractEpicEventDeployable {
             actions.add(action);
         }
 
-        if (TriggerConditions.isAboutToBeStolen(game, effectResult, Filters.Lukes_Lightsaber)) {
-            final AboutToBeStolenResult aboutToStealCardResult = (AboutToBeStolenResult) effectResult;
-            final PhysicalCard weaponToBeStolen = aboutToStealCardResult.getCardToBeStolen();
-
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, playerId, gameTextSourceCardId, gameTextActionId);
-            action.setText("Cancel attempt");
-            action.setActionMsg("Cancel attempt to steal "+ GameUtils.getCardLink(weaponToBeStolen));
-
-            action.appendCost(
-                    new LoseForceEffect(action, playerId, 2, true));
-            action.appendEffect(
-                    new PreventEffectOnCardEffect(action, aboutToStealCardResult.getPreventableCardEffect(), weaponToBeStolen, null));
-            actions.add(action);
-        }
         return actions;
     }
 
@@ -226,5 +162,15 @@ public class Card501_166 extends AbstractEpicEventDeployable {
                 }
         );
         return action;
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        String playerId = self.getOwner();
+        String opponent = game.getOpponent(playerId);
+
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new NoForceLossFromCardModifier(self, Filters.and(Filters.Anakin_Skywalker, Filters.not(Icon.VIRTUAL_SET_13)), opponent));
+        return modifiers;
     }
 }
