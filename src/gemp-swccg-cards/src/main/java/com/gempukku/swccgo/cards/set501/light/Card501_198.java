ed7137d1e0e0d292d4ec773f7dc1a905d40527cc
Greg Zinn <greg.zinn@gmail.com>
2024-12-12 23:20:15 -0500

    Game text

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_198.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_198.java
@@ -17,10 +17,9 @@ import com.gempukku.swccgo.common.Uniqueness;
 public class Card501_198 extends AbstractResistance {
     public Card501_198() {
         super(Side.LIGHT, 1, 5, 6, 5, 8, "Ben Solo", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setGameText("P3. If drawn for destiny, may ▲ Rey or a lightsaber. Only deploys if a [E7] Epic Event on table. Your total battle destiny here is +1 for each interrupt you have out of play (limit +3). Once per game, may deploy a lightsaber on Ben from lost pile.");
+        setGameText("If drawn for destiny, may [upload] Rey or a lightsaber. [Pilot] 3. Deploys only if an [Episode VII] Epic Event on table. Your total battle destiny here is +1 for each of your Interrupts out of play (limit +3). Once per game, may deploy a lightsaber on Ben from Lost Pile.");
         addIcons(Icon.EPISODE_VII, Icon.PILOT, Icon.WARRIOR, Icon.VIRTUAL_SET_24);
-        addPersona(Persona.KYLO);
+        addPersona(Persona.KYLO); //----double checking this with EBT----
         setTestingText("Ben Solo");
-        hideFromDeckBuilder();
     }
 }
