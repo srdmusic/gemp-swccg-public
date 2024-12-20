2efd7ae6608f3c09a550bd2209c1b5fa085170a0
hatou003 <hatou003@umn.edu>
2024-12-20 06:47:06 -0600

    Update: Bodhi Rook (Errata)

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_217.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_217.java
@@ -3,6 +3,7 @@ package com.gempukku.swccgo.cards.set501.light;
 import com.gempukku.swccgo.cards.AbstractRebel;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.effects.PeekAtOpponentsHandEffect;
+import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
 import com.gempukku.swccgo.cards.evaluators.CardMatchesEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
@@ -19,12 +20,14 @@ import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.effects.MoveCardAsRegularMoveEffect;
 import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
 import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -39,7 +42,7 @@ public class Card501_217 extends AbstractRebel {
     public Card501_217() {
         super(Side.LIGHT, 2, 2, 2, 2, 4, Title.Bodhi, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Gambler and spy.");
-        setGameText("[Pilot] 2, 3: Rogue One or a stolen starship. When deployed, may peek at opponent's hand. If about to be lost, your Rebel or Rebel starship at a related location may make a regular move.");
+        setGameText("[Pilot] 2, 3: Rogue One or a stolen starship. Once per game, during battle, may peek at opponent's hand. If about to be lost, your Rebel or Rebel starship at a related location may make a regular move.");
         addIcons(Icon.PILOT, Icon.WARRIOR, Icon.VIRTUAL_SET_6);
         addKeywords(Keyword.GAMBLER, Keyword.SPY, Keyword.ROGUE_SQUADRON);
         setMatchingStarshipFilter(Filters.Rogue_One);
@@ -58,20 +61,6 @@ public class Card501_217 extends AbstractRebel {
         List<OptionalGameTextTriggerAction> actions = new LinkedList<>();
 
         GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
-
-        String opponent = game.getOpponent(playerId);
-        if (TriggerConditions.justDeployed(game, effectResult, self)
-                && GameConditions.hasHand(game, opponent)) {
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-            action.setText("Peek at opponent's hand");
-            // Perform result(s)
-            action.appendEffect(
-                    new PeekAtOpponentsHandEffect(action, playerId));
-
-            actions.add(action);
-        }
-
-        gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_2;
 
         // Check condition(s)
         if (TriggerConditions.isAboutToBeLostIncludingAllCardsSituation(game, effectResult, self)
@@ -98,5 +87,28 @@ public class Card501_217 extends AbstractRebel {
             }
         }
         return actions;
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.BODHI__PEAK_AT_HAND;
+        String opponent = game.getOpponent(playerId);
+
+        // Check condition(s)
+        if (GameConditions.isOncePerGame(game, self, gameTextActionId)
+                && GameConditions.isInBattle(game, self)
+                && GameConditions.hasHand(game, opponent)) {
+
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
+            action.setText("Peek at opponent's hand");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerGameEffect(action));
+            // Perform result(s)
+            action.appendEffect(
+                    new PeekAtOpponentsHandEffect(action, playerId));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
