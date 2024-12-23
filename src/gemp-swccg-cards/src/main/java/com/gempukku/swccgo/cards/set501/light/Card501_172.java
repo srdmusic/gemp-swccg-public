2bff76345f3df9f24eb24cca7cbfb047f20bb090
Greg Zinn <greg.zinn@gmail.com>
2024-12-22 21:58:54 -0500

    First draft

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_172.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_172.java
@@ -3,7 +3,7 @@ package com.gempukku.swccgo.cards.set501.light;
 import com.gempukku.swccgo.cards.AbstractRebel;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.conditions.WithCondition;
-import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
+import com.gempukku.swccgo.cards.effects.usage.OncePerBattleEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
@@ -17,17 +17,13 @@ import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.TriggerConditions;
-import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
-import com.gempukku.swccgo.logic.effects.CancelDestinyAndCauseRedrawEffect;
 import com.gempukku.swccgo.logic.effects.RecirculateEffect;
 import com.gempukku.swccgo.logic.modifiers.AddsBattleDestinyModifier;
 import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
 import com.gempukku.swccgo.logic.modifiers.DrawsBattleDestinyIfUnableToOtherwiseModifier;
 import com.gempukku.swccgo.logic.modifiers.ManeuverModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.timing.EffectResult;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -44,7 +40,7 @@ public class Card501_172 extends AbstractRebel {
     public Card501_172() {
         super(Side.LIGHT, 1, 3, 3, 3, 6, Title.Han_Solo, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Smuggler, gambler and 'freelance law-bender.' Crafty Corellian pirate. Rebel hero. Owns Millennium Falcon. Co-pilot Chewbacca promised him 'life-debt.' Has bounty on head.");
-        setGameText("Adds 3 to power of anything he pilots. Draws one battle destiny if unable to otherwise. Adds one battle destiny with Chewie. While piloting Falcon, adds 2 to maneuver and, once per game, may choose: Cancel and redraw any destiny, or re-circulate.");
+        setGameText("[Pilot] 3. Draws one battle destiny if unable to otherwise. Adds one battle destiny with Chewie. While piloting Falcon, adds 2 to maneuver and, once during battle, may re-circulate.");
         addIcons(Icon.A_NEW_HOPE, Icon.PILOT, Icon.WARRIOR, Icon.VIRTUAL_SET_24);
         addKeywords(Keyword.SMUGGLER, Keyword.GAMBLER, Keyword.PIRATE);
         addPersona(Persona.HAN);
@@ -65,39 +61,18 @@ public class Card501_172 extends AbstractRebel {
     }
 
     @Override
-    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
-        GameTextActionId gameTextActionId = GameTextActionId.HAN_SOLO__REDRAW_DESTINY_OR_RECIRCULATE;
-
-        // Check condition(s)
-        if (TriggerConditions.isDestinyJustDrawn(game, effectResult)
-                && GameConditions.isPiloting(game, self, Filters.Falcon)
-                && GameConditions.isOncePerGame(game, self, gameTextActionId)
-                && GameConditions.canCancelDestinyAndCauseRedraw(game, playerId)) {
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-            action.setText("Cancel and re-draw destiny");
-            // Update usage limit(s)
-            action.appendUsage(
-                    new OncePerGameEffect(action));
-            // Perform result(s)
-            action.appendEffect(
-                    new CancelDestinyAndCauseRedrawEffect(action));
-            return Collections.singletonList(action);
-        }
-        return null;
-    }
-
-    @Override
     protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
-        GameTextActionId gameTextActionId = GameTextActionId.HAN_SOLO__REDRAW_DESTINY_OR_RECIRCULATE;
-        if (GameConditions.hasUsedPile(game, playerId)
-                && GameConditions.isOncePerGame(game, self, gameTextActionId)) {
+        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+        if (GameConditions.isPiloting(game, self, Filters.Falcon)
+                && GameConditions.isDuringBattleWithParticipant(game, self)
+                && GameConditions.hasUsedPile(game, playerId)) {
             
             final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
 
             action.setText("Re-circulate");
             action.setActionMsg("Re-circulate");
             action.appendUsage(
-                new OncePerGameEffect(action));
+                new OncePerBattleEffect(action));
             // Perform result(s)
             action.appendEffect(
                 new RecirculateEffect(action, playerId));
