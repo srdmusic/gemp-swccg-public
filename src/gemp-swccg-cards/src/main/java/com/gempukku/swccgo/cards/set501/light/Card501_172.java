80807e4291bed56a9fba2abd303f8491c8f12020
Greg Zinn <greg.zinn@gmail.com>
2025-02-22 02:11:32 -0500

    Deployment rules and game text canceler

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_172.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_172.java
@@ -1,14 +1,25 @@
 package com.gempukku.swccgo.cards.set501.light;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.gempukku.swccgo.cards.AbstractNormalEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.PlayCardOptionId;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.modifiers.CancelsGameTextModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.PowerModifier;
 
 /**
  * Set: Playtesting
@@ -24,5 +35,29 @@ public class Card501_172 extends AbstractNormalEffect {
         addIcon(Icon.VIRTUAL_SET_25);
         setVirtualSuffix(true);
         setTestingText("Demotion (V)");
+    }
+
+    @Override
+    protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        Filter baseRequirements = Filters.or(Filters.and(Filters.alien, Filters.leader), Filters.Imperial, Filters.senator);
+        Filter notExceptions = Filters.not(Filters.or(Filters.Bib, Filters.Jabba, Filters.Sidious, Filters.Thrawn, Filters.Vader, Filters.Xizor));
+        
+        return Filters.and(baseRequirements, notExceptions);
+    }
+
+    @Override
+    protected Filter getGameTextValidTargetFilterToRemainAttachedToAfterCrossingOver(final SwccgGame game, final PhysicalCard self, PlayCardOptionId playCardOptionId) {
+        Filter baseRequirements = Filters.or(Filters.and(Filters.alien, Filters.leader), Filters.Imperial, Filters.senator);
+        
+        return baseRequirements;
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        Filter attachedTo = Filters.hasAttached(self);
+
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new CancelsGameTextModifier(self, attachedTo));
+        return modifiers;
     }
 }
