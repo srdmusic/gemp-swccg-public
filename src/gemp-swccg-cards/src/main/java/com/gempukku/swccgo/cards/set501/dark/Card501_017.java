dad4c4fc37e3c52f66329319d2e784c66bf8be47
Greg Zinn <greg.zinn@gmail.com>
2025-03-02 03:12:15 -0500

    Replace old function with new function

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_017.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_017.java
@@ -8,7 +8,6 @@ import com.gempukku.swccgo.cards.AbstractAlien;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.effects.ConvertLocationByRaisingToTopEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
-import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
@@ -23,10 +22,8 @@ import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
-import com.gempukku.swccgo.logic.effects.PlaceCardInUsedPileFromTableEffect;
+import com.gempukku.swccgo.logic.effects.PutCardFromHandOnForcePileEffect;
 import com.gempukku.swccgo.logic.effects.SendMessageEffect;
-import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
-import com.gempukku.swccgo.logic.effects.choose.DrawCardIntoHandFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.modifiers.MayNotHaveGameTextCanceledModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 import com.gempukku.swccgo.logic.timing.EffectResult;
@@ -41,7 +38,7 @@ public class Card501_017 extends AbstractAlien {
     public Card501_017() {
         super(Side.DARK, 3, 2, 1, 1, 3, Title.Salacious_Crumb, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Male Kowakian. Prankster. Humiliates others for Jabba's amusement. His life depends on making Jabba laugh at least once per day.");
-        setGameText("When deployed, may draw top card of Reserve Deck or place a droid present in owner's Used Pile ('AH-hahahaha!'). Game text of your alien leaders here may not be canceled. If at a converted Jabba's Palace site, may raise yours to the top.");
+        setGameText("Game text of your alien leaders here may not be canceled. If at a converted Jabba's Palace site, may raise yours to the top. If opponent just deployed a character here, may place a card from hand on Force Pile ('AH-hahahaha!').");
         addIcons(Icon.JABBAS_PALACE, Icon.VIRTUAL_SET_25);
         setSpecies(Species.KOWAKIAN);
         setVirtualSuffix(true);
@@ -51,52 +48,24 @@ public class Card501_017 extends AbstractAlien {
     @Override
     protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, final SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
 
-        List<OptionalGameTextTriggerAction> actions = new LinkedList<>();
-
-        //Both possible responses share a gameTextActionId so that the player can only perform one of them
-        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
-        Filter droidPresent = Filters.and(Filters.droid, Filters.present(self));
-
         // Check condition(s)
-        if (TriggerConditions.justDeployed(game, effectResult, self)
-                && GameConditions.hasReserveDeck(game, playerId)) {
+        if(TriggerConditions.justDeployedToLocation(game, effectResult, game.getOpponent(playerId), Filters.character, Filters.here(self))
+            && GameConditions.hasHand(game, playerId)){
 
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-            
-            action.setText("Draw top card of Reserve Deck");
-            action.setActionMsg("Draw top card of Reserve Deck into hand");
+            OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId);
+            action.setText("Place card from hand on Force Pile");
+            action.setActionMsg("Place a card from hand on Force Pile");
+
+            // Send Easter Egg Message
+            action.appendCost(
+                new SendMessageEffect(action, "Salacious Crumb: AH-hahahaha!"));
+
             // Perform result(s)
-            action.appendEffect(
-                    new DrawCardIntoHandFromReserveDeckEffect(action, playerId));
-            actions.add(action);
+            action.appendEffect(new PutCardFromHandOnForcePileEffect(action, playerId));
+
+            return Collections.singletonList(action);
         }
-
-        // Check condition(s)
-        if (TriggerConditions.justDeployed(game, effectResult, self)
-                && GameConditions.canTarget(game, self, droidPresent)) {
-
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-            
-            action.setText("Place a droid in owner's Used Pile");
-            action.setActionMsg("Place a droid present in owner's Used Pile");
-            // Choose target(s)
-            action.appendTargeting(
-              new ChooseCardOnTableEffect(action, playerId, "Choose a droid present", Filters.and(Filters.droid, Filters.present(self))) {
-                        @Override
-                        protected void cardSelected(PhysicalCard selectedCard) {
-                            // Send Easter Egg Message
-                            action.appendCost(
-                                new SendMessageEffect(action, "Salacious Crumb: AH-hahahaha!"));
-                            // Perform result(s)
-                            action.appendEffect(
-                                    new PlaceCardInUsedPileFromTableEffect(action, selectedCard));
-                        }
-              }
-            );
-            actions.add(action);
-        }
-
-        return actions;
+        return null;
     }
 
     @Override
