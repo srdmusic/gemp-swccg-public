347d43116c514496398840d78dc11d1947ba01b6
Greg Zinn <greg.zinn@gmail.com>
2025-03-02 15:19:35 -0500

    Template, Deployment, Nabrun canceler

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_043.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_043.java
@@ -1,12 +1,25 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
+import java.util.Collections;
+import java.util.List;
+
 import com.gempukku.swccgo.cards.AbstractNormalEffect;
+import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.PlayCardOptionId;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.CancelCardActionBuilder;
+import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.timing.Effect;
 
 /**
  * Set: Playtesting
@@ -15,11 +28,29 @@ import com.gempukku.swccgo.common.Uniqueness;
  */
 public class Card501_043 extends AbstractNormalEffect {
     public Card501_043() {
-        super(Side.DARK, 6, PlayCardZoneOption.ATTACHED, "Stranded", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
+        super(Side.DARK, 6, PlayCardZoneOption.ATTACHED, "Stranded", Uniqueness.UNRESTRICTED, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Imperial troopers use tactics to strand and cut off fugitives. Only daring and unpredictable actions gave Luke and Leia a chance to escape.");
-        setGameText("Deploy on any character. Nabrun Leids is canceled. Characters here may not be Disarmed or fire weapons outside of battle. Opponent may not cancel battle destiny draws here.");
+        setGameText("Deploy on a character. Nabrun Leids is canceled. Characters here may not fire weapons except during battle. Opponent may not cancel battle destiny draws here. If on Luke or Leia, character may not move except during their owner's move phase. [Immune to Alter.]");
         addIcons(Icon.VIRTUAL_SET_25);
         setTestingText("Stranded");
-        hideFromDeckBuilder();
+    }
+
+    @Override
+    protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        return Filters.character;
+    }
+
+    @Override
+    protected List<RequiredGameTextTriggerAction> getGameTextRequiredBeforeTriggers(SwccgGame game, Effect effect, PhysicalCard self, int gameTextSourceCardId) {
+        // Check condition(s)
+        if (TriggerConditions.isPlayingCard(game, effect, Filters.Nabrun_Leids)
+                && GameConditions.canCancelCardBeingPlayed(game, self, effect)) {
+
+            RequiredGameTextTriggerAction action = new RequiredGameTextTriggerAction(self, gameTextSourceCardId);
+            // Build action using common utility
+            CancelCardActionBuilder.buildCancelCardBeingPlayedAction(action, effect);
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
