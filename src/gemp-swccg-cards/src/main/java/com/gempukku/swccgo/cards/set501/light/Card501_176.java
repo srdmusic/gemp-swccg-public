802d25231a5a9d26fb64685eacb78900e191c04c
Greg Zinn <greg.zinn@gmail.com>
2025-03-03 18:58:41 -0500

    Adding the functions

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_176.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_176.java
@@ -1,15 +1,34 @@
 package com.gempukku.swccgo.cards.set501.light;
 
 import com.gempukku.swccgo.cards.AbstractUsedOrLostInterrupt;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.common.CardSubtype;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.Persona;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.TargetingReason;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.GameUtils;
+import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
+import com.gempukku.swccgo.logic.effects.ExcludeFromBattleEffect;
+import com.gempukku.swccgo.logic.effects.LoseForceEffect;
+import com.gempukku.swccgo.logic.effects.PlaceCardOutOfPlayFromOffTableEffect;
+import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
+import com.gempukku.swccgo.logic.effects.SuspendCardUntilEndOfTurnEffect;
+import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
+import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.timing.Action;
 
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -17,7 +36,7 @@ import java.util.List;
  * Set: Playtesting
  * Type: Interrupt
  * Subtype: Used Or Lost Interrupt
- * Title: Insertion Planning (V)
+ * Title: Critical Error Revealed (V)
  */
 public class Card501_176 extends AbstractUsedOrLostInterrupt {
     public Card501_176() {
@@ -30,5 +49,99 @@ public class Card501_176 extends AbstractUsedOrLostInterrupt {
         setTestingText("Critical Error Revealed (V)");
     }
 
+    @Override
+    protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, final SwccgGame game, final PhysicalCard self) {
+        List<PlayInterruptAction> actions = new LinkedList<>();
 
+        Filter suspendFilter = Filters.or(Filters.Ominous_Rumors, Filters.There_Are_Many_Hunting_You_Now);
+
+        // Check condition(s)
+        if (GameConditions.canTarget(game, self, TargetingReason.TO_BE_SUSPENDED, suspendFilter)) {
+            final PlayInterruptAction action = new PlayInterruptAction(game, self, CardSubtype.USED);
+            
+            action.setText("Suspend card for remainder of turn");
+            // Choose target(s)
+            action.appendTargeting(
+                    new TargetCardOnTableEffect(action, playerId, "Choose card to suspend", suspendFilter) {
+                        @Override
+                        protected void cardTargeted(final int targetGroupId, final PhysicalCard targetedCard) {
+                            action.addAnimationGroup(targetedCard);
+                            // Allow response(s)
+                            action.allowResponses("Suspend " + GameUtils.getCardLink(targetedCard),
+                                    new RespondablePlayCardEffect(action) {
+                                        @Override
+                                        protected void performActionResults(Action targetingAction) {
+                                            // Perform result(s)
+                                            action.appendEffect(
+                                                    new SuspendCardUntilEndOfTurnEffect(action, targetedCard));
+                                        }
+                                    }
+                            );
+                        }
+                    }
+            );
+            actions.add(action);
+        }
+
+        Filter pullFilter = Filters.or(Filters.title("Lak Sivrak"), Filters.title("Orrimaarko"), Filters.persona(Persona.TALA_DURITH), Filters.and(Icon.ENDOR, Filters.Chewie));
+        GameTextActionId gameTextActionId = GameTextActionId.CRITICAL_ERROR_REVEALED__UPLOAD_CARD;
+
+        // Check condition(s)
+        if (GameConditions.canTakeCardsIntoHandFromReserveDeck(game, playerId, self, gameTextActionId)) {
+            final PlayInterruptAction action = new PlayInterruptAction(game, self, gameTextActionId, CardSubtype.USED);
+
+            action.setText("Take card into hand from Reserve Deck");
+
+            action.allowResponses("Take Lak Sivrak, Orrimaarko, Tala Durith, or [Endor] Chewie into hand from Reserve Deck",
+                new RespondablePlayCardEffect(action) {
+                    @Override
+                    protected void performActionResults(Action targetingAction) {
+                        // Perform result(s)
+                        action.appendEffect(
+                                new TakeCardIntoHandFromReserveDeckEffect(action, playerId, pullFilter, true));
+                    }
+                }
+            );
+            actions.add(action);
+        }
+
+        Filter passengerFilter = Filters.and(Filters.aboardAsPassenger(Filters.any), Filters.participatingInBattle);
+        TargetingReason targetingReason = TargetingReason.TO_BE_EXCLUDED_FROM_BATTLE;
+
+        // Check condition(s)
+        if (GameConditions.isDuringBattle(game)
+                && GameConditions.canTarget(game, self, targetingReason, passengerFilter)) {
+           
+            final PlayInterruptAction action = new PlayInterruptAction(game, self, CardSubtype.LOST);
+            action.setText("Exclude passenger from battle");
+            // Choose target(s)
+            action.appendTargeting(
+                new TargetCardOnTableEffect(action, playerId, "Choose a passenger", targetingReason, passengerFilter) {
+                    @Override
+                    protected void cardTargeted(final int targetGroupId, PhysicalCard targetedCard) {
+                        action.addAnimationGroup(targetedCard);
+                        // Pay cost(s)
+                        action.appendCost(new LoseForceEffect(action, playerId, 1, true));
+                        // Allow response(s)
+                        action.allowResponses("Exclude " + GameUtils.getCardLink(targetedCard) + " from battle",
+                                new RespondablePlayCardEffect(action) {
+                                    @Override
+                                    protected void performActionResults(Action targetingAction) {
+                                        // Get the targeted card(s) from the action using the targetGroupId.
+                                        // This needs to be done in case the target(s) were changed during the responses.
+                                        PhysicalCard finalTarget = action.getPrimaryTargetCard(targetGroupId);
+
+                                        // Perform result(s)
+                                        action.appendEffect(
+                                                new ExcludeFromBattleEffect(action, finalTarget));
+                                        action.appendEffect(new PlaceCardOutOfPlayFromOffTableEffect(action, self));
+                                    }
+                                }
+                        );
+                    }
+                });
+            actions.add(action);
+        }
+        return actions;
+    }
 }
