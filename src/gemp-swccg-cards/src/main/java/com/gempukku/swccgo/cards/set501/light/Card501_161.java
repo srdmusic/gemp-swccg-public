bcc584f86f6a12a299328ff595e11b01445a271d
Jared Greenwald <jared.greenwald@oracle.com>
2025-03-04 23:07:29 -0500

    typo and updated game text

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_161.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_161.java
@@ -41,7 +41,7 @@ public class Card501_161 extends AbstractAlien {
     public Card501_161() {
         super(Side.LIGHT, 3, 2, 2, 2, 3, Title.Wuta, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Ewok explorer. Scout. Searches for fallen trees to make tools. Tracks predators. First to notice the Imperial presence on Endor.");
-        setGameText("Once per game, when deployed, may [UPLOAD] an Endor site. Game text of your other scouts may not be canceled here. During any deploy phase, if an Imperial at an adjacent site, Wuta may move to that site (using landspeed) as a regular move.");
+        setGameText("Once per game, when deployed, may [upload] an Endor site. Game text of your other scouts here may not be canceled. During any deploy phase, if an Imperial at an adjacent site, Wuta may move to that site (using landspeed) as a regular move.");
         addIcons(Icon.ENDOR, Icon.VIRTUAL_SET_25);
         addKeywords(Keyword.SCOUT);
         setSpecies(Species.EWOK);
@@ -60,7 +60,7 @@ public class Card501_161 extends AbstractAlien {
 
             final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
             action.setText("Take an Endor site into hand from Reserve Deck");
-            action.setActionMsg("Take an Endor siet into hand from Reserve Deck");
+            action.setActionMsg("Take an Endor site into hand from Reserve Deck");
             // Perform result(s)
             action.appendEffect(
                     new TakeCardIntoHandFromReserveDeckEffect(action, playerId, Filters.Endor_site, true));
