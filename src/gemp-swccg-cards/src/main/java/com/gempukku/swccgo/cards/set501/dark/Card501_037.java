01c6dbbe07eecd6ef2fc7a5c7a6116331ac610f5
Greg Zinn <greg.zinn@gmail.com>
2025-03-12 20:59:13 -0400

    Basic template

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_037.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_037.java
@@ -18,11 +18,10 @@ import com.gempukku.swccgo.common.Uniqueness;
 public class Card501_037 extends AbstractImmediateEffect {
     public Card501_037() {
         super(Side.DARK, 4, PlayCardZoneOption.ATTACHED, "The Client's Bounty", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setLore("");
-        setGameText("Deploy on opponent's just deployed character. If that character captured, may take this card into hand; retrieve 2 Force (3 if The Client on table). If a bounty hunter here, once per turn, may reveal top card of every Reserve Deck. (Immune to Control.)");
+        setLore("One of the most profitable occupations in the galaxy is hunting down and capturing wanted beings. The more notable the quarry, the more profitable the venture.");
+        setGameText("Deploy on opponent's just deployed character. Once per turn, if a bounty hunter here, may reveal the top card of each player's Reserve Deck. If this character captured and seized, retrieve 2 Force (3 if The Client on table) and return this card to your hand. [Immune to Control.]");
         addIcons(Icon.VIRTUAL_SET_25);
         addImmuneToCardTitle(Title.Control);
         setTestingText("The Client's Bounty");
-        hideFromDeckBuilder();
     }
 }
