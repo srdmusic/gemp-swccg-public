82e5ad460896e23666755daf973a7d904a427442
Greg Zinn <greg.zinn@gmail.com>
2025-03-12 21:05:06 -0400

    Deployment

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_037.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_037.java
@@ -1,13 +1,24 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
+import java.util.Collections;
+import java.util.List;
+
 import com.gempukku.swccgo.cards.AbstractImmediateEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Keyword;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
+import com.gempukku.swccgo.logic.actions.PlayCardAction;
+import com.gempukku.swccgo.logic.timing.EffectResult;
+import com.gempukku.swccgo.logic.timing.results.PlayCardResult;
 
 /**
  * Set: Playtesting
@@ -22,6 +33,20 @@ public class Card501_037 extends AbstractImmediateEffect {
         setGameText("Deploy on opponent's just deployed character. Once per turn, if a bounty hunter here, may reveal the top card of each player's Reserve Deck. If this character captured and seized, retrieve 2 Force (3 if The Client on table) and return this card to your hand. [Immune to Control.]");
         addIcons(Icon.VIRTUAL_SET_25);
         addImmuneToCardTitle(Title.Control);
+        addKeywords(Keyword.BOUNTY);
         setTestingText("The Client's Bounty");
+    }
+
+    @Override
+    protected List<PlayCardAction> getGameTextOptionalAfterActions(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
+        // Check condition(s)
+        if (TriggerConditions.justDeployed(game, effectResult, Filters.and(Filters.opponents(self), Filters.character))) {
+            PhysicalCard deployedCard = ((PlayCardResult) effectResult).getPlayedCard();
+            PlayCardAction action = getPlayCardAction(playerId, game, self, self, false, 0, null, null, null, null, null, false, 0, Filters.sameCardId(deployedCard), null);
+            if (action != null) {
+                return Collections.singletonList(action);
+            }
+        }
+        return null;
     }
 }
