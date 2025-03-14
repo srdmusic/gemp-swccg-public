ea34013b3f28cbfb2a56328c10ace7c9bb1165fa
Greg Zinn <greg.zinn@gmail.com>
2025-03-13 22:13:33 -0400

    Basic template

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_051.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_051.java
@@ -17,11 +17,10 @@ import com.gempukku.swccgo.common.Uniqueness;
 public class Card501_051 extends AbstractSith {
     public Card501_051() {
         super(Side.DARK, 1, 6, 6, 6, 7, "Baylan Skoll", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setLore("Mercenary");
-        setGameText("Once per game, may [DOWNLOAD] a card with 'mercenary' in lore (or a lightsaber on Baylan). Unless Sidious on table, if in battle alone (or with Shin), opponent must have 2 characters (or Anakin or Yoda) in battle to draw battle destiny here. Immune to attrition < 5.");
+        setLore("Mercenary.");
+        setGameText("Once per game, may [download] a card with 'mercenary' in lore (or a lightsaber on Baylan). While in battle alone or with Shin, unless Sidious on table, opponent must have two characters (or Anakin or Yoda) in battle to draw battle destiny. Immune to attrition < 5.");
         addIcons(Icon.WARRIOR, Icon.VIRTUAL_SET_25);
         addKeyword(Keyword.MERCENARY);
         setTestingText("Baylan Skoll");
-        hideFromDeckBuilder();
     }
 }
