ee8f10120ec1dfefb7cf1e47b2a94b525967e53a
Greg Zinn <greg.zinn@gmail.com>
2025-03-15 03:07:56 -0400

    Basic templating 2

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_167.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_167.java
@@ -18,7 +18,7 @@ public class Card501_167 extends AbstractNormalEffect {
     public Card501_167() {
         super(Side.LIGHT, 0, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, "Medal Ceremony", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("With the destruction of the Death Star, the Rebel Alliance received new-found support throughout the galaxy.");
-        setGameText("f Massassi Throne Room on table, deploy on table. Non-Jedi Rebels are defense value +1. Once per game, may [download] a Yavin 4 battleground. Once per game, may retrieve a non-[Maintenance], non-[Permanent Weapon] Rebel of ability < 5. [Immune to Alter.]");
+        setGameText("If Massassi Throne Room on table, deploy on table. Non-Jedi Rebels are defense value +1. Once per game, may [download] a Yavin 4 battleground. Once per game, may retrieve a non-[Maintenance], non-[Permanent Weapon] Rebel of ability < 5. [Immune to Alter.]");
         addIcons(Icon.A_NEW_HOPE, Icon.VIRTUAL_SET_25);
         addImmuneToCardTitle(Title.Alter);
         setTestingText("Medal Ceremony");
