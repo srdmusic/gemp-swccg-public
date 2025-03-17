424e8ef62534e6ad7b0f8c97922434ac8e638835
Greg Zinn <greg.zinn@gmail.com>
2025-03-16 22:21:19 -0400

    Basic template

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_050.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_050.java
@@ -3,6 +3,7 @@ package com.gempukku.swccgo.cards.set501.dark;
 import com.gempukku.swccgo.cards.AbstractDarkJediMaster;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Keyword;
 import com.gempukku.swccgo.common.Persona;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
@@ -17,11 +18,11 @@ import com.gempukku.swccgo.common.Uniqueness;
 public class Card501_050 extends AbstractDarkJediMaster {
     public Card501_050() {
         super(Side.DARK, 1, 6, 5, 7, 8, "Master Sidious", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setLore("");
-        setGameText("While alone on Coruscant, your apprentice is immune to attrition. Disarmed canceled here. During your move phase, if you control this site, may use 1 Force to relocate between a Coruscant site and apprentice's site (or vice versa). Immune to attrition.");
-        addIcons(Icon.WARRIOR, Icon.EPISODE_I, Icon.VIRTUAL_SET_25);
+        setLore("Leader. Trade Federation.");
+        setGameText("While alone on Coruscant, your apprentice is immune to attrition. Disarmed is canceled here. During your move phase, if at a site you control, may use 1 Force to relocate between a Coruscant site and your apprentice's site. Immune to attrition.");
+        addIcons(Icon.EPISODE_I, Icon.WARRIOR, Icon.VIRTUAL_SET_25);
+        addKeywords(Keyword.LEADER);
         addPersona(Persona.SIDIOUS);
         setTestingText("Master Sidious");
-        hideFromDeckBuilder();
     }
 }
