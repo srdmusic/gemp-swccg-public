39bb11e76f86883641300e5d23956b0a9c3de596
Greg Zinn <greg.zinn@gmail.com>
2025-03-17 02:01:49 -0400

    Relocate action

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_050.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_050.java
@@ -8,10 +8,14 @@ import com.gempukku.swccgo.cards.AbstractDarkJediMaster;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.conditions.AloneCondition;
 import com.gempukku.swccgo.cards.conditions.OnCondition;
+import com.gempukku.swccgo.cards.effects.PayRelocateBetweenLocationsCostEffect;
+import com.gempukku.swccgo.cards.effects.usage.OncePerPhaseEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
 import com.gempukku.swccgo.common.Persona;
+import com.gempukku.swccgo.common.Phase;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
@@ -20,13 +24,19 @@ import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.CancelCardActionBuilder;
 import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.conditions.AndCondition;
 import com.gempukku.swccgo.logic.conditions.Condition;
+import com.gempukku.swccgo.logic.effects.RelocateBetweenLocationsEffect;
+import com.gempukku.swccgo.logic.effects.UnrespondableEffect;
+import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
 import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.timing.Action;
 import com.gempukku.swccgo.logic.timing.Effect;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
@@ -83,6 +93,62 @@ public class Card501_050 extends AbstractDarkJediMaster {
             // Build action using common utility
             CancelCardActionBuilder.buildCancelCardAction(action, filter, Title.Disarmed);
             return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+
+        // Check condition(s)
+        if (GameConditions.isOnceDuringYourPhase(game, self, playerId, gameTextSourceCardId, gameTextActionId, Phase.MOVE)
+                && GameConditions.isAtLocation(game, self, Filters.and(Filters.site, Filters.controls(playerId)))
+                && GameConditions.canTarget(game, self, Filters.and(Filters.Coruscant_site))
+                && GameConditions.canTarget(game, self, Filters.and(Filters.Sith_Apprentice))) {
+            
+            Filter siteToRelocateTo;
+            
+            if (GameConditions.isWith(game, self, Filters.Sith_Apprentice)) {
+                siteToRelocateTo = Filters.Coruscant_site;
+            } else if (GameConditions.isAtLocation(game, self, Filters.Coruscant_site)) {
+                siteToRelocateTo = Filters.sameLocationAs(self, Filters.Sith_Apprentice);
+            } else {
+                siteToRelocateTo = Filters.none;
+            }
+            siteToRelocateTo = Filters.and(siteToRelocateTo, Filters.locationCanBeRelocatedTo(self, false, 1));
+
+            if (GameConditions.canSpotLocation(game, siteToRelocateTo)) {
+                final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
+                action.setText("Relocate Sidious to site");
+                // Update usage limit(s)
+                action.appendUsage(
+                        new OncePerPhaseEffect(action));
+                // Choose target(s)
+                action.appendTargeting(
+                        new ChooseCardOnTableEffect(action, self.getOwner(), "Choose site to relocate Sidious to", siteToRelocateTo) {
+                            @Override
+                            protected void cardSelected(final PhysicalCard selectedCard) {
+                                action.addAnimationGroup(selectedCard);
+                                // Pay cost(s)
+                                action.appendCost(
+                                        new PayRelocateBetweenLocationsCostEffect(action, playerId, self, selectedCard, 1));
+                                // Allow response(s)
+                                action.allowResponses("Relocate Sidious to " + GameUtils.getCardLink(selectedCard),
+                                        new UnrespondableEffect(action) {
+                                            @Override
+                                            protected void performActionResults(Action targetingAction) {
+                                                // Perform result(s)
+                                                action.appendEffect(
+                                                        new RelocateBetweenLocationsEffect(action, self, selectedCard, false));
+                                            }
+                                        }
+                                );
+                            }
+                        }
+                );            
+                return Collections.singletonList(action);
+            }
         }
         return null;
     }
