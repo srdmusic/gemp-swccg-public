c235574153ec2f0a5fa3df6d7f6e8fd55023ca24
Greg Zinn <greg.zinn@gmail.com>
2025-04-02 23:50:04 -0400

    Deploys only on Endor

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_161.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_161.java
@@ -7,6 +7,7 @@ import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
 import com.gempukku.swccgo.common.Phase;
+import com.gempukku.swccgo.common.PlayCardOptionId;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Species;
@@ -41,12 +42,17 @@ public class Card501_161 extends AbstractAlien {
     public Card501_161() {
         super(Side.LIGHT, 3, 2, 2, 2, 3, Title.Wuta, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Ewok explorer. Scout. Searches for fallen trees to make tools. Tracks predators. First to notice the Imperial presence on Endor.");
-        setGameText("Once per game, when deployed, may [upload] an Endor site. Game text of your other scouts here may not be canceled. During any deploy phase, if an Imperial at an adjacent site, Wuta may move to that site (using landspeed) as a regular move.");
+        setGameText("Deploys only on Endor. Once per game, may [upload] an Endor site. Game text of your other scouts here may not be canceled. During any deploy phase, if an Imperial at an adjacent site, Wuta may move to that site (using landspeed) as a regular move.");
         addIcons(Icon.ENDOR, Icon.VIRTUAL_SET_25);
         addKeywords(Keyword.SCOUT);
         setSpecies(Species.EWOK);
         setVirtualSuffix(true);
         setTestingText("Wuta (V)");
+    }
+
+    @Override
+    protected Filter getGameTextValidDeployTargetFilter(SwccgGame game, PhysicalCard self, PlayCardOptionId playCardOptionId, boolean asReact) {
+        return Filters.Deploys_on_Endor;
     }
 
     @Override
