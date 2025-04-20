f74a87b5a8108fd1f05ccec9a94221210208be3b
Jared Greenwald <jared.greenwald@oracle.com>
2025-04-20 13:51:51 -0400

    go to dagobah

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_180.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_180.java
@@ -1,10 +1,10 @@
 package com.gempukku.swccgo.cards.set501.light;
 
-import com.gempukku.swccgo.cards.AbstractLostInterrupt;
+import com.gempukku.swccgo.cards.AbstractStartingInterrupt;
 import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.common.CardSubtype;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
-import com.gempukku.swccgo.common.Phase;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
@@ -12,109 +12,57 @@ import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
-import com.gempukku.swccgo.logic.effects.RelocateBetweenLocationsEffect;
+import com.gempukku.swccgo.logic.effects.ModifyNumCardsDrawnInStartingHandEffect;
+import com.gempukku.swccgo.logic.effects.PutCardFromVoidInLostPileEffect;
 import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
-import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
-import com.gempukku.swccgo.logic.effects.UseForceEffect;
-import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
+import com.gempukku.swccgo.logic.effects.choose.TakeCardAndCardIntoHandFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.effects.choose.TakeCardsIntoHandFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.timing.Action;
 
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Set: Playtesting
  * Type: Interrupt
- * Subtype: Lost Or 
+ * Subtype: Starting
  * Title: You Will Go To The Dagobah System (V)
  */
-public class Card501_180 extends AbstractLostInterrupt {
+public class Card501_180 extends AbstractStartingInterrupt {
     public Card501_180() {
         super(Side.LIGHT, 4, "You Will Go To The Dagobah System", Uniqueness.UNRESTRICTED, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("'There you will learn from Yoda, the Jedi Master who instructed me.'");
-        setGameText("During your control phase, use 1 Force to move any one of your [Hoth] characters of ability > 1 to any [Dagobah] location where you have presence. If moving to a Dagobah site, this movement is free and you do not need presence.");
+        setGameText("If your [Dagobah] objective on table, take Yoda, [Dagobah] Luke, and two Effects that deploy for free and are always immune to Alter into hand from Reserve Deck. When you draw your starting hand, draw only five more cards. Place Interrupt in Lost Pile.");
         addIcons(Icon.HOTH, Icon.VIRTUAL_SET_25);
         setVirtualSuffix(true);
         setTestingText("You Will Go To The Dagobah System (V)");
-        hideFromDeckBuilder();
     }
 
     @Override
-    protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, final SwccgGame game, final PhysicalCard self) {
-        List<PlayInterruptAction> actions = new LinkedList<PlayInterruptAction>();
+    protected PlayInterruptAction getGameTextStartingAction(final String playerId, SwccgGame game, final PhysicalCard self) {
 
-        // Check condition(s)
-        if (GameConditions.isDuringYourPhase(game, self, Phase.CONTROL)) {
-            final Filter locationFilter1 = Filters.and(Icon.DAGOBAH, Filters.location, Filters.not(Filters.Dagobah_site), Filters.occupies(playerId));
-            Filter characterFilter1 = Filters.and(Filters.your(self), Icon.HOTH, Filters.character, Filters.abilityMoreThan(1),
-                    Filters.canBeRelocatedToLocation(locationFilter1, true, false, false, 1, false));
-            if (GameConditions.canUseForceToPlayInterrupt(game, playerId, self, 1)
-                    && GameConditions.canTarget(game, self, characterFilter1)) {
-
-                final PlayInterruptAction action = new PlayInterruptAction(game, self);
-                action.setText("Move character to Dagobah icon location");
-                // Build action using common method
-                buildAction(action, playerId, game, self, characterFilter1, locationFilter1, false, 1);
-                actions.add(action);
-            }
-            final Filter locationFilter2 = Filters.and(Icon.DAGOBAH, Filters.Dagobah_site);
-            Filter characterFilter2 = Filters.and(Filters.your(self), Icon.HOTH, Filters.character, Filters.abilityMoreThan(1),
-                    Filters.canBeRelocatedToLocation(locationFilter2, true, false, true, 0, false));
-            if (GameConditions.canTarget(game, self, characterFilter2)) {
-
-                final PlayInterruptAction action = new PlayInterruptAction(game, self);
-                action.setText("Move character to Dagobah site");
-                // Build action using common method
-                buildAction(action, playerId, game, self, characterFilter2, locationFilter2, true, 0);
-                actions.add(action);
-            }
-        }
-        return actions;
-    }
-
-    private void buildAction(final PlayInterruptAction action, final String playerId, final SwccgGame game, final PhysicalCard self, final Filter characterFilter, final Filter locationFilter, final boolean forFree, final int baseCost) {
-        // Choose target(s)
-        action.appendTargeting(
-                new TargetCardOnTableEffect(action, playerId, "Choose character", characterFilter) {
-                    @Override
-                    protected void cardTargeted(final int targetGroupId1, final PhysicalCard character) {
-                        action.addAnimationGroup(character);
-                        Collection<PhysicalCard> locations = Filters.filterTopLocationsOnTable(game, locationFilter);
-                        Collection<PhysicalCard> validLocations = new LinkedList<PhysicalCard>();
-                        // Figure out which locations the character can be relocated to
-                        for (PhysicalCard location : locations) {
-                            if (Filters.canBeRelocatedToLocation(location, true, false, forFree, baseCost, false).accepts(game.getGameState(), game.getModifiersQuerying(), character)) {
-                                validLocations.add(location);
-                            }
+        final PlayInterruptAction action = new PlayInterruptAction(game, self, CardSubtype.STARTING);
+        action.setText("Take cards into hand from Reserve Deck");
+        final Filter dagobahLuke = Filters.and(Filters.icon(Icon.DAGOBAH), Filters.Luke);
+        final Filter effectFilter = Filters.and(Filters.Effect, Filters.deploysForFree, Filters.always_immune_to_Alter);
+        // Allow response(s)
+        if (GameConditions.canSpot(game, self, Filters.and(Filters.your(self), Filters.icon(Icon.DAGOBAH), Filters.Objective))) {
+            action.allowResponses("Take Yoda, [Dagobah] Luke, and two Effects that deploy for free and are always immune to Alter into hand from Reserve Deck",
+                    new RespondablePlayCardEffect(action) {
+                        @Override
+                        protected void performActionResults(Action targetingAction) {
+                            // Perform result(s)
+                            action.appendEffect(
+                                    new TakeCardAndCardIntoHandFromReserveDeckEffect(action, playerId, Filters.Yoda, dagobahLuke, false));
+                            action.appendEffect(
+                                    new TakeCardsIntoHandFromReserveDeckEffect(action, playerId, 1, 2, effectFilter, false));
+                            action.appendEffect(
+                                    new ModifyNumCardsDrawnInStartingHandEffect(action, playerId, 5));
+                            action.appendEffect(
+                                    new PutCardFromVoidInLostPileEffect(action, playerId, self));
                         }
-                        action.appendTargeting(
-                                new ChooseCardOnTableEffect(action, playerId, "Choose location", Filters.in(validLocations)) {
-                                    @Override
-                                    protected void cardSelected(final PhysicalCard location) {
-                                        action.addAnimationGroup(location);
-                                        if (!forFree) {
-                                            // Pay cost(s)
-                                            action.appendCost(
-                                                    new UseForceEffect(action, playerId, 1));
-                                        }
-                                        // Allow response(s)
-                                        action.allowResponses("Relocate " + GameUtils.getCardLink(character) + " to " + GameUtils.getCardLink(location),
-                                                new RespondablePlayCardEffect(action) {
-                                                    @Override
-                                                    protected void performActionResults(Action targetingAction) {
-                                                        // Perform result(s)
-                                                        action.appendEffect(
-                                                                new RelocateBetweenLocationsEffect(action, character, location));
-                                                    }
-                                                }
-                                        );
-                                    }
-                                });
                     }
-                }
-        );
+            );
+        }
+        return action;
     }
 }
