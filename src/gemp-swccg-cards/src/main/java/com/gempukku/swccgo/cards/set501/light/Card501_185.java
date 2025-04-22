3907825432549a8c401bcc5277c7b39e700ae1f4
Jared Greenwald <jared.greenwald@oracle.com>
2025-04-21 22:51:53 -0400

    initial commit

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_185.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_185.java
@@ -2,8 +2,7 @@ package com.gempukku.swccgo.cards.set501.light;
 
 import com.gempukku.swccgo.cards.AbstractAlien;
 import com.gempukku.swccgo.cards.AbstractPermanentWeapon;
-import com.gempukku.swccgo.cards.conditions.OnCloudCityCondition;
-import com.gempukku.swccgo.cards.conditions.WithCondition;
+import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
@@ -17,12 +16,15 @@ import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.game.state.BattleState;
+import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.FireWeaponAction;
 import com.gempukku.swccgo.logic.actions.FireWeaponActionBuilder;
-import com.gempukku.swccgo.logic.conditions.AndCondition;
-import com.gempukku.swccgo.logic.modifiers.AddsBattleDestinyModifier;
+import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
+import com.gempukku.swccgo.logic.effects.RetrieveForceEffect;
 import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.timing.EffectResult;
 
 import java.util.Collections;
 import java.util.LinkedList;
@@ -38,30 +40,27 @@ import java.util.List;
 
 public class Card501_185 extends AbstractAlien {
     public Card501_185() {
-        super(Side.LIGHT, 1, 4, 3, 3, 5, "Lando With Lando’s Blaster Rifle", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setLore("Smuggler, gambler and leader. 'Yeah, I'm responsible these days. It's the price you pay for being successful.'");
-        setGameText("Adds 2 to power of anything he pilots. Adds one battle destiny if with your other alien on Cloud City. Permanent weapon is blaster pistol (may target a character for free; draw destiny; target hit, and its forfeit = 0, if destiny +1 > defense value).");
+        super(Side.LIGHT, 1, 4, 3, 3, 5, "Lando With Lando's Blaster Rifle", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
+        setLore("Works every time. Smuggler.");
+        setGameText("[Pilot] 2. Permanent weapon is •Lando's Blaster Rifle (may target a character for free; draw destiny; target hit, and its forfeit = 0, if destiny +1 > defense value). During battle, if your total battle destiny is odd, may retrieve 1 Force (2 if total is 11).");
         addPersona(Persona.LANDO);
         addIcons(Icon.CLOUD_CITY, Icon.PILOT, Icon.WARRIOR, Icon.PERMANENT_WEAPON, Icon.VIRTUAL_SET_25);
-        addKeywords(Keyword.SMUGGLER, Keyword.GAMBLER, Keyword.LEADER);
+        addKeywords(Keyword.SMUGGLER);
         setVirtualSuffix(true);
         setTestingText("Lando With Lando's Blaster Rifle (V)");
-        hideFromDeckBuilder();
     }
 
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(new AddsPowerToPilotedBySelfModifier(self, 2));
-        modifiers.add(new AddsBattleDestinyModifier(self, new AndCondition(new WithCondition(self, Filters.and(Filters.your(self),
-                Filters.other(self), Filters.alien)), new OnCloudCityCondition(self)), 1));
         return modifiers;
     }
 
     // Define "Blaster Pistol" permanent weapon
     @Override
     protected AbstractPermanentWeapon getGameTextPermanentWeapon() {
-        AbstractPermanentWeapon permanentWeapon = new AbstractPermanentWeapon("blaster pistol") {
+        AbstractPermanentWeapon permanentWeapon = new AbstractPermanentWeapon("blaster rifle") {
             @Override
             public List<FireWeaponAction> getGameTextFireWeaponActions(String playerId, SwccgGame game, PhysicalCard self, boolean forFree, int extraForceRequired, PhysicalCard sourceCard, boolean repeatedFiring, Filter targetedAsCharacter, Float defenseValueAsCharacter, Filter fireAtTargetFilter, boolean ignorePerAttackOrBattleLimit) {
                 FireWeaponActionBuilder actionBuilder = FireWeaponActionBuilder.startBuildPrep(playerId, game, sourceCard, self, this, forFree, extraForceRequired, repeatedFiring, targetedAsCharacter, defenseValueAsCharacter, fireAtTargetFilter, ignorePerAttackOrBattleLimit)
@@ -77,6 +76,24 @@ public class Card501_185 extends AbstractAlien {
         };
         permanentWeapon.addKeyword(Keyword.BLASTER);
         return permanentWeapon;
+    }
+
+    @Override
+    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, final EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
+        if (TriggerConditions.isBattleDestinyDrawingJustCompletedForPlayer(game, effectResult, playerId)
+                && GameConditions.hasLostPile(game, playerId)) {
+            final BattleState battleState = game.getGameState().getBattleState();
+            final float totalBattleDestiny = battleState.getTotalBattleDestiny(game, playerId);
+            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId);
+            if (totalBattleDestiny == 11) {
+                action.appendEffect(
+                        new RetrieveForceEffect(action, playerId, 2));
+            } else if (totalBattleDestiny % 2 == 1) {
+                action.appendEffect(
+                        new RetrieveForceEffect(action, playerId, 1));
+            }
+        }
+        return null;
     }
 }
 
