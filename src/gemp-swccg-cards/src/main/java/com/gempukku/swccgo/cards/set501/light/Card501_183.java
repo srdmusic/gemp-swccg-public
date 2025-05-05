e14a85fd24eff8d28d97206351aa384b1d4e6204
Jared Greenwald <jared.greenwald@oracle.com>
2025-05-05 08:02:33 -0400

    updates

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_183.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_183.java
@@ -2,9 +2,7 @@ package com.gempukku.swccgo.cards.set501.light;
 
 import com.gempukku.swccgo.cards.AbstractRebel;
 import com.gempukku.swccgo.cards.GameConditions;
-import com.gempukku.swccgo.cards.conditions.ArmedWithCondition;
 import com.gempukku.swccgo.cards.conditions.InBattleWithCondition;
-import com.gempukku.swccgo.cards.conditions.PilotingCondition;
 import com.gempukku.swccgo.cards.effects.usage.OncePerTurnEffect;
 import com.gempukku.swccgo.cards.evaluators.ConditionEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
@@ -24,13 +22,13 @@ import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.conditions.Condition;
-import com.gempukku.swccgo.logic.conditions.OrCondition;
 import com.gempukku.swccgo.logic.effects.LoseForceEffect;
 import com.gempukku.swccgo.logic.effects.choose.DeployCardToTargetFromLostPileEffect;
 import com.gempukku.swccgo.logic.effects.choose.DeployCardToTargetFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
 import com.gempukku.swccgo.logic.modifiers.DrawsBattleDestinyIfUnableToOtherwiseModifier;
+import com.gempukku.swccgo.logic.modifiers.MayNotBeTargetedByModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
@@ -49,7 +47,7 @@ public class Card501_183 extends AbstractRebel {
     public Card501_183() {
         super(Side.LIGHT, 1, 4, 4, 3, 7, "Captain Han Solo", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Smuggler and scoundrel. His piloting abilities have become legend in the Rebellion. Intends to leave the Alliance to pay off Jabba the Hutt.");
-        setGameText("[Pilot] 3. While armed or piloting Falcon, draws one battle destiny if unable to otherwise (two if also with Leia or Chewie). Once per turn, if Han at a site, may [download] (or lose 1 Force to deploy from Lost Pile) a blaster on him. If just lost or captured, may [upload] [CC] Lando.");
+        setGameText("[Pilot] 3. Draws one battle destiny if unable to otherwise (two if with Chewie or Leia). Vader's gametext may not target Han. May [download] Han's Heavy Blaster Pistol (or lose 1 Force to deploy from Lost Pile) on Han. If just lost or captured, may [upload] [Cloud City] Lando.");
         addIcons(Icon.CLOUD_CITY, Icon.PILOT, Icon.WARRIOR, Icon.VIRTUAL_SET_25);
         addKeywords(Keyword.SMUGGLER, Keyword.CAPTAIN);
         addPersona(Persona.HAN);
@@ -61,11 +59,11 @@ public class Card501_183 extends AbstractRebel {
 
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
-        Condition armedOrPilotingFalcon = new OrCondition(new ArmedWithCondition(self, Filters.any), new PilotingCondition(self, Filters.Falcon));
         Condition inBattleWithLeiaOrChewie = new InBattleWithCondition(self, Filters.or(Filters.Leia, Filters.Chewie));
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(new AddsPowerToPilotedBySelfModifier(self, 3));
-        modifiers.add(new DrawsBattleDestinyIfUnableToOtherwiseModifier(self, armedOrPilotingFalcon, new ConditionEvaluator(1, 2, inBattleWithLeiaOrChewie)));
+        modifiers.add(new DrawsBattleDestinyIfUnableToOtherwiseModifier(self, new ConditionEvaluator(1, 2, inBattleWithLeiaOrChewie)));
+        modifiers.add(new MayNotBeTargetedByModifier(self, Filters.Vader));
         return modifiers;
     }
 
@@ -111,6 +109,24 @@ public class Card501_183 extends AbstractRebel {
         // Check condition(s)
         if ((TriggerConditions.captured(game, effectResult, Filters.sameCardId(self))
                 || TriggerConditions.isAboutToBeCaptured(game, effectResult, Filters.sameCardId(self)))
+                && GameConditions.canTakeCardsIntoHandFromReserveDeck(game, playerId, self, gameTextActionId)) {
+            Filter ccLando = Filters.and(Filters.icon(Icon.CLOUD_CITY), Filters.Lando);
+            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
+            action.setText("Take [Cloud City] Lando into hand from Reserve Deck");
+            // Perform result(s)
+            action.appendEffect(
+                    new TakeCardIntoHandFromReserveDeckEffect(action, playerId, ccLando, true));
+            return Collections.singletonList(action);
+        }
+        return null;
+    }
+
+    @Override
+    protected List<OptionalGameTextTriggerAction> getGameTextLeavesTableOptionalTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.CAPTAIN_HAN_SOLO_V__UPLOAD_LANDO;
+
+        // Check condition(s)
+        if (TriggerConditions.justLost(game, effectResult, self)
                 && GameConditions.canTakeCardsIntoHandFromReserveDeck(game, playerId, self, gameTextActionId)) {
             Filter ccLando = Filters.and(Filters.icon(Icon.CLOUD_CITY), Filters.Lando);
             final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
