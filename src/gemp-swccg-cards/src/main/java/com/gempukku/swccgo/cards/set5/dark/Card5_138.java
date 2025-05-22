3fbffaa12a917e38f287311947fce3c05699c279
Greg Zinn <greg.zinn@gmail.com>
2025-05-21 22:44:50 -0400

    Insult Function

---
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set5/dark/Card5_138.java
@@ -0,0 +1,96 @@
+package com.gempukku.swccgo.cards.set5.dark;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import com.gempukku.swccgo.cards.AbstractUsedInterrupt;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.GameUtils;
+import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
+import com.gempukku.swccgo.logic.effects.CancelGameTextEffect;
+import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
+import com.gempukku.swccgo.logic.effects.SendMessageEffect;
+import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
+import com.gempukku.swccgo.logic.timing.Action;
+
+/**
+ * Set: Cloud City
+ * Type: Interrupt
+ * Subtype: Used
+ * Title: E Chu Ta
+ */
+
+public class Card5_138 extends AbstractUsedInterrupt {
+    public Card5_138() {
+        super(Side.DARK, 3, "E Chu Ta", Uniqueness.UNIQUE, ExpansionSet.CLOUD_CITY, Rarity.C);
+        setLore("'How rude!'");
+        setGameText("One of your protocol droids 'insults' one character present, canceling that character's game text for remainder of turn. OR During your battle phase, initiate a battle where both players have droids (but no presence or spies). Both sides add one battle destiny.");
+        addIcons(Icon.CLOUD_CITY);
+    }
+
+        @Override
+    protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self) {
+        List<PlayInterruptAction> actions = new LinkedList<PlayInterruptAction>();
+
+        Filter opponentsCharacterPresent = Filters.and(Filters.opponents(self), Filters.character, Filters.presentAt(Filters.any));
+        Filter yourProtocolDroidWithOpponentsCharacterPresent = Filters.and(Filters.your(playerId), Filters.protocol_droid, Filters.with(self, opponentsCharacterPresent));
+
+        // Check condition(s)
+        if (GameConditions.canTarget(game, self, yourProtocolDroidWithOpponentsCharacterPresent)) {
+            final PlayInterruptAction action = new PlayInterruptAction(game, self);
+            action.setText("'Insult' a character");
+            // Choose target(s)
+            action.appendTargeting(
+                    new TargetCardOnTableEffect(action, playerId, "Choose your protocol droid", yourProtocolDroidWithOpponentsCharacterPresent) {
+                        @Override
+                        protected void cardTargeted(final int targetGroupId1, final PhysicalCard yourProtocolDroid) {
+                            action.appendTargeting(
+                                new TargetCardOnTableEffect(action, playerId, "Choose opponent's character", Filters.and(opponentsCharacterPresent, Filters.with(yourProtocolDroid))) {
+                                        @Override
+                                        protected void cardTargeted(final int targetGroupId2, final PhysicalCard opponentsCharacter) {
+                                            action.addAnimationGroup(yourProtocolDroid, opponentsCharacter);
+
+                                            // Allow response(s)
+                                            action.allowResponses("Target " + GameUtils.getCardLink(yourProtocolDroid) + " and " + GameUtils.getCardLink(opponentsCharacter),
+                                                    new RespondablePlayCardEffect(action) {
+                                                        @Override
+                                                        protected void performActionResults(Action targetingAction) {
+                                                            // Get the targeted card(s) from the action using the targetGroupId.
+                                                            // This needs to be done in case the target(s) were changed during the responses.
+                                                            final PhysicalCard yourFinalTarget = targetingAction.getPrimaryTargetCard(targetGroupId1);
+                                                            final PhysicalCard opponentsFinalTarget = targetingAction.getPrimaryTargetCard(targetGroupId2);
+                                                            
+                                                            // Send Easter egg
+                                                            action.appendCost(new SendMessageEffect(action, GameUtils.getFullName(yourFinalTarget) + ": E chu ta."));
+                                                            action.appendCost(new SendMessageEffect(action, GameUtils.getFullName(opponentsFinalTarget) + ": How rude!"));
+
+                                                            // Perform result(s)
+                                                            action.appendEffect(new CancelGameTextEffect(action, opponentsFinalTarget));
+                                                        }
+                                                    }
+                                            );
+
+
+                                        }
+                                }
+                            );
+                        }
+                    }
+            );
+            actions.add(action);
+        }
+        
+        
+        return actions;
+    }
+
+}
