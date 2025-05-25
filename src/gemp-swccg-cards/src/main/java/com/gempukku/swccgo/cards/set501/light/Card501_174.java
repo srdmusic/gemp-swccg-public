e252f75c36d6f7c5b0c6b987bb6877cc2ad86f17
Jared Greenwald <jared.greenwald@oracle.com>
2025-05-25 13:37:30 -0400

    update finn resistance hero

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_174.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_174.java
@@ -1,29 +1,36 @@
 package com.gempukku.swccgo.cards.set501.light;
 
 import com.gempukku.swccgo.cards.AbstractResistance;
-import com.gempukku.swccgo.cards.conditions.AloneAtCondition;
+import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.conditions.HereCondition;
 import com.gempukku.swccgo.cards.conditions.OutOfPlayCondition;
+import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
 import com.gempukku.swccgo.common.Persona;
+import com.gempukku.swccgo.common.Phase;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.game.state.GameState;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.conditions.Condition;
 import com.gempukku.swccgo.logic.conditions.OrCondition;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.modifiers.AddsDestinyToPowerModifier;
-import com.gempukku.swccgo.logic.modifiers.ForceDrainsMayNotBeCanceledModifier;
-import com.gempukku.swccgo.logic.modifiers.ForceDrainsMayNotBeModifiedModifier;
 import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionLessThanModifier;
 import com.gempukku.swccgo.logic.modifiers.MayDeployToTargetModifier;
 import com.gempukku.swccgo.logic.modifiers.MayUseWeaponModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.ModifiersQuerying;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -37,7 +44,7 @@ public class Card501_174 extends AbstractResistance {
     public Card501_174() {
         super(Side.LIGHT, 1, 4, 4, 4, 6, "Finn, Resistance Hero", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Leader.");
-        setGameText("During battle, if Luke out of play (or Rose or Jannah here), adds one destiny to total power. While alone, opponent may not cancel or reduce Force drains at same [E7] battleground. Jedi Lightsaber may deploy on Finn. Immune to attrition < 4.");
+        setGameText("During battle, if Luke or Paige out of play (or Rose or Jannah here), adds one destiny to total power. Once per game, may [download] a non-leader Resistance character of lesser ability here. Jedi Lightsaber may deploy on Finn. Immune to attrition < 4.");
         addPersona(Persona.FINN);
         addIcons(Icon.EPISODE_VII, Icon.WARRIOR, Icon.VIRTUAL_SET_25);
         addKeywords(Keyword.LEADER);
@@ -46,18 +53,46 @@ public class Card501_174 extends AbstractResistance {
 
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
-        Condition lukeOutOfPlay = new OutOfPlayCondition(self, Filters.Luke);
+        Condition lukeOrJannahOutOfPlay = new OutOfPlayCondition(self, Filters.or(Filters.Luke, Filters.Jannah));
         Condition roseOrJannahHere = new HereCondition(self, Filters.or(Filters.Rose, Filters.Jannah));
-        Condition aloneAtEVIIBG = new AloneAtCondition(self, Filters.and(Filters.icon(Icon.EPISODE_VII), Filters.battleground));
-        String opponent = game.getOpponent(self.getOwner());
 
         List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new AddsDestinyToPowerModifier(self, new OrCondition(lukeOutOfPlay, roseOrJannahHere), 1));
-        modifiers.add(new ForceDrainsMayNotBeCanceledModifier(self, Filters.here(self), aloneAtEVIIBG, opponent, self.getOwner()));
-        modifiers.add(new ForceDrainsMayNotBeModifiedModifier(self, Filters.here(self), aloneAtEVIIBG, opponent, self.getOwner()));
+        modifiers.add(new AddsDestinyToPowerModifier(self, new OrCondition(lukeOrJannahOutOfPlay, roseOrJannahHere), 1));
         modifiers.add(new MayDeployToTargetModifier(self, Filters.Jedi_Lightsaber, self));
         modifiers.add(new MayUseWeaponModifier(self, Filters.Jedi_Lightsaber));
         modifiers.add(new ImmuneToAttritionLessThanModifier(self, 4));
         return modifiers;
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, final SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.FINN_RESISTANCE_HERO__DOWNLOAD_CHARACTER;
+
+        // Check condition(s)
+        if (GameConditions.isOncePerGame(game, self, gameTextActionId)
+                && GameConditions.isDuringYourPhase(game, self, Phase.DEPLOY)
+                && GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId)) {
+            
+            Filter abilityLessThanSelf = new Filter() {
+                @Override
+                public boolean accepts(GameState gameState, ModifiersQuerying modifiersQuerying, PhysicalCard physicalCard) {
+                    return modifiersQuerying.getAbility(gameState, physicalCard) < modifiersQuerying.getAbility(gameState, self);
+                }
+            };
+
+            Filter downloadFilter = Filters.and(Filters.resistance, Filters.character, Filters.not(Filters.leader), abilityLessThanSelf);
+
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
+            action.setText("Deploy Resistance character from Reserve Deck");
+            action.setActionMsg("Deploy non-leader Resistance character of lesser ability from Reserve Deck");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerGameEffect(action));
+            // Perform result(s)
+            action.appendEffect(
+                    new DeployCardFromReserveDeckEffect(action, downloadFilter, true));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
