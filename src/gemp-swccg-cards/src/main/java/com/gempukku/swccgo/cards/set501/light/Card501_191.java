ca9711676765fad0f5838932b7cd009a9cc4c280
Greg Zinn <greg.zinn@gmail.com>
2025-06-12 16:21:55 -0400

    How Liberty Dies template

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_191.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_191.java
@@ -7,6 +7,7 @@ import com.gempukku.swccgo.common.PlayCardOptionId;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
@@ -20,15 +21,10 @@ import com.gempukku.swccgo.game.SwccgGame;
  */
 public class Card501_191 extends AbstractEpicEventDeployable {
     public Card501_191() {
-        super(Side.LIGHT, PlayCardZoneOption.ATTACHED, "How Liberty Des", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setGameText("Deploy on Galactic Senate. Your Political Effects are canceled. Twice per turn, may target your agenda here:\n" +
-                    "justice: during battle, subtract 1 from a just drawn weapon destiny.\n" +
-                    "order: during any move phase, peek at the top 2 cards of any Reserve Deck and replace in any order;\n" +
-                    "peace: during battle, subtract 1 from attrition against you;\n" +
-                    "taxation: place a card from hand on Used Pile to make the next [E1] character you deploy this turn deploy -1.");
-        addIcons(Icon.CORUSCANT, Icon.VIRTUAL_SET_25);
+        super(Side.LIGHT, PlayCardZoneOption.ATTACHED, Title.How_Liberty_Dies, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
+        setGameText("Deploy on Galactic Senate. Your Political Effects are canceled. Twice per turn, may target your agenda here: Justice: During battle, subtract 1 from a just drawn weapon destiny. Order: During any move phase, peek at the top 2 cards of any Reserve Deck and replace in any order. Peace: Subtract 1 from attrition against you. Taxation: Place an [Episode I] character from hand on Used Pile; the next [Episode I] character you deploy this turn is cumulatively deploy -1.");
+        addIcons(Icon.CORUSCANT, Icon.EPISODE_I, Icon.VIRTUAL_SET_25);
         setTestingText("How Liberty Dies");
-        hideFromDeckBuilder();
     }
 
     @Override
--- src/gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/Title.java
+++ src/gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/Title.java
@@ -514,6 +514,7 @@ public interface Title {
     String Hoth_Sentry = "Hoth Sentry";
     String Houjix = "Houjix";
     String How_Did_We_Get_Into_This_Mess = "How Did We Get Into This Mess?";
+    String How_Liberty_Dies = "How Liberty Dies";
     String Human_Shield = "Human_Shield";
     String Hunt_Down_And_Destroy_The_Jedi = "Hunt Down And Destroy The Jedi";
     String Hunt_For_The_Droid_General = "Hunt For The Droid General";
