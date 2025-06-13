59771be28e2408603df791e3c1ca86909b8b7f84
Greg Zinn <greg.zinn@gmail.com>
2025-06-12 22:14:25 -0400

    New Leadership Is Needed

--- src/gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/Title.java
+++ src/gemp-swccg-common/src/main/java/com/gempukku/swccgo/common/Title.java
@@ -784,6 +784,7 @@ public interface Title {
     String Nevar_Yalnal = "Nevar Yalnal";
     String Nevarro = "Nevarro";
     String Never_Tell_Me_The_Odds = "Never Tell Me The Odds";
+    String New_Leadership_Is_Needed = "New Leadership Is Needed";
     String Nien_Nunb = "Nien Nunb";
     String Nightclub = "Coruscant: Night Club";
     String Nightfall = "Nightfall";
