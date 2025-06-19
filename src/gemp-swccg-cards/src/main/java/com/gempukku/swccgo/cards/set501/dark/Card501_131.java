218af04c7deda65967a58cfa065be55dd4552a80
Greg Zinn <greg.zinn@gmail.com>
2025-06-18 22:45:44 -0400

    Check for hit by weapon firing repeatedtly

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_131.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_131.java
@@ -5,6 +5,9 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.gempukku.swccgo.cards.AbstractCharacterWeapon;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.AddDestinyToAttritionEffect;
+import com.gempukku.swccgo.cards.effects.AddDestinyToTotalPowerEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.PlayCardOptionId;
@@ -17,10 +20,13 @@ import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.FireWeaponAction;
 import com.gempukku.swccgo.logic.actions.FireWeaponActionBuilder;
+import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
 import com.gempukku.swccgo.logic.modifiers.MayFireRepeatedlyModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.timing.EffectResult;
 
 /**
  * Set: Playtesting
@@ -68,5 +74,36 @@ public class Card501_131 extends AbstractCharacterWeapon {
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(new MayFireRepeatedlyModifier(self, 1));
         return modifiers;
+    }
+
+    @Override
+    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, PhysicalCard self, int gameTextSourceCardId) {
+        final List<OptionalGameTextTriggerAction> actions = new LinkedList<>();
+
+        // Check condition(s)
+        if (TriggerConditions.justHitBy(game, effectResult, Filters.any, self, Filters.IG88)
+                && TriggerConditions.weaponJustFiredRepeatedly(game, effectResult, self)
+                && GameConditions.isDuringBattle(game)) {
+
+            // Check more condition(s)
+            if (GameConditions.canAddDestinyDrawsToPower(game, playerId)) {
+                final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, playerId, gameTextSourceCardId);
+                action.setText("Add one destiny to total power");
+                // Perform result(s)
+                action.appendEffect(
+                        new AddDestinyToTotalPowerEffect(action, 1));
+                actions.add(action);
+            }
+            // Check more condition(s)
+            if (GameConditions.canAddDestinyDrawsToAttrition(game, playerId)) {
+                final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, playerId, gameTextSourceCardId);
+                action.setText("Add one destiny to attrition");
+                // Perform result(s)
+                action.appendEffect(
+                        new AddDestinyToAttritionEffect(action, 1));
+                actions.add(action);
+            }
+        }
+        return actions;
     }
 }
