078e950983df7a09e323f7cce6f4fa207ddcbc55
Greg Zinn <greg.zinn@gmail.com>
2025-06-20 11:58:48 -0400

    Overload reorder - fewer parameter version first

--- src/gemp-swccg-logic/src/main/java/com/gempukku/swccgo/logic/effects/HitCardAndResetForfeitEffect.java
+++ src/gemp-swccg-logic/src/main/java/com/gempukku/swccgo/logic/effects/HitCardAndResetForfeitEffect.java
@@ -44,6 +44,19 @@ public class HitCardAndResetForfeitEffect extends AbstractSubActionEffect implem
      * @param hitByCard the card the card was hit by
      * @param hitByPermanentWeapon the permanent weapon that hit the card
      * @param cardFiringWeapon the card that fired the weapon
+     */
+    public HitCardAndResetForfeitEffect(Action action, PhysicalCard cardHitAndReset, float resetValue, PhysicalCard hitByCard, SwccgBuiltInCardBlueprint hitByPermanentWeapon, PhysicalCard cardFiringWeapon) {
+        this(action, cardHitAndReset, resetValue, hitByCard, hitByPermanentWeapon, cardFiringWeapon, false);
+    }
+
+    /**
+     * Creates an effect to 'hit' a card and resets its forfeit value.
+     * @param action the action performing this effect
+     * @param cardHitAndReset the card that is hit and whose forfeit value is reset
+     * @param resetValue the reset value
+     * @param hitByCard the card the card was hit by
+     * @param hitByPermanentWeapon the permanent weapon that hit the card
+     * @param cardFiringWeapon the card that fired the weapon
      * @param hitByRepeatedFiring true if the weapon was firing repeatedly when it hit
      */
     public HitCardAndResetForfeitEffect(Action action, PhysicalCard cardHitAndReset, float resetValue, PhysicalCard hitByCard, SwccgBuiltInCardBlueprint hitByPermanentWeapon, PhysicalCard cardFiringWeapon, boolean hitByRepeatedFiring) {
@@ -55,19 +68,6 @@ public class HitCardAndResetForfeitEffect extends AbstractSubActionEffect implem
         _cardFiringWeapon = cardFiringWeapon;
         _hitByRepeatedFiring = hitByRepeatedFiring;
         _that = this;
-    }
-
-    /**
-     * Creates an effect to 'hit' a card and resets its forfeit value.
-     * @param action the action performing this effect
-     * @param cardHitAndReset the card that is hit and whose forfeit value is reset
-     * @param resetValue the reset value
-     * @param hitByCard the card the card was hit by
-     * @param hitByPermanentWeapon the permanent weapon that hit the card
-     * @param cardFiringWeapon the card that fired the weapon
-     */
-    public HitCardAndResetForfeitEffect(Action action, PhysicalCard cardHitAndReset, float resetValue, PhysicalCard hitByCard, SwccgBuiltInCardBlueprint hitByPermanentWeapon, PhysicalCard cardFiringWeapon) {
-        this(action, cardHitAndReset, resetValue, hitByCard, hitByPermanentWeapon, cardFiringWeapon, false);
     }
 
     @Override
