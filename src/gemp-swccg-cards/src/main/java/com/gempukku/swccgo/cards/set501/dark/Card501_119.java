50dbbc875a1e1e0aae31d36df84a3634c64ed63c
Greg Zinn <greg.zinn@gmail.com>
2025-07-07 17:14:10 -0400

    Rework card

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_119.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_119.java
@@ -6,23 +6,19 @@ import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
-import com.gempukku.swccgo.common.TargetingReason;
 import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
-import com.gempukku.swccgo.logic.effects.DrawDestinyEffect;
-import com.gempukku.swccgo.logic.effects.HitCardEffect;
+import com.gempukku.swccgo.logic.effects.AddUntilEndOfBattleModifierEffect;
 import com.gempukku.swccgo.logic.effects.ModifyTotalBattleDestinyEffect;
-import com.gempukku.swccgo.logic.effects.ModifyTotalPowerUntilEndOfBattleEffect;
 import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
-import com.gempukku.swccgo.logic.effects.choose.ChooseCardEffect;
+import com.gempukku.swccgo.logic.modifiers.NumberOfBattleDestinyDrawsMayNotBeLimitedForEitherPlayerModifier;
 import com.gempukku.swccgo.logic.timing.Action;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -41,68 +37,51 @@ public class Card501_119 extends AbstractUsedInterrupt {
 
     @Override
     protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self) {
-        TargetingReason targetingReason = TargetingReason.TO_BE_HIT;
-        Filter opponentsCharacterInBattleFilter = Filters.and(Filters.opponents(self), Filters.character, Filters.participatingInBattle);
 
+        Filter systemRelatedToBattle = Filters.relatedSystemTo(self, Filters.battleLocation);
+
+        // Check condition(s)
         if (GameConditions.isDuringBattleAt(game, Filters.site)
-                && GameConditions.controlsWith(game, self, playerId, Filters.relatedSystemTo(self, Filters.battleLocation), Filters.and(Icon.FIRST_ORDER, Filters.capital_starship))
-                && GameConditions.canTarget(game, self, targetingReason, opponentsCharacterInBattleFilter)) {
+                && GameConditions.occupies(game, playerId, systemRelatedToBattle)
+                && GameConditions.occupiesWith(game, self, playerId, systemRelatedToBattle, Filters.or(Filters.Dreadnaught, Filters.Star_Destroyer))) {
 
             final PlayInterruptAction action = new PlayInterruptAction(game, self);
 
-            String opponent = game.getOpponent(playerId);
-            String actionMessage;
-
-            final int drawX; //number of destiny to draw (as in "draw X choose Y")
-            if (GameConditions.controlsWith(game, self, playerId, Filters.relatedSystemTo(self, Filters.battleLocation), Filters.Fulminatrix)) {
-                drawX = 2;
-                actionMessage = "Draw two destiny and choose one";
-            } else {
-                drawX = 1;
-                actionMessage = "Draw destiny";
+            // Check more condition(s)
+            if (GameConditions.controls(game, playerId, systemRelatedToBattle)
+                    || GameConditions.occupiesWith(game, self, playerId, systemRelatedToBattle, Filters.Fulminatrix)) {
+                
+                action.setText("Add 2 to total battle destiny and prevent limits");
+                // Allow response(s)
+                action.allowResponses("Add 2 to total battle destiny and prevent the number of battle destiny draws from being limited",
+                        new RespondablePlayCardEffect(action) {
+                            @Override
+                            protected void performActionResults(Action targetingAction) {
+                                // Perform result(s)
+                                action.appendEffect(
+                                    new ModifyTotalBattleDestinyEffect(action, playerId, 2));
+                                action.appendEffect(
+                                        new AddUntilEndOfBattleModifierEffect(action,
+                                                new NumberOfBattleDestinyDrawsMayNotBeLimitedForEitherPlayerModifier(self, Filters.battleLocation),
+                                                "Prevents the number of battle destiny draws from being limited"));
+                            }
+                        });
             }
-
-            action.setText(actionMessage);
-            action.setActionMsg(actionMessage);
-            action.allowResponses(actionMessage,
-                    new RespondablePlayCardEffect(action) {
-                        @Override
-                        protected void performActionResults(Action targetingAction) {
-                            action.appendEffect(new DrawDestinyEffect(action, playerId, drawX, 1) {
-                                @Override
-                                protected void destinyDraws(final SwccgGame game, List<PhysicalCard> destinyCardDraws, List<Float> destinyDrawValues, final Float totalDestiny) {
-                                    if (totalDestiny >= 0 && totalDestiny <= 2) {
-                                        // (0-2) your total battle destiny is +1
-                                        action.appendEffect(
-                                                new ModifyTotalBattleDestinyEffect(action, playerId, 1));
-                                    } else if (totalDestiny >= 3 && totalDestiny <= 5) {
-                                        // (3-5) opponent's total power is -2;
-                                        action.appendEffect(
-                                                new ModifyTotalPowerUntilEndOfBattleEffect(action, -2, opponent, "Subtracts 2 from total power"));
-                                    } else if (totalDestiny >= 5) {
-                                        // (6+) opponent's character is 'hit' (your choice)
-                                        final List<PhysicalCard> cardsToHitOptions = new LinkedList<PhysicalCard>();
-                                        cardsToHitOptions.addAll(Filters.filterActive(game, self, opponentsCharacterInBattleFilter));
-                                        if (!cardsToHitOptions.isEmpty()) {
-                                            action.appendEffect(
-                                                new ChooseCardEffect(action, playerId, "Choose character to hit", cardsToHitOptions) {
-                                                    @Override
-                                                    protected void cardSelected(PhysicalCard selectedCard) {
-                                                        action.appendEffect(new HitCardEffect(action, selectedCard, self));
-                                                    }
-                                                }
-                                            );
-                                        }
-                                    } else {
-                                        game.getGameState().sendMessage("Result: No effect");
-                                    }
-                                }
-                            });
-                        }
-                    });
+            else {
+                action.setText("Add 1 to total battle destiny");
+                // Allow response(s)
+                action.allowResponses("Add 1 to total battle destiny",
+                        new RespondablePlayCardEffect(action) {
+                            @Override
+                            protected void performActionResults(Action targetingAction) {
+                                // Perform result(s)
+                                action.appendEffect(
+                                    new ModifyTotalBattleDestinyEffect(action, playerId, 1));
+                            }
+                        });
+            }
             return Collections.singletonList(action);
         }
-
         return null;
     }
 }
