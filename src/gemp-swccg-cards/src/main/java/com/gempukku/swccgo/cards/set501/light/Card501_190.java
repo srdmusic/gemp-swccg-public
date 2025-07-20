f7c6b13eef4b40e7a2376359dc3966c8e87e6a8c
Greg Zinn <greg.zinn@gmail.com>
2025-07-20 16:27:27 -0400

    Game text update

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_190.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_190.java
@@ -39,7 +39,7 @@ public class Card501_190 extends AbstractLostInterrupt {
     public Card501_190() {
         super(Side.LIGHT, 5, "Honoring What They Fight For", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Luke's experience on Dagobah gave him great skill in using the Force. Vader had to keep his focus on Luke at all times, or face the consequences.");
-        setGameText("At the start of your turn, if a [Cloud City] Rebel controls a battleground, turn a card stacked on Patience! face up. OR If a [Cloud City] Rebel in battle, add one battle destiny. OR Place a [Cloud City] Rebel (except Luke) out of play from hand to cancel all battle damage against you.");
+        setGameText("If a [Cloud City] Rebel controls a battleground, turn a card stacked on Patience! face up. OR Place a card stacked on Patience! out of play to choose: if a [Cloud City] Rebel in battle, add one battle destiny. OR Take a character weapon into hand from Lost Pile.");
         addIcons(Icon.CLOUD_CITY, Icon.VIRTUAL_SET_25);
         setTestingText("Honoring What They Fight For");
     }
