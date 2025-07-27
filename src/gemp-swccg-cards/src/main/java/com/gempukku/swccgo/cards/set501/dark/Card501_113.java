432c7625ae3f00880a0563e1796088b1b9f910c7
Greg Zinn <greg.zinn@gmail.com>
2025-07-27 13:41:34 -0400

    No longer relocated Tracked Fleet

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_113.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_113.java
@@ -3,34 +3,26 @@ package com.gempukku.swccgo.cards.set501.dark;
 import com.gempukku.swccgo.cards.AbstractCapitalStarship;
 import com.gempukku.swccgo.cards.AbstractPermanentAboard;
 import com.gempukku.swccgo.cards.AbstractPermanentPilot;
-import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.conditions.HasAboardCondition;
 import com.gempukku.swccgo.cards.conditions.HereCondition;
-import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
 import com.gempukku.swccgo.cards.evaluators.ConditionEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
-import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.ModelType;
 import com.gempukku.swccgo.common.Persona;
-import com.gempukku.swccgo.common.Phase;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
-import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.conditions.Condition;
-import com.gempukku.swccgo.logic.effects.AttachCardFromTableEffect;
 import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionLessThanModifier;
 import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -74,30 +66,5 @@ public class Card501_113 extends AbstractCapitalStarship {
         modifiers.add(new ImmuneToAttritionLessThanModifier(self, new ConditionEvaluator(10, 12, snokeAboard)));
         modifiers.add(new ImmuneToAttritionModifier(self, trackedFleetHere));
         return modifiers;
-    }
-
-    @Override
-    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
-        Filter relocatableTrackedFleet = Filters.and(Filters.Tracked_Fleet, Filters.not(Filters.here(self)));
-        GameTextActionId gameTextActionId = GameTextActionId.SUPREMACY__RELOCATE_TRACKED_FLEET;
-        // Check condition(s)
-        if (GameConditions.isOncePerGame(game, self, gameTextActionId)
-                && GameConditions.isDuringYourPhase(game, self, Phase.MOVE)
-                && GameConditions.canSpot(game, self, relocatableTrackedFleet)) {
-
-            PhysicalCard trackedFleet = Filters.findFirstActive(game, self, relocatableTrackedFleet);
-            PhysicalCard thisSystem = Filters.findFirstActive(game, self, Filters.sameSystem(self));
-            // Build action using common utility)
-            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
-            action.setText("Relocate Tracked Fleet here");
-            // Append usage limit(s)
-            action.appendUsage(
-                new OncePerGameEffect(action));
-            // Perform result(s)
-            action.appendEffect(
-                    new AttachCardFromTableEffect(action, trackedFleet, thisSystem));
-            return Collections.singletonList(action);
-        }
-        return null;
     }
 }
