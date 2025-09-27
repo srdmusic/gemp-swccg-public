fe7c05118e2990521eaedd6a97b3df487431c2c7
Greg Zinn <greg.zinn@gmail.com>
2025-09-27 02:24:24 -0400

    Make sites horizontal

--- src/gemp-swccg-async/src/main/web/js/gemp-016/cards/Card.js
+++ src/gemp-swccg-async/src/main/web/js/gemp-016/cards/Card.js
@@ -452,6 +452,11 @@ class Card {
                    (cardNo == 36) ||
                    (cardNo == 38) ||
                    (cardNo == 39) ||
+                   (cardNo == 62) ||
+                   (cardNo == 63) ||
+                   (cardNo == 64) ||
+                   (cardNo == 65) ||
+                   (cardNo == 66) ||
                    (cardNo == 164) ||
                    (cardNo == 171);
         }
