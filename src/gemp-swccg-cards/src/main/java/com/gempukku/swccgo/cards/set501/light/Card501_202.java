ee3bc974c77b79888180386f37c0c5cb06e191da
Greg Zinn <greg.zinn@gmail.com>
2025-09-28 15:16:24 -0400

    hideFromDeckBuilder

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_202.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_202.java
@@ -20,6 +20,7 @@ public class Card501_202 extends AbstractEpicEventDeployable {
         super(Side.LIGHT, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, Title.Fallen_Order, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setGameText("If The Hidden Path on table, deploy on table and stack three Jedi Survivors from Reserve Deck here. Once per turn, may deploy your Jedi Survivor from here as if from hand. If your Jedi Survivor is about to be lost from table, stack it here. The Light Will Fade: While The Hidden Path on table, your Jedi Survivors may not be targeted by weapons and their game text is canceled. But It Is Never Forgotten: During your draw phase, if Gather Allies And Train on table and no Jedi Survivors are stacked here, may retrieve 1 Force.");
         addIcons(Icon.VIRTUAL_SET_26);
+        hideFromDeckBuilder();
     }
 
 }
