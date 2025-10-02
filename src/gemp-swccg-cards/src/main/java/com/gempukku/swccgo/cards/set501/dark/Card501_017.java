3d59bfa7b42ea31990fcb9e2abdc182baf7200f3
Greg Zinn <greg.zinn@gmail.com>
2025-10-02 01:51:06 -0400

    Salacrious Crumb V

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_017.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_017.java
@@ -1,36 +1,13 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
 import com.gempukku.swccgo.cards.AbstractAlien;
-import com.gempukku.swccgo.cards.GameConditions;
-import com.gempukku.swccgo.cards.effects.ConvertLocationByRaisingToTopEffect;
-import com.gempukku.swccgo.cards.effects.usage.OncePerTurnEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
-import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Species;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
-import com.gempukku.swccgo.filters.Filter;
-import com.gempukku.swccgo.filters.Filters;
-import com.gempukku.swccgo.game.PhysicalCard;
-import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.GameUtils;
-import com.gempukku.swccgo.logic.TriggerConditions;
-import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
-import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
-import com.gempukku.swccgo.logic.effects.PlaceCardInUsedPileFromTableEffect;
-import com.gempukku.swccgo.logic.effects.SendMessageEffect;
-import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
-import com.gempukku.swccgo.logic.effects.choose.DrawCardIntoHandFromReserveDeckEffect;
-import com.gempukku.swccgo.logic.modifiers.MayNotHaveGameTextCanceledModifier;
-import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.timing.EffectResult;
 
 /**
  * Set: Playtesting
@@ -42,95 +19,11 @@ public class Card501_017 extends AbstractAlien {
     public Card501_017() {
         super(Side.DARK, 3, 1, 1, 1, 3, Title.Salacious_Crumb, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Male Kowakian. Prankster. Humiliates others for Jabba's amusement. His life depends on making Jabba laugh at least once per day.");
-        setGameText("When deployed, may draw top card of Reserve Deck or place a droid present in owner's Used Pile ('AH-hahahaha!'). Game text of your alien leaders here may not be canceled. Once per turn, if at a converted Jabba's Palace site, may raise yours to the top.");
-        addIcons(Icon.JABBAS_PALACE, Icon.VIRTUAL_SET_25);
+        setGameText("When deployed, may shuffle opponent's Reserve Deck or search your Force Pile and take any one card into hand; reshuffle. Unless Crumb is 'hit', Jabba may not be targeted by weapons here. Undercover droids here are lost. ('AH-hahahaha!')");
+        addIcons(Icon.JABBAS_PALACE, Icon.VIRTUAL_SET_26);
         setSpecies(Species.KOWAKIAN);
         setVirtualSuffix(true);
         setTestingText("Salacious Crumb (V)");
     }
 
-    @Override
-    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, final SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
-
-        List<OptionalGameTextTriggerAction> actions = new LinkedList<>();
-
-        //Both possible responses share a gameTextActionId so that the player can only perform one of them
-        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
-        Filter droidPresent = Filters.and(Filters.droid, Filters.present(self));
-
-        // Check condition(s)
-        if (TriggerConditions.justDeployed(game, effectResult, self)
-                && GameConditions.hasReserveDeck(game, playerId)) {
-
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-            
-            action.setText("Draw top card of Reserve Deck");
-            action.setActionMsg("Draw top card of Reserve Deck into hand");
-            // Perform result(s)
-            action.appendEffect(
-                    new DrawCardIntoHandFromReserveDeckEffect(action, playerId));
-            actions.add(action);
-        }
-
-        // Check condition(s)
-        if (TriggerConditions.justDeployed(game, effectResult, self)
-                && GameConditions.canTarget(game, self, droidPresent)) {
-
-            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-            
-            action.setText("Place a droid in owner's Used Pile");
-            action.setActionMsg("Place a droid present in owner's Used Pile");
-            // Choose target(s)
-            action.appendTargeting(
-              new ChooseCardOnTableEffect(action, playerId, "Choose a droid present", Filters.and(Filters.droid, Filters.present(self))) {
-                        @Override
-                        protected void cardSelected(PhysicalCard selectedCard) {
-                            // Send Easter Egg Message
-                            action.appendCost(
-                                new SendMessageEffect(action, "Salacious Crumb: AH-hahahaha!"));
-                            // Perform result(s)
-                            action.appendEffect(
-                                    new PlaceCardInUsedPileFromTableEffect(action, selectedCard));
-                        }
-              }
-            );
-            actions.add(action);
-        }
-
-        return actions;
-    }
-
-    @Override
-    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<>();
-        Filter yourAlienLeadersHere = Filters.and(Filters.your(self), Filters.alien, Filters.leader, Filters.here(self));
-        modifiers.add(new MayNotHaveGameTextCanceledModifier(self, yourAlienLeadersHere));
-        return modifiers;
-    }
-
-    @Override
-    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(String playerId, SwccgGame game, PhysicalCard self, int gameTextSourceCardId) {
-        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
-        // Check condition(s)
-        if (GameConditions.isAtLocation(game, self, Filters.and(Filters.canBeConvertedByRaisingYourLocationToTop(playerId), Filters.Jabbas_Palace_site))
-                && GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)) {
-
-            final PhysicalCard location = game.getModifiersQuerying().getLocationThatCardIsAt(game.getGameState(), self);
-            if (location != null) {
-
-                final TopLevelGameTextAction action = new TopLevelGameTextAction(self, gameTextSourceCardId, gameTextActionId);
-                action.setText("Raise converted site to the top");
-                action.setActionMsg("Raise converted site to the top to convert " + GameUtils.getCardLink(location));
-                action.addAnimationGroup(location);
-                // Update usage limit(s)
-                action.appendUsage(
-                        new OncePerTurnEffect(action));
-                // Perform result(s)
-                action.appendEffect(
-                        new ConvertLocationByRaisingToTopEffect(action, location, true));
-                return Collections.singletonList(action);
-            }
-        }
-        return null;
-    }
 }
