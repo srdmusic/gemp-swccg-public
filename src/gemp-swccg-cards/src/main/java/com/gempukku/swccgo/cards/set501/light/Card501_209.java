fa52df025202c36fceefc1893fe4e543eb76a974
Greg Zinn <greg.zinn@gmail.com>
2025-10-04 14:10:39 -0400

    Basic template

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_209.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_209.java
@@ -20,12 +20,11 @@ public class Card501_209 extends AbstractRebel {
     public Card501_209() {
         super(Side.LIGHT, 1, 8, 6, 6, 8, "Obi-Wan Kenobi, Jedi In Exile", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Jedi survivor.");
-        setGameText("Adds one battle destiny with Maul, Sidious, or Vader. Opponent's characters here are power and forfeit -1. Once per game may [upload] (or retrieve into hand) Glancing Blow or Help Me Obi-Wan Kenobi. Immune to You Are Beaten and attrition < 6.");
+        setGameText("Adds one battle destiny with Maul, Sidious, or Vader. Opponent's characters here are power and forfeit -1. Once per game, may [upload] (or retrieve into hand) Glancing Blow or Help Me Obi-Wan Kenobi. Immune to You Are Beaten and attrition < 6.");
         addKeyword(Keyword.JEDI_SURVIVOR);
         addPersona(Persona.OBIWAN);
         addIcons(Icon.PILOT, Icon.WARRIOR, Icon.VIRTUAL_SET_26);
         setTestingText("Obi-Wan Kenobi, Jedi In Exile");
-        hideFromDeckBuilder();
     }
     
 }
