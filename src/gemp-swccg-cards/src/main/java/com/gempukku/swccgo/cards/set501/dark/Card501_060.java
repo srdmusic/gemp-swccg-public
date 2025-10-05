f76bdacaff702df8f3be8705932e7a71bee0e039
Jared Greenwald <jared.greenwald@oracle.com>
2025-10-04 23:58:05 -0400

    Guri (V)

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_060.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_060.java
@@ -1,6 +1,10 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractDroid;
+import com.gempukku.swccgo.cards.conditions.GameTextModificationCondition;
+import com.gempukku.swccgo.cards.conditions.HitCondition;
+import com.gempukku.swccgo.cards.conditions.OnTableCondition;
+import com.gempukku.swccgo.cards.conditions.PresentWithCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
@@ -9,6 +13,21 @@ import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.conditions.AndCondition;
+import com.gempukku.swccgo.logic.conditions.Condition;
+import com.gempukku.swccgo.logic.conditions.NotCondition;
+import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
+import com.gempukku.swccgo.logic.modifiers.DrawsBattleDestinyIfUnableToOtherwiseModifier;
+import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionLessThanModifier;
+import com.gempukku.swccgo.logic.modifiers.MayNotBeTargetedByWeaponsModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.ModifyGameTextType;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Set: Playtesting
@@ -27,7 +46,22 @@ public class Card501_060 extends AbstractDroid {
         addModelTypes(ModelType.ASSASSIN);
         setVirtualSuffix(true);
         setTestingText("Guri (V)");
-        hideFromDeckBuilder();
     }
 
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new AddsPowerToPilotedBySelfModifier(self, 2));
+        modifiers.add(new DrawsBattleDestinyIfUnableToOtherwiseModifier(self, 1));
+        Condition notHit = new NotCondition(new HitCondition(self));
+        Condition modifierXizorCondition = new AndCondition(new PresentWithCondition(self, Filters.Xizor), 
+            new NotCondition(new GameTextModificationCondition(self, ModifyGameTextType.LEGACY__TREAT_XIZOR_AS_SHADA)), notHit);
+        modifiers.add(new MayNotBeTargetedByWeaponsModifier(self, Filters.Xizor, modifierXizorCondition));
+        Condition modifierShadaCondition = new AndCondition(new PresentWithCondition(self, Filters.title("Shada")), 
+            new GameTextModificationCondition(self, ModifyGameTextType.LEGACY__TREAT_XIZOR_AS_SHADA), notHit);
+        modifiers.add(new MayNotBeTargetedByWeaponsModifier(self, Filters.title("Shada"), modifierShadaCondition));
+        Condition yourRef2ObjectiveOnTable = new OnTableCondition(self, Filters.and(Filters.your(self), Icon.REFLECTIONS_II, Filters.Objective));
+        modifiers.add(new ImmuneToAttritionLessThanModifier(self, yourRef2ObjectiveOnTable, 5));
+        return modifiers;
+    }
 }
