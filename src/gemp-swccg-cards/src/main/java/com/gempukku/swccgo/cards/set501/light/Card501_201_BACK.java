877e3b90ca7fa5a9afc15e91cac0e71fdaab6ee1
Greg Zinn <greg.zinn@gmail.com>
2025-10-10 17:25:22 -0400

    Battle destiny -1 or -2

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_201_BACK.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_201_BACK.java
@@ -4,16 +4,20 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.gempukku.swccgo.cards.AbstractObjective;
+import com.gempukku.swccgo.cards.evaluators.ConditionEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.conditions.InBattleCondition;
 import com.gempukku.swccgo.logic.modifiers.ForceDrainModifiersMayNotBeCanceledModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
+import com.gempukku.swccgo.logic.modifiers.TotalBattleDestinyModifier;
 
 /**
  * Set: Playtesting
@@ -24,7 +28,7 @@ import com.gempukku.swccgo.logic.modifiers.Modifier;
 public class Card501_201_BACK extends AbstractObjective {
     public Card501_201_BACK() {
         super(Side.LIGHT, 7, Title.Gather_Allies_And_Train, ExpansionSet.PLAYTESTING, Rarity.V);
-        setGameText("While this side up, Force drain bonuses from your lightsabers may not be canceled. Opponent's total battle destiny where they have a character of ability > 4 is -1 (-2 if an Inquisitor there). If your holocron is about to leave table, place it in Used Pile. When you initiate battle with a Jedi, you may retrieve 1 Force. During your move phase, may relocate a Jedi between a Jabiim site and a battleground site as a regular move. At the end of your turn, opponent loses 1 Force. Flip this card if Jedi do not occupy two locations.");
+        setGameText("While this side up, Force drain bonuses from your lightsabers may not be canceled. Opponent's total battle destiny where they have a character of ability > 4 is -1 (-2 if an Inquisitor). If your holocron is about to leave table, place it in Used Pile. When you initiate battle with a Jedi, you may retrieve 1 Force. During your move phase, may relocate a Jedi between a Jabiim site and a battleground site as a regular move. At the end of your turn, opponent loses 1 Force. Flip this card if Jedi do not occupy two locations.");
         addIcons(Icon.VIRTUAL_SET_26);
         setTestingText("Gather Allies And Train");
     }
@@ -34,8 +38,18 @@ public class Card501_201_BACK extends AbstractObjective {
         List<Modifier> modifiers = new LinkedList<Modifier>();
 
         String playerId = self.getOwner();
+        String opponent = game.getOpponent(playerId);
         
         modifiers.add(new ForceDrainModifiersMayNotBeCanceledModifier(self, Filters.and(Filters.your(playerId), Filters.lightsaber)));
+
+        Filter opponentsHighAbilityCharacter = Filters.and(Filters.opponents(self), Filters.character, Filters.abilityMoreThan(4));
+        Filter opponentsHighAbilityInquisitor = Filters.and(opponentsHighAbilityCharacter, Filters.inquisitor);
+
+        modifiers.add(new TotalBattleDestinyModifier(self, 
+                new InBattleCondition(self, opponentsHighAbilityCharacter),
+                new ConditionEvaluator(-1, -2, new InBattleCondition(self, opponentsHighAbilityInquisitor)),
+                opponent, true));
+
         return modifiers;
     }    
 }
