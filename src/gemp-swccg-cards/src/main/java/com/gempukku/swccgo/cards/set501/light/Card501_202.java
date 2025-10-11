c325808e260b37494132f78748e42bc9a49205ae
Greg Zinn <greg.zinn@gmail.com>
2025-10-11 02:04:03 -0400

    Fallen Order template

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_202.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_202.java
@@ -18,10 +18,9 @@ import com.gempukku.swccgo.common.Uniqueness;
 public class Card501_202 extends AbstractEpicEventDeployable {
     public Card501_202() {
         super(Side.LIGHT, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, Title.Fallen_Order, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setGameText("If The Hidden Path on table, deploy on table and stack three Jedi Survivors from Reserve Deck here. Once per turn, may deploy your Jedi Survivor from here as if from hand. If your Jedi Survivor is about to be lost from table, stack it here. The Light Will Fade: While The Hidden Path on table, your Jedi Survivors may not be targeted by weapons and their game text is canceled. But It Is Never Forgotten: During your draw phase, if Gather Allies And Train on table and no Jedi Survivors are stacked here, may retrieve 1 Force.");
+        setGameText("If The Hidden Path on table, deploy on table and stack three Jedi Survivors from Reserve Deck here. During your deploy phase, may deploy a Jedi survivor from here as if from hand. If your Jedi Survivor is about to be lost from table, stack it here. The Light Will Fade: While The Hidden Path on table, your Jedi Survivors may not be targeted by weapons and their game text is canceled. But It Is Never Forgotten: During your draw phase, if Gather Allies And Train on table and no Jedi Survivors are stacked here, may retrieve 1 Force.");
         addIcons(Icon.VIRTUAL_SET_26);
         setTestingText("Fallen Order");
-        hideFromDeckBuilder();
     }
 
 }
