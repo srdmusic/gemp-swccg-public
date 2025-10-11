b7543305669f3b69b16fe46e2e3c075a893c4188
Greg Zinn <greg.zinn@gmail.com>
2025-10-11 01:36:12 -0400

    Relocate Jedi

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_201_BACK.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_201_BACK.java
@@ -1,13 +1,17 @@
 package com.gempukku.swccgo.cards.set501.light;
 
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.gempukku.swccgo.cards.AbstractObjective;
 import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.usage.OncePerPhaseEffect;
 import com.gempukku.swccgo.cards.evaluators.ConditionEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Phase;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
@@ -19,15 +23,20 @@ import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
 import com.gempukku.swccgo.logic.actions.RequiredGameTextTriggerAction;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.conditions.InBattleCondition;
 import com.gempukku.swccgo.logic.effects.PlaceCardInUsedPileFromTableEffect;
+import com.gempukku.swccgo.logic.effects.RelocateBetweenLocationsEffect;
 import com.gempukku.swccgo.logic.effects.RetrieveForceEffect;
+import com.gempukku.swccgo.logic.effects.UnrespondableEffect;
+import com.gempukku.swccgo.logic.effects.choose.ChooseCardOnTableEffect;
 import com.gempukku.swccgo.logic.modifiers.ForceDrainModifiersMayNotBeCanceledModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 import com.gempukku.swccgo.logic.modifiers.TotalBattleDestinyModifier;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 import com.gempukku.swccgo.logic.timing.PassthruEffect;
 import com.gempukku.swccgo.logic.timing.results.AboutToLeaveTableResult;
+import com.gempukku.swccgo.logic.timing.Action;
 
 /**
  * Set: Playtesting
@@ -107,6 +116,74 @@ public class Card501_201_BACK extends AbstractObjective {
             actions.add(action);
         }
 
+        return actions;
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, final SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        List<TopLevelGameTextAction> actions = new LinkedList<TopLevelGameTextAction>();
+
+        // A Jedi who can relocate from a battleground site to a Jabiim site
+        Filter jediValidFromBattleground = Filters.and(Filters.Jedi, Filters.presentAt(Filters.battleground_site), Filters.canBeRelocatedToLocation(Filters.Jabiim_site, true, 0), Filters.hasNotPerformedRegularMove);
+        // A Jedi who can relocate from a Jabiim site to a battleground site
+        Filter jediValidFromJabiim = Filters.and(Filters.Jedi, Filters.presentAt(Filters.Jabiim_site), Filters.canBeRelocatedToLocation(Filters.battleground_site, true, 0), Filters.hasNotPerformedRegularMove);
+
+        Filter jediValidToRelocate = Filters.or(jediValidFromBattleground, jediValidFromJabiim);
+
+        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+
+        // Check condition(s)
+        if (GameConditions.canSpot(game, self, jediValidToRelocate)
+                && GameConditions.isOnceDuringYourPhase(game, self, playerId, gameTextSourceCardId, gameTextActionId, Phase.MOVE)) {
+
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerId, gameTextSourceCardId, gameTextActionId);
+            action.setText("Relocate a Jedi");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerPhaseEffect(action));
+            // Perform result(s)
+            action.appendTargeting(
+                    new ChooseCardOnTableEffect(action, playerId, "Choose Jedi to relocate", jediValidToRelocate) {
+                        @Override
+                        protected void cardSelected(final PhysicalCard jediSelected) {
+                            Filter validDestination = Filters.none;
+                            if (jediValidFromBattleground.accepts(game, jediSelected) && jediValidFromJabiim.accepts(game, jediSelected)) {
+                                validDestination = Filters.or(Filters.battleground_site, Filters.Jabiim_site);
+                            }
+                            else if (jediValidFromBattleground.accepts(game, jediSelected)) {
+                                validDestination = Filters.Jabiim_site;
+                            }
+                            else if (jediValidFromJabiim.accepts(game, jediSelected)) {
+                                validDestination = Filters.battleground_site;
+                            }
+
+                            Collection<PhysicalCard> destinationSites = Filters.filterTopLocationsOnTable(game, Filters.and(validDestination, Filters.locationCanBeRelocatedTo(jediSelected, true, 0)));
+
+                            action.appendTargeting(
+                                    new ChooseCardOnTableEffect(action, playerId, "Choose site to relocate " + GameUtils.getCardLink(jediSelected) + " to", Filters.in(destinationSites)) {
+                                        @Override
+                                        protected void cardSelected(final PhysicalCard siteSelected) {
+                                            action.addAnimationGroup(self);
+                                            action.addAnimationGroup(jediSelected);
+                                            // Allow response(s)
+                                            action.allowResponses("Relocate " + GameUtils.getCardLink(jediSelected) + " to " + GameUtils.getCardLink(siteSelected),
+                                                    new UnrespondableEffect(action) {
+                                                        @Override
+                                                        protected void performActionResults(Action targetingAction) {
+                                                            // Perform result(s)
+                                                            action.appendEffect(
+                                                                    new RelocateBetweenLocationsEffect(action, jediSelected, siteSelected, true));
+                                                        }
+                                                    }
+                                            );
+                                        }
+                                    }
+                            );
+                        }
+                    }
+            );
+            actions.add(action);
+        }
         return actions;
     }
 }
