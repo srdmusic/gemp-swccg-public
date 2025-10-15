d4480680545db318b4f0d94cd8a9584a067054c2
Greg Zinn <greg.zinn@gmail.com>
2025-10-15 18:08:25 -0400

    TDIGWATT V front and back

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066.java
@@ -1,11 +1,27 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
+import com.gempukku.swccgo.cards.AbstractObjective;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
+
 /**
  * Set: Playtesting
  * Type: Objective
  * Title: This Deal Is Getting Worse All The Time / Pray I Don't Alter It Any Further (V)
  */
 
-public class Card501_066 {
+public class Card501_066 extends AbstractObjective {
+    public Card501_066() {
+        super(Side.DARK, 0, Title.This_Deal_Is_Getting_Worse_All_The_Time, ExpansionSet.PLAYTESTING, Rarity.V);
+        setFrontOfDoubleSidedCard(true);
+        setGameText("Deploy one Cloud City battleground site and I'm Sorry. For remainder of game, Surreptitious Glance may not cancel Dark Deal and your Bespin system may not be converted. While this side up, once per turn, may [upload] Bespin system, Dark Deal, or Cloud City Occupation. Flip this card if Dark Deal (V) on table, you occupy Bespin System and opponent controls less than 3 Cloud City battlegrounds.");
+        addIcons(Icon.CLOUD_CITY, Icon.PREMIUM, Icon.VIRTUAL_SET_26);
+        setVirtualSuffix(true);
+        setTestingText("This Deal Is Getting Worse All The Time (V)");
+        hideFromDeckBuilder();
+    }
     
 }
--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066_BACK.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066_BACK.java
@@ -1,11 +1,26 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
+import com.gempukku.swccgo.cards.AbstractObjective;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
+
 /**
  * Set: Playtesting
  * Type: Objective
  * Title: This Deal Is Getting Worse All The Time / Pray I Don't Alter It Any Further (V)
  */
 
-public class Card501_066_BACK {
+public class Card501_066_BACK extends AbstractObjective {
+    public Card501_066_BACK() {
+        super(Side.DARK, 7, Title.Pray_I_Dont_Alter_It_Any_Further, ExpansionSet.PLAYTESTING, Rarity.V);
+        setGameText("While this side up, It’s a Trap! is canceled. If you have an alien/Imperial pair in battle, your total battle destiny is +2 (or +4 if alien is an Ugnaught). Your Force drain bonuses at same site as your Lando or your Lobot may not be canceled. While Vader present at a Cloud City site, Sense and Alter are canceled. While Executor at Bespin, Cloud City Occupation may not be canceled. Flip this card if opponent controls Bespin system or 3 Cloud City battlegrounds.");
+        addIcons(Icon.CLOUD_CITY, Icon.PREMIUM, Icon.VIRTUAL_SET_26);
+        setVirtualSuffix(true);
+        setTestingText("Pray I Don't Alter It Any Further (V)");
+        hideFromDeckBuilder();
+    }
     
 }
