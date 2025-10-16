542d54aa7120489ffd40e9aba3ddba2393772b27
Greg Zinn <greg.zinn@gmail.com>
2025-10-16 00:39:26 -0400

    Kashyyyk Chewie's Hut

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_213.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_213.java
@@ -1,5 +1,13 @@
 package com.gempukku.swccgo.cards.set501.light;
 
+import com.gempukku.swccgo.cards.AbstractSite;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.common.Uniqueness;
+
 /**
  * Set: Playtesting
  * Type: Location
@@ -7,6 +15,16 @@ package com.gempukku.swccgo.cards.set501.light;
  * Title: Kashyyyk: Chewie's Hut
  */
 
-public class Card501_213 {
+public class Card501_213 extends AbstractSite {
+    public Card501_213() {
+        super(Side.LIGHT, "Kashyyyk: Chewie's Hut", Title.Kashyyyk, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
+        setLocationLightSideGameText("Deploys only as a starting location. While Wookiee Homestead on table, no Force Drains here.");
+        setLocationDarkSideGameText("Until the end of your first turn, you must first use 1 Force to deploy a character to a Kashyyyk site.");
+        addIcon(Icon.LIGHT_FORCE, 2);
+        addIcon(Icon.DARK_FORCE, 0);
+        addIcons(Icon.INTERIOR_SITE, Icon.PLANET, Icon.EPISODE_I, Icon.VIRTUAL_SET_26);
+        setTestingText("Kashyyyk: Chewie's Hut");
+        hideFromDeckBuilder();
+    }
     
 }
