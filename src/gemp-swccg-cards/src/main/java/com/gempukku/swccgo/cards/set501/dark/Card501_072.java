83cd3639287b71f3b45cdb536f706bdaca9856d0
Greg Zinn <greg.zinn@gmail.com>
2025-10-16 00:13:55 -0400

    Bespin V Errata

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_072.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_072.java
@@ -1,5 +1,12 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
+import com.gempukku.swccgo.cards.AbstractSystem;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Title;
+
 /**
  * Set: Playtesting
  * Type: Location
@@ -8,6 +15,17 @@ package com.gempukku.swccgo.cards.set501.dark;
  * Errata E1 of Card223_008.java
  */
 
-public class Card501_072 {
+public class Card501_072 extends AbstractSystem {
+    public Card501_072() {
+        super(Side.DARK, Title.Bespin, 6, ExpansionSet.PLAYTESTING, Rarity.V);
+        setLocationDarkSideGameText("If your [Cloud City] objective on table, Executor is deploy = 7 here (and may not move from here unless Vader aboard).");
+        setLocationLightSideGameText("You lose no more than 2 Force to Cloud City Occupation. Intensify the Forward Batteries is canceled.");
+        addIcon(Icon.DARK_FORCE, 2);
+        addIcon(Icon.LIGHT_FORCE, 1);
+        addIcons(Icon.SPECIAL_EDITION, Icon.PLANET, Icon.VIRTUAL_SET_23);
+        setVirtualSuffix(true);
+        setTestingText("Bespin (V)");
+        hideFromDeckBuilder();
+    }
     
 }
