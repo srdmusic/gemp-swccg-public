4b98cc0b2888538192de96d1eea728ec6f597bf9
Greg Zinn <greg.zinn@gmail.com>
2025-10-15 20:17:53 -0400

    Garindan Imperial Spy

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_068.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_068.java
@@ -1,5 +1,14 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
+import com.gempukku.swccgo.cards.AbstractAlienImperial;
+import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Keyword;
+import com.gempukku.swccgo.common.Rarity;
+import com.gempukku.swccgo.common.Side;
+import com.gempukku.swccgo.common.Species;
+import com.gempukku.swccgo.common.Uniqueness;
+
 /**
  * Set: Playtesting
  * Type: Character
@@ -7,6 +16,17 @@ package com.gempukku.swccgo.cards.set501.dark;
  * Title: Garindan, Imperial Spy
  */
 
-public class Card501_068 {
+public class Card501_068 extends AbstractAlienImperial {
+    public Card501_068() {
+        super(Side.DARK, 4, 2, 1, 1, 3, "Garindan, Imperial Spy", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
+        setLore("Long-nosed, male Kubaz from Kubindi. Spy. Squealed on Obi-Wan and Luke outside Docking Bay 94. Works for Jabba the Hutt or the highest bidder. Not particularly brave.");
+        setGameText("May deploy as a 'react.' Imperials move to here for free using landspeed. Unless Garindan 'hit,' may place him in Used Pile to cancel a just drawn weapon destiny targeting another character here (or to make an Undercover spy here lost).");
+        addIcons(Icon.VIRTUAL_SET_26);
+        addKeywords(Keyword.SPY);
+        setSpecies(Species.KUBAZ);
+        setVirtualSuffix(true);
+        setTestingText("Garindan, Imperial Spy");
+        hideFromDeckBuilder();
+    }
     
 }
