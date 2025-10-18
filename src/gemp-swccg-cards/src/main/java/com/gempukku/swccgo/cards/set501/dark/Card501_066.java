bbbcde9f01601588e8cc3e796fbb76428b88c1c0
Greg Zinn <greg.zinn@gmail.com>
2025-10-18 19:41:07 -0400

    Deploy starting cards

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066.java
@@ -1,11 +1,16 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractObjective;
+import com.gempukku.swccgo.cards.actions.ObjectiveDeployedTriggerAction;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardFromReserveDeckEffect;
 
 /**
  * Set: Playtesting
@@ -22,5 +27,24 @@ public class Card501_066 extends AbstractObjective {
         setVirtualSuffix(true);
         setTestingText("This Deal Is Getting Worse All The Time (V)");
     }
-    
+
+    @Override
+    protected ObjectiveDeployedTriggerAction getGameTextWhenDeployedAction(final String playerId, final SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        ObjectiveDeployedTriggerAction action = new ObjectiveDeployedTriggerAction(self);
+        action.appendRequiredEffect(
+                new DeployCardFromReserveDeckEffect(action, Filters.Cloud_City_battleground_site, true, false) {
+                    @Override
+                    public String getChoiceText() {
+                        return "Choose Cloud City battleground site to deploy";
+                    }
+                });
+        action.appendRequiredEffect(
+                new DeployCardFromReserveDeckEffect(action, Filters.Im_Sorry, true, false) {
+                    @Override
+                    public String getChoiceText() {
+                        return "Choose I'm Sorry to deploy";
+                    }
+                });
+        return action;
+    }
 }
