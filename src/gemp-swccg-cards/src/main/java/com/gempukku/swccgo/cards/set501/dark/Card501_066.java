13181611e6a528b02b21ae29a77dda7cb7e7a315
Greg Zinn <greg.zinn@gmail.com>
2025-10-18 19:36:30 -0400

    Latest text and unhide

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066.java
@@ -17,11 +17,10 @@ public class Card501_066 extends AbstractObjective {
     public Card501_066() {
         super(Side.DARK, 0, Title.This_Deal_Is_Getting_Worse_All_The_Time, ExpansionSet.PLAYTESTING, Rarity.V);
         setFrontOfDoubleSidedCard(true);
-        setGameText("Deploy one Cloud City battleground site and I'm Sorry. For remainder of game, Surreptitious Glance may not cancel Dark Deal and your Bespin system may not be converted. While this side up, once per turn, may [upload] Bespin system, Dark Deal, or Cloud City Occupation. Flip this card if Dark Deal (V) on table, you occupy Bespin System and opponent controls less than 3 Cloud City battlegrounds.");
+        setGameText("Deploy one Cloud City battleground site and I'm Sorry. For remainder of game, Surreptitious Glance may not cancel Dark Deal and your Bespin system may not be converted. While this side up, once per turn, may [upload] Cloud City Occupation, Dark Deal, or [Special Edition] Bespin system. Flip this card if [Set 23] Dark Deal on table, you occupy Bespin System, and opponent controls less than 3 Cloud City battlegrounds.");
         addIcons(Icon.CLOUD_CITY, Icon.PREMIUM, Icon.VIRTUAL_SET_26);
         setVirtualSuffix(true);
         setTestingText("This Deal Is Getting Worse All The Time (V)");
-        hideFromDeckBuilder();
     }
     
 }
