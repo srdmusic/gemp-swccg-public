65f7e8bb66926edafc9f1c4b4ab1b492aac492e5
Greg Zinn <greg.zinn@gmail.com>
2025-10-19 00:12:34 -0400

    Minor proofing text change

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_068.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_068.java
@@ -47,7 +47,7 @@ public class Card501_068 extends AbstractAlienImperial {
     public Card501_068() {
         super(Side.DARK, 4, 2, 1, 1, 3, "Garindan, Imperial Spy", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Long-nosed, male Kubaz from Kubindi. Spy. Squealed on Obi-Wan and Luke outside Docking Bay 94. Works for Jabba the Hutt or the highest bidder. Not particularly brave.");
-        setGameText("May deploy as a 'react.' Imperials move to here for free using landspeed. Unless Garindan 'hit,' may place him in Used Pile to cancel a just drawn weapon destiny targeting your other character here (or to make an Undercover spy here lost).");
+        setGameText("May deploy as a 'react.' Imperials move to here for free using landspeed. Unless 'hit,' may place Garindan in Used Pile to cancel a just drawn weapon destiny targeting your other character here (or to make an Undercover spy here lost).");
         addIcons(Icon.VIRTUAL_SET_26);
         addKeywords(Keyword.SPY);
         addPersona(Persona.GARINDAN);
