e74d244ca96b026da904173141c89d223460c886
Greg Zinn <greg.zinn@gmail.com>
2025-10-19 04:05:38 -0400

    Updated game text

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_061.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_061.java
@@ -38,7 +38,7 @@ public class Card501_061 extends AbstractNormalEffect {
     public Card501_061() {
         super(Side.DARK, 5, PlayCardZoneOption.YOUR_SIDE_OF_TABLE, Title.Information_Exchange, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("'Chisa nyooda ishaley. Kun Jabba neguda len malta.' 'Ikkit ui! Yobbit, yobbit. Nelan tui ke bada.'");
-        setGameText("Deploy on table. Black Sun agents are defense value +1 and forfeit +1. Once per turn, if you just deployed an information broker, may exchange the top card of Force Pile with any one card in hand. Guri and [Reflections II] Emperor deploy -2 and move for free. [Immune to Alter.]");
+        setGameText("Deploy on table. Black Sun agents are defense value +1 and forfeit +1. Once per turn, if you just deployed an information broker, may exchange the top card of Force Pile with any one card in hand. [Reflections II] Emperor deploys -1 and moves for free. [Immune to Alter.]");
         addIcons(Icon.JABBAS_PALACE, Icon.REFLECTIONS_II, Icon.VIRTUAL_SET_26);
         setVirtualSuffix(true);
         setTestingText("Information Exchange (V)");
