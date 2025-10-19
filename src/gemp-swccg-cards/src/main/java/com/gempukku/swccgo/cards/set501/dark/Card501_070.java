2c088110194fdebdeda24da0c5b79f59dce39bcd
Greg Zinn <greg.zinn@gmail.com>
2025-10-19 14:38:44 -0400

    Latest text and unhide

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_070.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_070.java
@@ -20,14 +20,13 @@ import com.gempukku.swccgo.common.Uniqueness;
 public class Card501_070 extends AbstractAlienImperial {
     public Card501_070() {
         super(Side.DARK, 1, 5, 4, 5, 7, Title.Mara_Jade_The_Emperors_Hand, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setLore("Spy. Ordered to kill Luke Skywalker. Assumed the identity of a dancer named 'Arica' in order to sneak into Jabba's Palace.");
-        setGameText("Deploys +2 to a non-battleground. May [download] Mara's Lightsaber here. While Revenge Of The Sith on table, gains [Sith]. If you just won a battle here, may retrieve a card with 'Mara' in game text. Immune to attrition < 4 (< 5 if alone or no Jedi here, < 6 if both).");
+        setLore("Spy. Ordered to kill Luke Skywalker. Assumed the identity of a dancer named 'Arica' in order to sneak into Jabba's palace.");
+        setGameText("Deploys +2 to non-battleground locations. May ▼ Mara Jade's Lightsaber here. While Revenge Of The Sith on table, gains [Sith]. If you just won a battle here, may retrieve a card with 'Mara' in game text. Immune to attrition < 4 (< 5 if alone or no Jedi here, < 6 if both).");
         addPersona(Persona.MARA_JADE);
         addIcons(Icon.PREMIUM, Icon.PILOT, Icon.WARRIOR, Icon.DEATH_STAR_II, Icon.VIRTUAL_SET_26);
         addKeywords(Keyword.SPY, Keyword.FEMALE);
         setVirtualSuffix(true);
         setTestingText("Mara Jade, The Emperor's Hand (V)");
-        hideFromDeckBuilder();
     }
     
 }
