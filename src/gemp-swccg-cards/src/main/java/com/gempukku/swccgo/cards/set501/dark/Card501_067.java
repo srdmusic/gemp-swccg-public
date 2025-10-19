12ff5ef609bc36322400a500104a77e93888a7b7
Greg Zinn <greg.zinn@gmail.com>
2025-10-19 01:26:15 -0400

    Card puller and modifiers

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_067.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_067.java
@@ -4,7 +4,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.gempukku.swccgo.cards.AbstractNormalEffect;
+import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.usage.OncePerTurnEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.PlayCardOptionId;
 import com.gempukku.swccgo.common.PlayCardZoneOption;
@@ -15,6 +18,10 @@ import com.gempukku.swccgo.common.Uniqueness;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardToLocationFromReserveDeckEffect;
+import com.gempukku.swccgo.logic.modifiers.ForfeitIncreaseLimitModifier;
+import com.gempukku.swccgo.logic.modifiers.ForfeitModifier;
 import com.gempukku.swccgo.logic.modifiers.MayNotPlayModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 
@@ -46,7 +53,33 @@ public class Card501_067 extends AbstractNormalEffect {
 
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(new MayNotPlayModifier(self, Filters.Imperial_Barrier, playerId));
+        modifiers.add(new ForfeitModifier(self, Filters.and(Filters.your(playerId), Filters.unique, Filters.character, Filters.abilityLessThan(4)), 2));
+        modifiers.add(new ForfeitIncreaseLimitModifier(self, Filters.and(Filters.your(playerId), Filters.unique, Filters.character, Filters.abilityLessThan(4)), 2));
         return modifiers;
     }
-    
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextTopLevelActions(final String playerId, final SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        List<TopLevelGameTextAction> actions = new LinkedList<TopLevelGameTextAction>();
+
+        GameTextActionId gameTextActionId = GameTextActionId.IM_SORRY_V__DOWNLOAD_CARD;
+
+        // Check condition(s)
+        if (GameConditions.isOncePerTurn(game, self, playerId, gameTextSourceCardId, gameTextActionId)
+                && GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId)) {
+
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerId, gameTextSourceCardId, gameTextActionId);
+
+            action.setText("Deploy card from Reserve Deck");
+            action.setActionMsg("Deploy an interior Cloud City site (or Lando to Dining Room) from Reserve Deck");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerTurnEffect(action));
+            // Perform result(s)
+            action.appendEffect(
+                    new DeployCardToLocationFromReserveDeckEffect(action, Filters.or(Filters.and(Filters.interior_site, Filters.Cloud_City_site), Filters.Lando), Filters.Dining_Room, Filters.location, true));
+            actions.add(action);
+        }
+        return actions;
+    }
 }
