a721c8c9588f110d25371ef3c947b88835031e18
Greg Zinn <greg.zinn@gmail.com>
2025-10-19 17:31:23 -0400

    Update from Design

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_070.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_070.java
@@ -48,7 +48,7 @@ public class Card501_070 extends AbstractAlienImperial {
     public Card501_070() {
         super(Side.DARK, 1, 5, 4, 5, 7, Title.Mara_Jade_The_Emperors_Hand, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Spy. Ordered to kill Luke Skywalker. Assumed the identity of a dancer named 'Arica' in order to sneak into Jabba's palace.");
-        setGameText("Deploys +2 to non-battleground locations. May [download] Mara Jade's Lightsaber here. While Revenge Of The Sith on table, gains [Sith]. If you just won a battle here, may retrieve a card with 'Mara' in game text. Immune to attrition < 4 (< 5 if alone or no Jedi here, < 6 if both).");
+        setGameText("While Revenge Of The Sith on table, gains [Sith]. Deploys +2 to non-battlegrounds. If you just won a battle here, may retrieve a card with 'Mara' in game text. May [download] Mara Jade's Lightsaber here. Immune to attrition < 4 (< 5 if alone or no Jedi here, < 6 if both).");
         addPersona(Persona.MARA_JADE);
         addIcons(Icon.PREMIUM, Icon.PILOT, Icon.WARRIOR, Icon.DEATH_STAR_II, Icon.VIRTUAL_SET_26);
         addKeywords(Keyword.SPY, Keyword.FEMALE);
@@ -59,6 +59,9 @@ public class Card501_070 extends AbstractAlienImperial {
     @Override
     protected List<Modifier> getGameTextAlwaysOnModifiers(SwccgGame game, PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<>();
+        Condition revengeOfTheSithOnTable = new OnTableCondition(self, Filters.Revenge_Of_The_Sith);
+
+        modifiers.add(new AddCardTypeModifier(self, self, revengeOfTheSithOnTable, CardType.SITH));
         modifiers.add(new DeployCostToLocationModifier(self, 2, Filters.non_battleground_location));
         return modifiers;
     }
@@ -66,11 +69,10 @@ public class Card501_070 extends AbstractAlienImperial {
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<>();
-        Condition revengeOfTheSithOnTable = new OnTableCondition(self, Filters.Revenge_Of_The_Sith);
+        
         Condition alone = new AloneCondition(self);
         Condition noJediHere = new NotCondition(new OnTableCondition(self, Filters.and(Filters.Jedi, Filters.here(self))));
 
-        modifiers.add(new AddCardTypeModifier(self, self, revengeOfTheSithOnTable, CardType.SITH));
         modifiers.add(new ImmuneToAttritionLessThanModifier(self, new ConditionEvaluator(4, new ConditionEvaluator(5, 6,
                 new AndCondition(alone, noJediHere)), new OrCondition(alone, noJediHere))));
         return modifiers;
