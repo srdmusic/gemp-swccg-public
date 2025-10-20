0e6acf3461a64e89e06610f28538a0262d2710bf
Jared Greenwald <jared.greenwald@oracle.com>
2025-10-19 22:38:53 -0400

    updates

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_060.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_060.java
@@ -1,7 +1,6 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractDroid;
-import com.gempukku.swccgo.cards.conditions.GameTextModificationCondition;
 import com.gempukku.swccgo.cards.conditions.HitCondition;
 import com.gempukku.swccgo.cards.conditions.OnTableCondition;
 import com.gempukku.swccgo.cards.conditions.PresentWithCondition;
@@ -21,10 +20,10 @@ import com.gempukku.swccgo.logic.conditions.Condition;
 import com.gempukku.swccgo.logic.conditions.NotCondition;
 import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
 import com.gempukku.swccgo.logic.modifiers.DrawsBattleDestinyIfUnableToOtherwiseModifier;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainModifier;
 import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionLessThanModifier;
 import com.gempukku.swccgo.logic.modifiers.MayNotBeTargetedByWeaponsModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.modifiers.ModifyGameTextType;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -37,10 +36,10 @@ import java.util.List;
  */
 public class Card501_060 extends AbstractDroid {
     public Card501_060() {
-        super(Side.DARK, 2, 6, 6, 6, Title.Guri, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
+        super(Side.DARK, 2, 4, 6, 6, Title.Guri, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setArmor(5);
         setLore("Human-replica droid. Programmed to function as Xizor's personal bodyguard and assassin. Black Sun agent. Cost 9 million credits. Worth every decicred.");
-        setGameText("[Pilot] 2. Draws one battle destiny if unable to otherwise. While present at a site with Xizor, Force drain +1 here and he may not be targeted by weapons (unless Guri 'hit'). If your [Reflections II] objective on table, immune to attrition < 5.");
+        setGameText("[Pilot] 2. While Guri and Xizor present at same site, Force drain +1 here and Xizor may not be targeted by weapons (unless Guri 'hit'). While your [Reflections II] objective on table, draws one battle destiny if unable to otherwise and immune to attrition < 5");
         addIcons(Icon.REFLECTIONS_II, Icon.PILOT, Icon.WARRIOR, Icon.PRESENCE, Icon.VIRTUAL_SET_26);
         addKeywords(Keyword.FEMALE, Keyword.BLACK_SUN_AGENT, Keyword.BODYGUARD, Keyword.ASSASSIN);
         addModelTypes(ModelType.ASSASSIN);
@@ -51,16 +50,14 @@ public class Card501_060 extends AbstractDroid {
     @Override
     protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
         List<Modifier> modifiers = new LinkedList<Modifier>();
+        String playerId = self.getOwner();
         modifiers.add(new AddsPowerToPilotedBySelfModifier(self, 2));
-        modifiers.add(new DrawsBattleDestinyIfUnableToOtherwiseModifier(self, 1));
-        Condition notHit = new NotCondition(new HitCondition(self));
-        Condition modifierXizorCondition = new AndCondition(new PresentWithCondition(self, Filters.Xizor), 
-            new NotCondition(new GameTextModificationCondition(self, ModifyGameTextType.LEGACY__TREAT_XIZOR_AS_SHADA)), notHit);
-        modifiers.add(new MayNotBeTargetedByWeaponsModifier(self, Filters.Xizor, modifierXizorCondition));
-        Condition modifierShadaCondition = new AndCondition(new PresentWithCondition(self, Filters.title("Shada")), 
-            new GameTextModificationCondition(self, ModifyGameTextType.LEGACY__TREAT_XIZOR_AS_SHADA), notHit);
-        modifiers.add(new MayNotBeTargetedByWeaponsModifier(self, Filters.title("Shada"), modifierShadaCondition));
+        Condition presentWithXizor = new PresentWithCondition(self, Filters.and(Filters.Xizor, Filters.atSameSite(self)));
+        modifiers.add(new ForceDrainModifier(self, Filters.sameSite(self), presentWithXizor, 1, playerId));
+        Condition presetnWithXizorAndNotHit = new AndCondition(presentWithXizor, new NotCondition(new HitCondition(self)));
+        modifiers.add(new MayNotBeTargetedByWeaponsModifier(self, Filters.Xizor, presetnWithXizorAndNotHit));
         Condition yourRef2ObjectiveOnTable = new OnTableCondition(self, Filters.and(Filters.your(self), Icon.REFLECTIONS_II, Filters.Objective));
+        modifiers.add(new DrawsBattleDestinyIfUnableToOtherwiseModifier(self, yourRef2ObjectiveOnTable, 1));
         modifiers.add(new ImmuneToAttritionLessThanModifier(self, yourRef2ObjectiveOnTable, 5));
         return modifiers;
     }
