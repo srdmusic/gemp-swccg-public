27bf03006fe1e2608b027103e89182c36bb2c1d3
Greg Zinn <greg.zinn@gmail.com>
2025-10-22 00:11:05 -0400

    Power and Forfeit (exact copy from non-V Rystall)

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_062.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_062.java
@@ -1,12 +1,23 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.gempukku.swccgo.cards.AbstractAlien;
+import com.gempukku.swccgo.cards.conditions.AtCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Keyword;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.conditions.Condition;
+import com.gempukku.swccgo.logic.modifiers.ForfeitModifier;
+import com.gempukku.swccgo.logic.modifiers.PowerModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
 
 /**
  * Set: Playtesting
@@ -23,5 +34,15 @@ public class Card501_062 extends AbstractAlien {
         addKeywords(Keyword.MUSICIAN, Keyword.FEMALE);
         setVirtualSuffix(true);
         setTestingText("Rystall (V)");
+    }
+
+    @Override
+    protected List<Modifier> getGameTextWhileActiveInPlayModifiers(SwccgGame game, final PhysicalCard self) {
+        Condition atCoruscantSite = new AtCondition(self, Filters.Coruscant_site);
+
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new PowerModifier(self, atCoruscantSite, 2));
+        modifiers.add(new ForfeitModifier(self, atCoruscantSite, 2));
+        return modifiers;
     }
 }
