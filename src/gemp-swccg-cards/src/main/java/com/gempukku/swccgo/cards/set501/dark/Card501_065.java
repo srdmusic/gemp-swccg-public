94dabb7d8bf1c7ff90c949aa98266df2b8fef541
Greg Zinn <greg.zinn@gmail.com>
2025-10-26 00:38:08 -0400

    Templating

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_065.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_065.java
@@ -16,14 +16,13 @@ import com.gempukku.swccgo.common.Title;
 public class Card501_065 extends AbstractSystem {
     public Card501_065() {
         super(Side.DARK, Title.Coruscant, 0, ExpansionSet.PLAYTESTING, Rarity.V);
-        setLocationDarkSideGameText("Invisible Hand is hyperspeed +3 moving to or from here. Your [Episode I] pilots deploy -1 here.");
-        setLocationLightSideGameText("If Insidious Prisoner on table, unless a Jedi here, your total battle destiny -1 here.");
+        setLocationDarkSideGameText("Invisible Hand is hyperspeed +3 when moving to or from here. Your [Episode I] pilots deploy -1 here.");
+        setLocationLightSideGameText("If Insidious Prisoner on table, unless a Jedi here, your total battle destiny is -1 here.");
         addIcon(Icon.DARK_FORCE, 2);
         addIcon(Icon.LIGHT_FORCE, 1);
         addIcons(Icon.PLANET, Icon.EPISODE_I, Icon.VIRTUAL_SET_26);
         setVirtualSuffix(true);
         setTestingText("Coruscant (V)");
-        hideFromDeckBuilder();
     }
     
 }
