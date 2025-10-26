6e8e0e47c9228b0e8bea5dd06ca8e592834d2346
Greg Zinn <greg.zinn@gmail.com>
2025-10-25 22:55:11 -0400

    Upload Wookiee (copy from Naboo Battle Plains)

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_213.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_213.java
@@ -1,15 +1,23 @@
 package com.gempukku.swccgo.cards.set501.light;
 
+import java.util.Collections;
+import java.util.List;
+
 import com.gempukku.swccgo.cards.AbstractSite;
 import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
+import com.gempukku.swccgo.logic.effects.choose.TakeCardIntoHandFromReserveDeckEffect;
 
 /**
  * Set: Playtesting
@@ -35,5 +43,27 @@ public class Card501_213 extends AbstractSite {
         return GameConditions.isDuringStartOfGame(game)
                 && game.getModifiersQuerying().getStartingLocation(playerId) == null
                 && game.getGameState().getObjectivePlayed(playerId) == null;
+    }
+
+    @Override
+    protected List<TopLevelGameTextAction> getGameTextLightSideTopLevelActions(String playerOnLightSideOfLocation, SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.CHEWIES_HUT__UPLOAD_WOOKIEE;
+
+        // Check condition(s)
+        if (GameConditions.isOncePerGame(game, self, gameTextActionId)
+                && GameConditions.canTakeCardsIntoHandFromReserveDeck(game, playerOnLightSideOfLocation, self, gameTextActionId)) {
+
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerOnLightSideOfLocation, gameTextSourceCardId, gameTextActionId);
+            action.setText("Take Wookiee into hand from Reserve Deck");
+            action.setActionMsg("Take a Wookiee into hand from Reserve Deck");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerGameEffect(action));
+            // Perform result(s)
+            action.appendEffect(
+                    new TakeCardIntoHandFromReserveDeckEffect(action, playerOnLightSideOfLocation, Filters.Wookiee, true));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 }
