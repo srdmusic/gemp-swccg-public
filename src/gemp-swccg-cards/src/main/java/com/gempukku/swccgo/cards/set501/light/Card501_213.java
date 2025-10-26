7dfaf450a62d97ece2572031bff6d1467a2536e4
Greg Zinn <greg.zinn@gmail.com>
2025-10-25 21:23:18 -0400

    Template update

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_213.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_213.java
@@ -18,13 +18,12 @@ import com.gempukku.swccgo.common.Uniqueness;
 public class Card501_213 extends AbstractSite {
     public Card501_213() {
         super(Side.LIGHT, "Kashyyyk: Chewie's Hut", Title.Kashyyyk, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setLocationLightSideGameText("Deploys only as a starting location. While Wookiee Homestead on table, no Force Drains here.");
-        setLocationDarkSideGameText("Until the end of your first turn, you must first use 1 Force to deploy a character to a Kashyyyk site.");
+        setLocationLightSideGameText("Deploys only as a starting location. Once per game, may [upload] a Wookiee. While Wookiee Homestead on table, no Force Drains here.");
+        setLocationDarkSideGameText("");
         addIcon(Icon.LIGHT_FORCE, 2);
         addIcon(Icon.DARK_FORCE, 0);
         addIcons(Icon.INTERIOR_SITE, Icon.PLANET, Icon.EPISODE_I, Icon.VIRTUAL_SET_26);
         setTestingText("Kashyyyk: Chewie's Hut");
-        hideFromDeckBuilder();
     }
     
 }
