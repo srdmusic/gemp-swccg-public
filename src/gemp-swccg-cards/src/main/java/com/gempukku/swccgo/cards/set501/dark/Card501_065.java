7a0d1bba5e76b59921fa8917e6126c2c10ac0c41
Greg Zinn <greg.zinn@gmail.com>
2025-10-26 00:43:32 -0400

    Invisible Hand Hyperspeed (copy from Rendezvous point)

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_065.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_065.java
@@ -1,11 +1,20 @@
 package com.gempukku.swccgo.cards.set501.dark;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import com.gempukku.swccgo.cards.AbstractSystem;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
+import com.gempukku.swccgo.filters.Filters;
+import com.gempukku.swccgo.game.PhysicalCard;
+import com.gempukku.swccgo.game.SwccgGame;
+import com.gempukku.swccgo.logic.modifiers.HyperspeedWhenMovingFromLocationModifier;
+import com.gempukku.swccgo.logic.modifiers.HyperspeedWhenMovingToLocationModifier;
+import com.gempukku.swccgo.logic.modifiers.Modifier;
 
 /**
  * Set: Playtesting
@@ -25,4 +34,11 @@ public class Card501_065 extends AbstractSystem {
         setTestingText("Coruscant (V)");
     }
     
+    @Override
+    protected List<Modifier> getGameTextDarkSideWhileActiveModifiers(String playerOnDarkSideOfLocation, SwccgGame game, PhysicalCard self) {
+        List<Modifier> modifiers = new LinkedList<Modifier>();
+        modifiers.add(new HyperspeedWhenMovingToLocationModifier(self, Filters.Invisible_Hand, 3, self));
+        modifiers.add(new HyperspeedWhenMovingFromLocationModifier(self, Filters.Invisible_Hand, 3, self));
+        return modifiers;
+    }
 }
