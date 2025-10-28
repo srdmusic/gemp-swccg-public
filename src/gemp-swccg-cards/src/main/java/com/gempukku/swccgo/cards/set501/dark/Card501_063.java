1a53a09572d8c6047b3252db67478986822d50ec
Greg Zinn <greg.zinn@gmail.com>
2025-10-28 19:28:45 -0400

    Work in progress

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_063.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_063.java
@@ -6,15 +6,21 @@ import java.util.List;
 
 import com.gempukku.swccgo.cards.AbstractUsedInterrupt;
 import com.gempukku.swccgo.cards.GameConditions;
+import com.gempukku.swccgo.cards.effects.usage.OncePerGameEffect;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.actions.PlayInterruptAction;
 import com.gempukku.swccgo.logic.effects.choose.DrawCardsIntoHandFromForcePileEffect;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainsMayNotBeCanceledModifier;
+import com.gempukku.swccgo.logic.modifiers.ForceDrainsMayNotBeReducedModifier;
+import com.gempukku.swccgo.logic.effects.AddUntilEndOfTurnModifierEffect;
 import com.gempukku.swccgo.logic.effects.PutCardsFromHandOnForcePileEffect;
 import com.gempukku.swccgo.logic.effects.RespondablePlayCardEffect;
 import com.gempukku.swccgo.logic.timing.Action;
@@ -29,7 +35,7 @@ public class Card501_063 extends AbstractUsedInterrupt {
     public Card501_063() {
         super(Side.DARK, 3, "Endless Legions", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("");
-        setGameText("If your stormtroopers control three battlegrounds and/or Rebel Base locations, choose: Draw 3 cards from Force Pile, then place 2 cards from hand on Force Pile. OR Once per game, your Force drains where you have a stormtrooper may not be canceled or reduced this turn.");
+        setGameText("If your stormtroopers control three battleground and/or Rebel Base locations, choose: Draw three cards from Force Pile, then place two cards from hand on Force Pile. OR Once per game, your Force drains at those locations may not be canceled or reduced this turn.");
         addIcons(Icon.VIRTUAL_SET_26);
         setTestingText("Endless Legions");
     }
@@ -38,9 +44,12 @@ public class Card501_063 extends AbstractUsedInterrupt {
     protected List<PlayInterruptAction> getGameTextTopLevelActions(final String playerId, SwccgGame game, PhysicalCard self) {
         List<PlayInterruptAction> actions = new LinkedList<>();
 
-        // Check condition(s)
-        if (GameConditions.hasForcePile(game, playerId)) {
-            final PlayInterruptAction action = new PlayInterruptAction(game, self);
+        GameTextActionId gameTextActionId = GameTextActionId.OTHER_CARD_ACTION_1;
+        // Check more condition(s) for action 1
+        if (GameConditions.hasForcePile(game, playerId)
+                && GameConditions.hasHand(game, playerId)) {
+
+            final PlayInterruptAction action = new PlayInterruptAction(game, self, gameTextActionId);
 
             action.setText("Draw three cards from Force Pile");
             // Allow response(s)
@@ -48,13 +57,6 @@ public class Card501_063 extends AbstractUsedInterrupt {
                     new RespondablePlayCardEffect(action) {
                         @Override
                         protected void performActionResults(Action targetingAction) {
-                            // Perform result(s)
-                            /*
-                            action.appendEffect(
-                                    new DrawCardsIntoHandFromForcePileEffect(action, playerId, 3));
-                            action.appendEffect(
-                                    new PutCardsFromHandOnForcePileEffect(action, playerId, 2, 2));
-                            */
                             action.appendEffect(new DrawCardsIntoHandFromForcePileEffect(action, playerId, 3) {
                                 @Override
                                 protected void cardsDrawnIntoHand(Collection<PhysicalCard> cards) {
@@ -66,6 +68,37 @@ public class Card501_063 extends AbstractUsedInterrupt {
                         }
                     }
             );
+            actions.add(action);
+        }
+
+        gameTextActionId = GameTextActionId.ENDLESS_LEGIONS__PROTECT_FORCE_DRAINS;
+        // Check more condition(s) for action 2
+        if (GameConditions.isOncePerGame(game, self, gameTextActionId)) {
+            final PlayInterruptAction action = new PlayInterruptAction(game, self, gameTextActionId);
+
+            action.setText("Protect Force drains");
+            action.setActionMsg("Protect Force drains where they have a stormtrooper from being canceled or reduced this turn");
+            // Update usage limit(s)
+            action.appendUsage(
+                    new OncePerGameEffect(action));
+            // Allow response(s)
+            action.allowResponses(
+                    new RespondablePlayCardEffect(action) {
+                        @Override
+                        protected void performActionResults(Action targetingAction) {
+                            // Perform result(s)
+                            action.appendEffect(
+                                    new AddUntilEndOfTurnModifierEffect(action,
+                                            new ForceDrainsMayNotBeCanceledModifier(self, Filters.any, playerId), "Force drains may not be canceled")
+                            );
+                            action.appendEffect(
+                                    new AddUntilEndOfTurnModifierEffect(action,
+                                            new ForceDrainsMayNotBeReducedModifier(self, Filters.any, playerId), "Force drains may not be reduced")
+                            );
+                        }
+                    }
+            );
+
             actions.add(action);
         }
 
