b28c6628613723cc83d9d94a7fc88d54aed50c48
jdipaolo1 <jeremydipaolo1@gmail.com>
2025-10-28 16:26:26 -0400

    Handle Horizontal Server Side

--- src/gemp-swccg-async/src/main/web/js/gemp-016/autoZoomHandler.js
+++ src/gemp-swccg-async/src/main/web/js/gemp-016/autoZoomHandler.js
@@ -266,7 +266,8 @@ class AutoZoom {
 		const blueprintId = target.attr("value");
 		const testingText = target.attr("data-testingText");
 		const backSideTestingText = target.attr("data-backSideTestingText");
-		const card = new Card(blueprintId, testingText, backSideTestingText, "SPECIAL", "hint", "");
+		const horizontal = target.attr("data-horizontal") === "true";
+		const card = new Card(blueprintId, testingText, backSideTestingText, horizontal, "SPECIAL", "hint", "");
 
 		this.baseImageDiv = target[0];
 		this.displayPreviewImage(card, this.baseImageDiv);
