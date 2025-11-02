f67fe6a070d48d99f77d0dba914623fdc3544de0
Greg Zinn <greg.zinn@gmail.com>
2025-11-02 01:28:33 -0500

    New text

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_216.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_216.java
@@ -29,7 +29,7 @@ public class Card501_216 extends AbstractStartingInterrupt {
     public Card501_216() {
         super(Side.LIGHT, 3, "Something About This Boy", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("'What'd he mean by that?' 'I'll tell you later.'");
-        setGameText("If your starting location was City Outskirts, deploy Slave Quarters (with Prophecy Of The Force there), Jedi Business and Your Thoughts Dwell On Your Mother. Light Side goes first. When you draw your starting hand, draw only 6 cards. Place Interrupt in Lost Pile.");
+        setGameText("If your starting location was Slave Quarters, deploy Prophecy Of The Force there, Jedi Business, and Your Thoughts Dwell On Your Mother. [Upload] City Outskirts. Light Side goes first. When you draw your starting hand, draw only 6 cards. Place Interrupt in Lost Pile.");
         addIcons(Icon.EPISODE_I, Icon.VIRTUAL_SET_26);
         setTestingText("Something About This Boy");
     }
