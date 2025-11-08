90fd5ba6883c762a8e893b82343711bdb6a52c8b
Greg Zinn <greg.zinn@gmail.com>
2025-11-08 18:03:54 -0500

    Underground Corridor remove drain bonus

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_205.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_205.java
@@ -6,7 +6,6 @@ import java.util.List;
 import com.gempukku.swccgo.cards.AbstractSite;
 import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.actions.MoveUsingLocationTextAction;
-import com.gempukku.swccgo.cards.conditions.OnTableCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Phase;
@@ -19,9 +18,6 @@ import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
-import com.gempukku.swccgo.logic.conditions.Condition;
-import com.gempukku.swccgo.logic.modifiers.ForceDrainModifier;
-import com.gempukku.swccgo.logic.modifiers.Modifier;
 
 /**
  * Set: Playtesting
@@ -57,15 +53,5 @@ public class Card501_205 extends AbstractSite {
             actions.add(action);
         }
         return actions;
-    }
-
-    @Override
-    protected List<Modifier> getGameTextDarkSideWhileActiveModifiers(String playerOnDarkSideOfLocation, SwccgGame game, PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<Modifier>();
-
-        Condition GatherAlliesAndTrainOnTable = new OnTableCondition(self, Filters.Gather_Allies_And_Train);
-
-        modifiers.add(new ForceDrainModifier(self, GatherAlliesAndTrainOnTable, 1, playerOnDarkSideOfLocation));
-        return modifiers;
-    }    
+    } 
 }
