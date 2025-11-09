9647f969361ee2a618f6886c2be1819ef4830d1a
Greg Zinn <greg.zinn@gmail.com>
2025-11-08 19:42:38 -0500

    Boy - Updated text and change starting requirements

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_216.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/light/Card501_216.java
@@ -6,6 +6,7 @@ import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Uniqueness;
+import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
@@ -30,16 +31,18 @@ public class Card501_216 extends AbstractStartingInterrupt {
     public Card501_216() {
         super(Side.LIGHT, 3, "Something About This Boy", Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("'What'd he mean by that?' 'I'll tell you later.'");
-        setGameText("If your starting location was Slave Quarters, deploy Prophecy Of The Force there, Jedi Business, and Your Thoughts Dwell On Your Mother. [Upload] City Outskirts. Light Side goes first. When you draw your starting hand, draw only 6 cards. Place Interrupt in Lost Pile.");
+        setGameText("If your starting location was [Skywalker] Slave Quarters, deploy Prophecy Of The Force there, Jedi Business, and Your Thoughts Dwell On Your Mother. [Upload] City Outskirts. Light Side goes first. When you draw your starting hand, draw only 5 cards. Place Interrupt in Lost Pile.");
         addIcons(Icon.EPISODE_I, Icon.VIRTUAL_SET_26);
         setTestingText("Something About This Boy");
     }
 
     @Override
     protected PlayInterruptAction getGameTextStartingAction(final String playerId, final SwccgGame game, final PhysicalCard self) {
+        Filter requiredStart = Filters.and(Icon.SKYWALKER, Filters.Slave_Quarters);
+        
         // Check condition(s)
         final PhysicalCard startingLocation = game.getModifiersQuerying().getStartingLocation(playerId);
-        if (startingLocation != null && Filters.Slave_Quarters.accepts(game, startingLocation)) {
+        if (startingLocation != null && requiredStart.accepts(game, startingLocation)) {
 
             final PlayInterruptAction action = new PlayInterruptAction(game, self);
             action.setText("Deploy Prophecy Of The Force and other cards from Reserve Deck");
