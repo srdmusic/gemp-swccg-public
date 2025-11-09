3a2174fc2f5c5418a902e1bc0bda1bfd60df1d63
hatou003 <hatou003@umn.edu>
2025-11-09 10:26:28 -0600

    UPDATE Coruscant (V)

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_065.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_065.java
@@ -4,8 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import com.gempukku.swccgo.cards.AbstractSystem;
-import com.gempukku.swccgo.cards.conditions.HereCondition;
-import com.gempukku.swccgo.cards.conditions.OnTableCondition;
+import com.gempukku.swccgo.cards.conditions.ControlsWithCondition;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.Icon;
 import com.gempukku.swccgo.common.Rarity;
@@ -15,13 +14,10 @@ import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.conditions.AndCondition;
-import com.gempukku.swccgo.logic.conditions.Condition;
-import com.gempukku.swccgo.logic.conditions.UnlessCondition;
 import com.gempukku.swccgo.logic.modifiers.DeployCostToLocationModifier;
 import com.gempukku.swccgo.logic.modifiers.HyperspeedWhenMovingFromLocationModifier;
 import com.gempukku.swccgo.logic.modifiers.HyperspeedWhenMovingToLocationModifier;
-import com.gempukku.swccgo.logic.modifiers.TotalBattleDestinyModifier;
+import com.gempukku.swccgo.logic.modifiers.SuspendsCardModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 
 /**
@@ -34,7 +30,7 @@ public class Card501_065 extends AbstractSystem {
     public Card501_065() {
         super(Side.DARK, Title.Coruscant, 0, ExpansionSet.PLAYTESTING, Rarity.V);
         setLocationDarkSideGameText("Invisible Hand is hyperspeed +3 when moving to or from here. Your [Episode I] pilots deploy -1 here.");
-        setLocationLightSideGameText("If Insidious Prisoner on table, unless a Jedi here, your total battle destiny is -1 here.");
+        setLocationLightSideGameText("If Invisible Hand controls this system, Menace Fades is suspended.");
         addIcon(Icon.DARK_FORCE, 2);
         addIcon(Icon.LIGHT_FORCE, 1);
         addIcons(Icon.PLANET, Icon.EPISODE_I, Icon.VIRTUAL_SET_26);
@@ -55,11 +51,9 @@ public class Card501_065 extends AbstractSystem {
 
     @Override
     protected List<Modifier> getGameTextLightSideWhileActiveModifiers(String playerOnLightSideOfLocation, SwccgGame game, PhysicalCard self) {
-        Condition insidiousPrisonerOnTable = new OnTableCondition(self, Filters.Insidious_Prisoner);
-        Condition unlessJediHere = new UnlessCondition(new HereCondition(self, Filters.Jedi));
-
         List<Modifier> modifiers = new LinkedList<Modifier>();
-        modifiers.add(new TotalBattleDestinyModifier(self, new AndCondition(insidiousPrisonerOnTable, unlessJediHere), -1, playerOnLightSideOfLocation));
+        modifiers.add(new SuspendsCardModifier(self, Filters.title(Title.Menace_Fades), new ControlsWithCondition(game.getOpponent(playerOnLightSideOfLocation), self, Filters.Invisible_Hand)));
+
         return modifiers;
     }
 }
