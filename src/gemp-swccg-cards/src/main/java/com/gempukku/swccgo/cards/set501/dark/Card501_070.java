03a76358a0c65222767583a98f30b9b01369d5d1
Greg Zinn <greg.zinn@gmail.com>
2025-11-09 23:31:45 -0500

    Remove old functions

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_070.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_070.java
@@ -9,7 +9,6 @@ import com.gempukku.swccgo.cards.GameConditions;
 import com.gempukku.swccgo.cards.conditions.AloneCondition;
 import com.gempukku.swccgo.cards.conditions.OnTableCondition;
 import com.gempukku.swccgo.cards.evaluators.ConditionEvaluator;
-import com.gempukku.swccgo.common.CardType;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
@@ -31,8 +30,6 @@ import com.gempukku.swccgo.logic.conditions.NotCondition;
 import com.gempukku.swccgo.logic.conditions.OrCondition;
 import com.gempukku.swccgo.logic.effects.choose.DeployCardToTargetFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.effects.RetrieveCardEffect;
-import com.gempukku.swccgo.logic.modifiers.AddCardTypeModifier;
-import com.gempukku.swccgo.logic.modifiers.DeployCostToLocationModifier;
 import com.gempukku.swccgo.logic.modifiers.ImmuneToAttritionLessThanModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
 import com.gempukku.swccgo.logic.timing.EffectResult;
@@ -48,22 +45,12 @@ public class Card501_070 extends AbstractAlienImperial {
     public Card501_070() {
         super(Side.DARK, 1, 5, 4, 5, 7, Title.Mara_Jade_The_Emperors_Hand, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
         setLore("Spy. Ordered to kill Luke Skywalker. Assumed the identity of a dancer named 'Arica' in order to sneak into Jabba's palace.");
-        setGameText("Functions as your apprentice. Battle destiny draws may not be added here. May [download] Mara Jade's Lightsaber here. If you just won a battle here, may retrieve a card with 'Mara' in gametext. Immune to Rebel Barrier and attrition < 4 (< 5 if Luke on table).");
+        setGameText("Functions as your apprentice. Battle destiny draws may not be added here. May [download] Mara Jade's Lightsaber here. If you just won a battle here, may retrieve a card with 'Mara' in game text. Immune to Rebel Barrier and attrition < 4 (< 5 if Luke on table).");
         addPersona(Persona.MARA_JADE);
         addIcons(Icon.PREMIUM, Icon.PILOT, Icon.WARRIOR, Icon.DEATH_STAR_II, Icon.VIRTUAL_SET_26);
         addKeywords(Keyword.SPY, Keyword.FEMALE);
         setVirtualSuffix(true);
         setTestingText("Mara Jade, The Emperor's Hand (V)");
-    }
-
-    @Override
-    protected List<Modifier> getGameTextAlwaysOnModifiers(SwccgGame game, PhysicalCard self) {
-        List<Modifier> modifiers = new LinkedList<>();
-        Condition revengeOfTheSithOnTable = new OnTableCondition(self, Filters.Revenge_Of_The_Sith);
-
-        modifiers.add(new AddCardTypeModifier(self, self, revengeOfTheSithOnTable, CardType.SITH));
-        modifiers.add(new DeployCostToLocationModifier(self, 2, Filters.non_battleground_location));
-        return modifiers;
     }
 
     @Override
