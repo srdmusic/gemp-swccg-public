92d8ab62f813bb77e36c87b65b70ace9def8b5e1
BOTVHD <spartacus5380@yahoo.com>
2026-01-29 00:04:54 -0500

    minor fixes, add tests, fix test rig battle init

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set6/dark/Card6_100.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set6/dark/Card6_100.java
@@ -2,9 +2,6 @@ package com.gempukku.swccgo.cards.set6.dark;
 
 import com.gempukku.swccgo.cards.AbstractDroid;
 import com.gempukku.swccgo.cards.GameConditions;
-import com.gempukku.swccgo.cards.conditions.PilotingCondition;
-import com.gempukku.swccgo.cards.effects.usage.OncePerBattleEffect;
-import com.gempukku.swccgo.cards.evaluators.CardMatchesEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
 import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
@@ -16,19 +13,12 @@ import com.gempukku.swccgo.filters.Filter;
 import com.gempukku.swccgo.filters.Filters;
 import com.gempukku.swccgo.game.PhysicalCard;
 import com.gempukku.swccgo.game.SwccgGame;
-import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
-import com.gempukku.swccgo.logic.effects.choose.DeployCardFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.effects.choose.DeployCardToLocationFromReserveDeckEffect;
-import com.gempukku.swccgo.logic.effects.choose.DeployCardToTargetFromReserveDeckEffect;
-import com.gempukku.swccgo.logic.modifiers.AddsPowerToPilotedBySelfModifier;
-import com.gempukku.swccgo.logic.modifiers.DrawsBattleDestinyIfUnableToOtherwiseModifier;
-import com.gempukku.swccgo.logic.modifiers.MayDeployOtherCardsAsReactToLocationModifier;
 import com.gempukku.swccgo.logic.modifiers.MayNotReactFromLocationModifier;
 import com.gempukku.swccgo.logic.modifiers.MayNotReactToLocationModifier;
 import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.timing.Action;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
 import java.util.Collections;
@@ -57,26 +47,20 @@ public class Card6_100 extends AbstractDroid {
         List<Modifier> modifiers = new LinkedList<Modifier>();
         modifiers.add(new MayNotReactToLocationModifier(self, Filters.sameSite(self), opponent));
         modifiers.add(new MayNotReactFromLocationModifier(self, Filters.sameSite(self), opponent));
-//        modifiers.add(new MayDeployOtherCardsAsReactToLocationModifier(self, "Deploy non-unique alien as a 'react'", self.getOwner(),
-//                Filters.any, Filters.or(Filters.sameLocation(self), Filters.adjacentSite(self))));
         return modifiers;
     }
 
-/// may need to add some conditions for inactive due to being excluded from battle (when adjacent)?
     @Override
     protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
         String opponent = game.getOpponent(playerId);
 
         GameTextActionId gameTextActionId = GameTextActionId.CZ_4__DOWNLOAD_NON_UNIQUE_ALIEN_AS_REACT;//LOBOT__DOWNLOAD_ALIEN_AS_REACT;
 
-        Filter validSites = Filters.sameOrAdjacentSiteAs(self,Filters.Jabbas_Palace_site);
-        Filter battleOrForceDrainLocation;
-
         // Check condition(s)
-//        if ((TriggerConditions.battleInitiatedAt(game, effectResult, opponent, validSites)
-//                || TriggerConditions.forceDrainInitiatedBy(game, effectResult, opponent, validSites))
-//                && GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, true)) {
         if (GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, true)) {
+            final Filter validSites = Filters.and(Filters.sameOrAdjacentSite(self),Filters.Jabbas_Palace_site);
+            Filter battleOrForceDrainLocation;
+
             if(TriggerConditions.battleInitiatedAt(game, effectResult, opponent, validSites))
                 battleOrForceDrainLocation = Filters.battleLocation;
             else if(TriggerConditions.forceDrainInitiatedBy(game, effectResult, opponent, validSites))
@@ -96,76 +80,4 @@ public class Card6_100 extends AbstractDroid {
         }
         return null;
     }
-
-    //Filters.and(Filters.non_unique, Filters.alien)
-
-
-
-//    @Override
-//    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
-//        String opponent = game.getOpponent(playerId);
-//
-//        GameTextActionId gameTextActionId = GameTextActionId.EARLY_WARNING_NETWORK__DOWNLOAD_IMPERIAL_STARSHIP_AS_REACT;
-//
-//        // Check condition(s)
-//        if (TriggerConditions.battleInitiatedAt(game, effectResult, opponent, Filters.relatedSystem(self))
-//                && GameConditions.isOncePerBattle(game, self, playerId, gameTextSourceCardId, gameTextActionId)
-//                && GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, true)) {
-//
-//            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-//            action.setText("Deploy card as a 'react' from Reserve Deck");
-//            action.setActionMsg("Deploy a non-unique Imperial starship as a 'react' from Reserve Deck");
-//            // Update usage limit(s)
-//            action.appendUsage(
-//                    new OncePerBattleEffect(action));
-//            // Perform result(s)
-//            action.appendEffect(
-//                    new DeployCardFromReserveDeckEffect(action, Filters.and(Filters.non_unique, Filters.Imperial_starship), Filters.starfighter, true, true));
-//            return Collections.singletonList(action);
-//        }
-//        return null;
-//    }
-
-//    @Override
-//    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
-//        String opponent = game.getOpponent(playerId);
-//
-//        GameTextActionId gameTextActionId = GameTextActionId.HERMI_ODLE__DOWNLOAD_NON_UNIQUE_BLASTER_AS_REACT;
-//
-//        // Check condition(s)
-//        if (TriggerConditions.battleInitiated(game, effectResult, opponent)
-//                && GameConditions.isInBattle(game, self)
-//                && GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, true)) {
-//
-//            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-//            action.setText("Deploy blaster as 'react' from Reserve Deck");
-//            action.setActionMsg("Deploy a non-unique blaster as 'react' on " + GameUtils.getCardLink(self) + " from Reserve Deck");
-//            // Perform result(s)
-//            action.appendEffect(
-//                    new DeployCardToTargetFromReserveDeckEffect(action, Filters.and(Filters.non_unique, Filters.blaster), Filters.sameCardId(self), true, true, true));
-//            return Collections.singletonList(action);
-//        }
-//        return null;
-//    }
-
-//    @Override
-//    protected List<OptionalGameTextTriggerAction> getGameTextOptionalAfterTriggers(final String playerId, SwccgGame game, EffectResult effectResult, final PhysicalCard self, int gameTextSourceCardId) {
-//        String opponent = game.getOpponent(playerId);
-//
-//        GameTextActionId gameTextActionId = GameTextActionId.LOBOT__DOWNLOAD_ALIEN_AS_REACT;
-//
-//        // Check condition(s)
-//        if (TriggerConditions.battleInitiatedAt(game, effectResult, opponent, Filters.sameSite(self))
-//                && GameConditions.canDeployCardFromReserveDeck(game, playerId, self, gameTextActionId, true)) {
-//
-//            final OptionalGameTextTriggerAction action = new OptionalGameTextTriggerAction(self, gameTextSourceCardId, gameTextActionId);
-//            action.setText("Deploy alien as a 'react' from Reserve Deck");
-//            action.setActionMsg("Deploy an alien as a 'react' from Reserve Deck");
-//            // Perform result(s)
-//            action.appendEffect(
-//                    new DeployCardToLocationFromReserveDeckEffect(action, Filters.alien, Filters.here(self), Filters.Lando, true, true));
-//            return Collections.singletonList(action);
-//        }
-//        return null;
-//    }
 }
