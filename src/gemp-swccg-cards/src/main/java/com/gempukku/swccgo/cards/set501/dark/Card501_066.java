77e09275256f75768d823a408d9867bb297a4d82
Greg Zinn <greg.zinn@gmail.com>
2026-02-13 21:06:47 -0500

    Scarif Command Center

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set501/dark/Card501_066.java
@@ -2,11 +2,11 @@ package com.gempukku.swccgo.cards.set501.dark;
 
 import com.gempukku.swccgo.cards.AbstractSite;
 import com.gempukku.swccgo.cards.GameConditions;
-import com.gempukku.swccgo.cards.conditions.OccupiesWithCondition;
 import com.gempukku.swccgo.cards.effects.ConvertLocationByRaisingToTopEffect;
-import com.gempukku.swccgo.cards.evaluators.HereEvaluator;
 import com.gempukku.swccgo.common.ExpansionSet;
+import com.gempukku.swccgo.common.GameTextActionId;
 import com.gempukku.swccgo.common.Icon;
+import com.gempukku.swccgo.common.Persona;
 import com.gempukku.swccgo.common.Rarity;
 import com.gempukku.swccgo.common.Side;
 import com.gempukku.swccgo.common.Title;
@@ -18,14 +18,13 @@ import com.gempukku.swccgo.game.SwccgGame;
 import com.gempukku.swccgo.logic.GameUtils;
 import com.gempukku.swccgo.logic.TriggerConditions;
 import com.gempukku.swccgo.logic.actions.OptionalGameTextTriggerAction;
+import com.gempukku.swccgo.logic.actions.TopLevelGameTextAction;
 import com.gempukku.swccgo.logic.effects.TargetCardOnTableEffect;
 import com.gempukku.swccgo.logic.effects.UnrespondableEffect;
-import com.gempukku.swccgo.logic.modifiers.Modifier;
-import com.gempukku.swccgo.logic.modifiers.TotalPowerModifier;
+import com.gempukku.swccgo.logic.effects.choose.DeployCardToLocationFromReserveDeckEffect;
 import com.gempukku.swccgo.logic.timing.Action;
 import com.gempukku.swccgo.logic.timing.EffectResult;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -38,7 +37,7 @@ import java.util.List;
 public class Card501_066 extends AbstractSite {
     public Card501_066() {
         super(Side.DARK, "Scarif: Command Center", Title.Scarif, Uniqueness.UNIQUE, ExpansionSet.PLAYTESTING, Rarity.V);
-        setLocationDarkSideGameText("If you occupy with X leaders here, your total power is +X in battles at Scarif locations.");
+        setLocationDarkSideGameText("May [download] Krennic here.");
         setLocationLightSideGameText("If a player just Force drained here, they may raise a converted Scarif location to the top.");
         addIcon(Icon.DARK_FORCE, 2);
         addIcon(Icon.LIGHT_FORCE, 1);
@@ -47,12 +46,20 @@ public class Card501_066 extends AbstractSite {
     }
 
     @Override
-    protected List<Modifier> getGameTextDarkSideWhileActiveModifiers(String playerOnDarkSideOfLocation, SwccgGame game, PhysicalCard self) {
-        List<Modifier> modifiers = new ArrayList<>();
-        modifiers.add(new TotalPowerModifier(self, Filters.and(Filters.Scarif_location, Filters.battleLocation),
-                new OccupiesWithCondition(playerOnDarkSideOfLocation, self, Filters.leader),
-                new HereEvaluator(self, Filters.and(Filters.your(playerOnDarkSideOfLocation), Filters.leader)), playerOnDarkSideOfLocation));
-        return modifiers;
+    protected List<TopLevelGameTextAction> getGameTextDarkSideTopLevelActions(String playerOnDarkSideOfLocation, SwccgGame game, final PhysicalCard self, int gameTextSourceCardId) {
+        GameTextActionId gameTextActionId = GameTextActionId.SCARIF_COMMAND_CENTER__DOWNLOAD_KRENNIC;
+
+        // Check condition(s)
+        if (GameConditions.canDeployCardFromReserveDeck(game, playerOnDarkSideOfLocation, self, gameTextActionId, Persona.KRENNIC)) {
+
+            final TopLevelGameTextAction action = new TopLevelGameTextAction(self, playerOnDarkSideOfLocation, gameTextSourceCardId, gameTextActionId);
+            action.setText("Deploy Krennic from Reserve Deck");
+            // Perform result(s)
+            action.appendEffect(
+                    new DeployCardToLocationFromReserveDeckEffect(action, Filters.Krennic, Filters.here(self), true));
+            return Collections.singletonList(action);
+        }
+        return null;
     }
 
     @Override
