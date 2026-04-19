607e6b6057f98158b07f3502b1a58ea024e75254
BOTVHD <spartacus5380@yahoo.com>
2026-04-19 15:12:14 -0400

    fix incorrect game text string

--- src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set217/dark/Card217_015.java
+++ src/gemp-swccg-cards/src/main/java/com/gempukku/swccgo/cards/set217/dark/Card217_015.java
@@ -28,7 +28,7 @@ public class Card217_015 extends AbstractSite {
     public Card217_015() {
         super(Side.DARK, "Malachor: Sith Temple Gateway", Title.Malachor, Uniqueness.UNIQUE, ExpansionSet.SET_17, Rarity.V);
         setLocationDarkSideGameText("[Set 13] Maul ignores [Set 13] objective deployment restrictions here.");
-        setLocationLightSideGameText("Opponent may not target Ezra with weapons here.");
+        setLocationLightSideGameText("Opponent's weapons here may not target Ezra.");
         addIcon(Icon.DARK_FORCE, 1);
         addIcons(Icon.UNDERGROUND, Icon.INTERIOR_SITE, Icon.PLANET, Icon.CLOUD_CITY, Icon.VIRTUAL_SET_17);
     }
