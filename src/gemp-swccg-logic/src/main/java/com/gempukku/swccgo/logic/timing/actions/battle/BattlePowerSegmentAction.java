6ffa5464376df0d20101a5f7e1f5f82c15a99e7d
BOTVHD <spartacus5380@yahoo.com>
2026-04-24 20:11:04 -0400

    add user feedback message

--- src/gemp-swccg-logic/src/main/java/com/gempukku/swccgo/logic/timing/actions/battle/BattlePowerSegmentAction.java
+++ src/gemp-swccg-logic/src/main/java/com/gempukku/swccgo/logic/timing/actions/battle/BattlePowerSegmentAction.java
@@ -247,7 +247,10 @@ public class BattlePowerSegmentAction extends SystemQueueAction {
 
             // Determine how many destinies to attrition only player must draw
             int numDraws = modifiersQuerying.getNumDestinyDrawsToAttritionOnly(gameState, _playerId, false, false);
-            if (battleState.getNumBattleDestinyDrawn(_playerId) == 0) numDraws = 0; //unless total battle destiny exists, numDraws for attrition destinies is an unmodifiable 0
+            if (numDraws > 0 && battleState.getNumBattleDestinyDrawn(_playerId) == 0) { //unless total battle destiny exists, numDraws for attrition destinies is an unmodifiable 0
+                numDraws = 0;
+                gameState.sendMessage("Number of attrition destiny draws for " + _playerId + " reduced to 0 due to not having a total battle destiny.");
+            }
             if (numDraws > 0) {
                 if (gameState.getReserveDeckSize(_playerId) > 0) {
                     battleState.setDrewDestinyToAttrition(_playerId, true);
